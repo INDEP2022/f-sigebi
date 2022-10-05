dc63c2a466d8283f65cc05101fcbee588cc78c6e
Henry Giovanni Sanchez Solis <sshenrygiovanni@gmail.com>
2022-10-05 17:23:27 -0500

    abrir archivo excel o csv agregado

--- src/app/pages/executive-processes/pe-amdvda-m-update-mss-value/pe-amdvda-c-update-mss-value/pe-amdvda-c-update-mss-value.component.ts
+++ src/app/pages/executive-processes/pe-amdvda-m-update-mss-value/pe-amdvda-c-update-mss-value/pe-amdvda-c-update-mss-value.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+//XLSX
+import * as XLSX from 'xlsx';
+
 @Component({
   selector: 'app-pe-amdvda-c-update-mss-value',
   templateUrl: './pe-amdvda-c-update-mss-value.component.html',
@@ -8,9 +13,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeAmdvdaCUpdateMssValueComponent implements OnInit {
 
-  constructor() { }
+  ExcelData:any;
+  form: FormGroup = new FormGroup({}); 
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.prepareForm();
+  }
+
+  private prepareForm() {
+    this.form = this.fb.group({
+      readRecords: ['', []],
+      processedRecords: ['', []],
+      
+    });
+  }
+
+  settings = {
+
+    actions: false,
+    hideSubHeader: true,
+
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    
+    columns: {
+      solicitante: {
+        title: 'Solicitante',
+        width: '25px'
+      },
+      fecavaluo: {
+        title: 'Fecha de Avalúo',
+      },
+      institucion: {
+        title: 'Institución',
+      },
+      perito: {
+        title: 'Perito',
+      },
+      observ: {
+        title: 'Observaciones',
+      },
+      nobien: {
+        title: 'No. Bien',
+      },
+      valoravaluo: {
+        title: 'Valor avalúo',
+      },
+      moneda: {
+        title: 'Moneda',
+      },
+    },
+    noDataMessage: "No se encontrarón registros"
+  };
+
+  ReadExcel(event:any){
+
+    let file = event.target.files[0];
+
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+
+    fileReader.onload = (e)=>{
+      var workbook = XLSX.read(fileReader.result, {type: 'binary'});
+      var sheetNames = workbook.SheetNames;
+      this.ExcelData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetNames[0]])
+      console.log(this.ExcelData);
+    }
+
   }
 
 }
--- src/app/pages/executive-processes/pe-amdvda-m-update-mss-value/pe-amdvda-m-update-mss-value.module.ts
+++ src/app/pages/executive-processes/pe-amdvda-m-update-mss-value/pe-amdvda-m-update-mss-value.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { SharedModule } from 'src/app/shared/shared.module';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+
 import { PeAmdvdaMUpdateMssValueRoutingModule } from './pe-amdvda-m-update-mss-value-routing.module';
 import { PeAmdvdaCUpdateMssValueComponent } from './pe-amdvda-c-update-mss-value/pe-amdvda-c-update-mss-value.component';
 
@@ -11,7 +14,9 @@ import { PeAmdvdaCUpdateMssValueComponent } from './pe-amdvda-c-update-mss-value
   ],
   imports: [
     CommonModule,
-    PeAmdvdaMUpdateMssValueRoutingModule
+    PeAmdvdaMUpdateMssValueRoutingModule,
+    SharedModule,
+    Ng2SmartTableModule
   ]
 })
 export class PeAmdvdaMUpdateMssValueModule { }
