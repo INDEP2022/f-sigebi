7ef34c0f03f4117375a3c428a13259055530143a
Fernando <f3rfams@gmail.com>
2022-10-06 08:45:58 -0600

    feat(migracion catalogos): Migracion de catalogos desde lotes a parrafos

--- src/app/core/menu.ts
+++ src/app/core/menu.ts
@@ -42,6 +42,46 @@ export const MENU: IMenuItem[] = [
         label: 'Sub Delegacion',
         link: '/pages/catalogs/sub-delegations',
       },
+      {
+        label: 'Lotes',
+        link: '/pages/catalogs/batch',
+      },
+      {
+        label: 'Medio Fotografía',
+        link: '/pages/catalogs/photograph-media',
+      },
+      {
+        label: 'Medio Imagen',
+        link: '/pages/catalogs/image-media',
+      },
+      {
+        label: 'MinPub',
+        link: '/pages/catalogs/minpub',
+      },
+      {
+        label: 'Motivo Revisión',
+        link: '/pages/catalogs/revision-reason',
+      },
+      {
+        label: 'Motivo No Entrega',
+        link: '/pages/catalogs/non-delivery-reasons',
+      },
+      {
+        label: 'Municipios',
+        link: '/pages/catalogs/municipalities',
+      },
+      {
+        label: 'Normas',
+        link: '/pages/catalogs/norms',
+      },
+      {
+        label: 'Notarios',
+        link: '/pages/catalogs/notary',
+      },
+      {
+        label: 'Párrafos',
+        link: '/pages/catalogs/paragraphs',
+      },
     ],
   },
   {
