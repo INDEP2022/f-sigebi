42d1f334d2f8a94ab1e3e55f6851a90c5ec42dd0
genaroadrian <al221711695@gmail.com>
2022-10-07 12:03:45 -0500

    Documentos relacionados

--- src/app/pages/documents-reception/dr-flyers/dr-flyers.module.ts
+++ src/app/pages/documents-reception/dr-flyers/dr-flyers.module.ts
@@ -4,14 +4,26 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { DrFlyersRoutingModule } from './dr-flyers-routing.module';
 import { RdFDocumentsReceptionRegisterComponent } from './rd-f-documents-reception-register/rd-f-documents-reception-register.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { RdFRecordUpdateComponent } from './rd-f-record-update/rd-f-record-update.component';
+import { RdFShiftChangeComponent } from './rd-f-shift-change/rd-f-shift-change.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { RdFShitChangeHistoryComponent } from './rd-f-shift-change/rd-f-shit-change-history/rd-f-shit-change-history.component';
+import { RdFRelatedDocumentsComponent } from './rd-f-related-documents/rd-f-related-documents.component';
 
 @NgModule({
-  declarations: [RdFDocumentsReceptionRegisterComponent],
+  declarations: [
+    RdFDocumentsReceptionRegisterComponent,
+    RdFRecordUpdateComponent,
+    RdFShiftChangeComponent,
+    RdFShitChangeHistoryComponent,
+    RdFRelatedDocumentsComponent,
+  ],
   imports: [
     CommonModule,
     DrFlyersRoutingModule,
     SharedModule,
     BsDatepickerModule,
+    ModalModule.forChild(),
   ],
 })
 export class DrFlyersModule {}
---
+++ src/app/pages/documents-reception/dr-flyers/rd-f-related-documents/rd-f-related-documents.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { TABLE_SETTINGS } from 'src/app/common/constants/table-settings';
+import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import { BasePage } from 'src/app/core/shared/base-page';
+import { DefaultSelect } from 'src/app/shared/components/select/default-select';
+import {
+  RELATED_DOCUMENTS_COLUMNS,
+  RELATED_DOCUMENTS_EXAMPLE_DATA,
+} from './related-documents-columns';
+
+@Component({
+  selector: 'app-rd-f-related-documents',
+  templateUrl: './rd-f-related-documents.component.html',
+  styles: [
+    `
+      :host ::ng-deep form-radio .form-group {
+        margin: 0;
+        padding-bottom: 0;
+        padding-top: 0;
+      }
+    `,
+  ],
+})
+export class RdFRelatedDocumentsComponent extends BasePage implements OnInit {
+  managementForm: FormGroup;
+  settings = { ...TABLE_SETTINGS, actions: false };
+  select = new DefaultSelect();
+  data = RELATED_DOCUMENTS_EXAMPLE_DATA;
+  params = new BehaviorSubject<ListParams>(new ListParams());
+  constructor(private fb: FormBuilder) {
+    super();
+    this.settings.columns = RELATED_DOCUMENTS_COLUMNS;
+  }
+
+  ngOnInit(): void {
+    this.prepareForm();
+  }
+
+  prepareForm() {
+    this.managementForm = this.fb.group({
+      noVolante: [null, [Validators.required]],
+      noExpediente: [null, [Validators.required]],
+      tipoOficio: [null],
+      relacionado: [null],
+      numero: [null],
+      noRemitente: [null],
+      remitente: [null],
+      noDestinatario: [null],
+      destinatario: [null],
+      noCiudad: [null],
+      ciudad: [null],
+      claveOficio: [null],
+      parrafoInicial: [null],
+      subtipo: [null],
+      indPDoctos: [null],
+      noBienes: [null],
+      bienes: [null],
+      noBienes2: [null],
+      bienes2: [null],
+      noDocumento: [null],
+      documento: [null],
+      noDocumento2: [null],
+      documento2: [null],
+      parrafoFinal: [null],
+      parrafoAusencia: [null],
+      ccp: [null],
+      ccp2: [null],
+      ccp3: [null],
+      ccp4: [null],
+      ccp5: [null],
+      ccp6: [null],
+    });
+  }
+}
