61c2a3e8b0d7d41e92c79c17708de3b1e7252706
AbnerMendoza <abnerjmendoza@gmail.com>
2022-10-13 15:17:39 -0400

    global configurations

--- src/app/pages/executive-processes/pe-ad-m-authorization-assets-destruction/pe-ad-c-authorization-assets-destruction/pe-ad-c-authorization-assets-destruction.component.ts
+++ src/app/pages/executive-processes/pe-ad-m-authorization-assets-destruction/pe-ad-c-authorization-assets-destruction/pe-ad-c-authorization-assets-destruction.component.ts
@@ -1,28 +1,25 @@
-import { Component, OnInit } from '@angular/core'; 
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BasePage } from 'src/app/core/shared/base-page';
 
 import { NUMBERS_PATTERN } from 'src/app/core/shared/patterns';
-import { TABLE_SETTINGS } from 'src/app/common/constants/table-settings';
 import { ASSETS_DESTRUCTION_COLUMLNS } from './authorization-assets-destruction-columns';
 
 @Component({
   selector: 'app-pe-ad-c-authorization-assets-destruction',
   templateUrl: './pe-ad-c-authorization-assets-destruction.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PeAdCAuthorizationAssetsDestructionComponent extends BasePage implements OnInit {
-
-  settings = {
-    ...TABLE_SETTINGS,
-    actions: false
-  };
-  form: FormGroup = new FormGroup({}); 
+export class PeAdCAuthorizationAssetsDestructionComponent
+  extends BasePage
+  implements OnInit
+{
+  form: FormGroup = new FormGroup({});
 
   constructor(private fb: FormBuilder) {
     super();
     this.settings.columns = ASSETS_DESTRUCTION_COLUMLNS;
+    this.settings.actions=false;
   }
 
   ngOnInit(): void {
@@ -31,12 +28,28 @@ export class PeAdCAuthorizationAssetsDestructionComponent extends BasePage imple
 
   private prepareForm() {
     this.form = this.fb.group({
-      idExp: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(1), Validators.pattern(NUMBERS_PATTERN)]],
+      idExp: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(10),
+          Validators.minLength(1),
+          Validators.pattern(NUMBERS_PATTERN),
+        ],
+      ],
       preInquiry: ['', [Validators.required]],
       criminalCase: ['', [Validators.required]],
       circumstAct: ['', [Validators.required]],
       touchPenalty: ['', [Validators.required]],
-      noAuth: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(1), Validators.pattern(NUMBERS_PATTERN)]],
+      noAuth: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(10),
+          Validators.minLength(1),
+          Validators.pattern(NUMBERS_PATTERN),
+        ],
+      ],
       authNotice: ['', [Validators.required]],
       fromDate: ['', [Validators.required]],
       scanFolio: ['', [Validators.required]],
@@ -47,35 +60,33 @@ export class PeAdCAuthorizationAssetsDestructionComponent extends BasePage imple
   data = [
     {
       noBien: 1448,
-      description: "CUARENTA Y DOS CHAMARRAS",
-      ubiExact: "ALMACEN",
-      direction: "PROLONGACIÓN MORELOS",
-      noOficio: "DG/006/2004",
-      fecha: "12/12/2005",
-      status: "ADE",
-      extraDom: "DECOMISO",
+      description: 'CUARENTA Y DOS CHAMARRAS',
+      ubiExact: 'ALMACEN',
+      direction: 'PROLONGACIÓN MORELOS',
+      noOficio: 'DG/006/2004',
+      fecha: '12/12/2005',
+      status: 'ADE',
+      extraDom: 'DECOMISO',
     },
     {
       noBien: 1449,
-      description: "SETENTA Y DOS CELULARES",
-      ubiExact: "ALMACEN",
-      direction: "PROLONGACIÓN MORELOS",
-      noOficio: "DG/006/2004",
-      fecha: "12/12/2005",
-      status: "ADE",
-      extraDom: "DECOMISO",
+      description: 'SETENTA Y DOS CELULARES',
+      ubiExact: 'ALMACEN',
+      direction: 'PROLONGACIÓN MORELOS',
+      noOficio: 'DG/006/2004',
+      fecha: '12/12/2005',
+      status: 'ADE',
+      extraDom: 'DECOMISO',
     },
     {
       noBien: 1450,
-      description: "CUARENTA Y TRES CABLES USB",
-      ubiExact: "ALMACEN",
-      direction: "PROLONGACIÓN MORELOS",
-      noOficio: "DG/006/2004",
-      fecha: "12/12/2005",
-      status: "ADE",
-      extraDom: "DECOMISO",
+      description: 'CUARENTA Y TRES CABLES USB',
+      ubiExact: 'ALMACEN',
+      direction: 'PROLONGACIÓN MORELOS',
+      noOficio: 'DG/006/2004',
+      fecha: '12/12/2005',
+      status: 'ADE',
+      extraDom: 'DECOMISO',
     },
-
   ];
-
 }
