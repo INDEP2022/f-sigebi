17841b00c13828515ec49f1d1675e5024c59f429
Felix Mamani <felixmamanicala@gmail.com>
2022-10-14 18:50:19 -0400

    FESTPROCESOS_0001, FESTSERVICIOS_0001,  FESTTIPOSERVICIO_0001,  FESTTURNO_TIPO_0001,  FESTUNIDAD_MEDIDA_0001, FESTVARIABLE_COSTO_0001

---
+++ src/app/pages/administrative-processes/administration-third/services-unit-prices/services-unit-prices/services-unit-prices.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import { BasePage } from 'src/app/core/shared/base-page';
+import { SERVICEUNITPRECES_COLUMNS } from './service-unit-preces-columns';
+
+@Component({
+  selector: 'app-services-unit-prices',
+  templateUrl: './services-unit-prices.component.html',
+  styles: [
+  ]
+})
+export class ServicesUnitPricesComponent extends BasePage implements OnInit {
+  data1: any[] = [];
+  params = new BehaviorSubject<ListParams>(new ListParams());
+  totalItems: number = 0;
+  constructor()
+  {
+    super();
+    this.settings = {
+      ...this.settings,
+      actions: false,
+      columns: SERVICEUNITPRECES_COLUMNS,
+    };
+  }
+
+  ngOnInit(): void {
+  }
+}
