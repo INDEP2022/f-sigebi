f1e5ae80fb524772cd07a7fde980f2fe9520928e
Henry Giovanni Sanchez Solis <sshenrygiovanni@gmail.com>
2022-10-17 10:20:58 -0500

    cambiando las rutas a un archivo de procesos ejecutivos

--- src/app/core/menu.ts
+++ src/app/core/menu.ts
@@ -2,6 +2,7 @@ import { DOCUMENTS_RECEPTION_ROUTES } from '../common/routes/documents-reception
 import { menuOptionsJuridicalProcesses } from '../common/constants/juridical-processes/juridical-processes-nombres-rutas-archivos';
 import { IMenuItem } from './interfaces/menu.interface';
 import { CATALOGS_ROUTES } from '../common/routes/catalogs.routes';
+import { EXECUTIVE_PROCESSES_ROUTES } from '../common/routes/executive-processes.routes';
 
 export const MENU: IMenuItem[] = [
   {
@@ -352,79 +353,7 @@ export const MENU: IMenuItem[] = [
   },
   ...DOCUMENTS_RECEPTION_ROUTES,
   //Procesos ejecutivos
-  {
-    label: 'Procesos Ejecutivos',
-    icon: 'bx-folder',
-    subItems: [
-      {
-        label: 'Acumulado Anual de Bienes',
-        link: '/pages/executive-processes/pe-aab-m-annual-accumulated-assets',
-      },
-      {
-        label: 'Acumulado Trimestral de Bienes',
-        link: '/pages/executive-processes/pe-atb-m-quarterly-accumulated-assets',
-      },
-      {
-        label: 'Aprobación destrucción',
-        link: '/pages/executive-processes/pe-ad-m-authorization-assets-destruction',
-      },
-      {
-        label: 'Aprobación donación',
-        link: '/pages/executive-processes/pe-ad-m-donation-approval',
-      },
-      {
-        label: 'Aprobación destino',
-        link: '/pages/executive-processes/pe-ad-m-approval-assets-destination',
-      },
-      {
-        label: 'Control Mensual de Recepción Documental',
-        link: '/pages/executive-processes/pe-cmrd-m-cumulative-goods',
-      },
-      {
-        label: 'Información Bienes Asegurados/Decomisos/Abandonos',
-        link: '/pages/executive-processes/pe-ibs-d-a-m-report-registration-module',
-      },
-      {
-        label: 'Recepción de Doctos. x Destino en el SERA',
-        link: '/pages/executive-processes/pe-rddxdees-m-reception-area-sera',
-      },
-      {
-        label: 'Recepción de Doctos. Por Autoridad Emisora',
-        link: '/pages/executive-processes/pe-drpae-m-doc-received-authority',
-      },
-      {
-        label: 'Recepción Diaria de Expedientes',
-        link: '/pages/executive-processes/pe-rdde-m-daily-control-reception',
-      },
-      {
-        label: 'Reportes del Director Gral',
-        icon: 'bx-folder',
-        subItems: [
-          {
-            label: 'Documentación recibida X Área Destino',
-            link: '/pages/executive-processes/pe-rddg-drpad-m-totaldoc-received-destinationarea',
-          },
-          {
-            label: 'Total de Documentación Recibida',
-            link: '/pages/executive-processes/pe-rddg-tddr-m-report-doc-received',
-          },
-          {
-            label: 'Bienes recibidos en Administración',
-            link: '/pages/executive-processes/pe-rddg-brea-m-assets-received-admon',
-          },
-        ],
-      },
-      {
-        label: 'Actualización masiva de Valor de Avalúo',
-        link: '/pages/executive-processes/pe-amdvda-m-update-mss-value',
-      },
-      {
-        label: 'Gestión de Autorización de Destrucción',
-        link: '/pages/executive-processes/pe-gdadd-m-destruction-authorization-management',
-      },
-      
-    ],
-  },
+  ...EXECUTIVE_PROCESSES_ROUTES,
   //Comercialización
   {
     label: 'Comercialización',
