62cdd5f970d1e45f99303b3fa5bf71b3233de98d
Raul Legaspi <rul130394@gmail.com>
2022-10-27 09:35:13 -0500

    SIG-172: FALTAALMCONTRATO - Alta de almacenes por contrato

--- src/app/pages/administrative-processes/third-party-admin/warehouse/pa-tp-w-m-warehouse.module.ts
+++ src/app/pages/administrative-processes/third-party-admin/warehouse/pa-tp-w-m-warehouse.module.ts
@@ -1,17 +1,38 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+//Shared
+import { SharedModule } from 'src/app/shared/shared.module';
+//Reactive Forms
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//Ngx Bootstrap
+import { ModalModule } from 'ngx-bootstrap/modal';
+//Ng Apexcharts
+import { NgApexchartsModule } from 'ng-apexcharts';
+//@Standalone Components
+import { WarehouseSharedComponent } from 'src/app/@standalone/shared-forms/warehouse-shared/warehouse-shared.component';
+import { ContractsSharedComponent } from 'src/app/@standalone/shared-forms/contracts-shared/contracts-shared.component';
+//Routing
 import { PaTpWMWarehouseRoutingModule } from './pa-tp-w-m-warehouse-routing.module';
+//Components
 import { PaRwcCRegWarehouseContractComponent } from './reg-warehouse-contract/pa-rwc-c-reg-warehouse-contract.component';
+import { PaPdCProceedingsDetailsComponent } from './proceedings-details/pa-pd-c-proceedings-details.component';
 
 
 @NgModule({
   declarations: [
-    PaRwcCRegWarehouseContractComponent
+    PaRwcCRegWarehouseContractComponent,
+    PaPdCProceedingsDetailsComponent
   ],
   imports: [
     CommonModule,
-    PaTpWMWarehouseRoutingModule
+    PaTpWMWarehouseRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
+    WarehouseSharedComponent,
+    ContractsSharedComponent,
+    NgApexchartsModule,
+    ModalModule.forChild(),
   ]
 })
 export class PaTpWMWarehouseModule { }
