10f6f3bf086104675818f4080396bc0e7f351ce7
unknown <Administrador@ITRASIG.ORG>
2022-10-27 06:17:17 -0500

    Ajuste de menu en depositarias de juridicos

--- src/app/common/constants/juridical-processes/juridical-processes-nombres-rutas-archivos.ts
+++ src/app/common/constants/juridical-processes/juridical-processes-nombres-rutas-archivos.ts
@@ -1,3 +1,5 @@
+import { CREAR_MENU_DEPOSITARY } from './depositary-routes';
+
 export const baseMenu: string = '/pages/juridicos/'; // Base url Menu
 export const baseMenuDepositaria: string = 'depositaria/'; // Base url Menu Depositaria
 export const baseMenuProcesoDispercionPagos: string =
@@ -28,123 +30,123 @@ export const routesJuridicalProcesses: any = [
     link: 'abandonos',
     menu: 'Abandonos',
   },
-  // DEPOSITARIA
-  {
-    // REGISTRO DE DEPOSITARIA
-    label: 'Nombranientos',
-    link: 'registro-depositaria',
-    menu: 'Registro de Depositaría',
-  },
-  {
-    // CONCILIACIÓN DE DISPERCIÓN DE PAGOS
-    label: 'Conciliación de Pagos Depositarias',
-    link: 'conciliacion-pagos-depositaria',
-    menu: 'Proceso de Disperción de Pagos',
-  },
-  {
-    // VALIDACIÓN DE PAGOS
-    label: 'Consulta de Pagos Relacionados Depositarias',
-    link: 'consulta-pagos-relacionados-depositaria',
-    menu: 'Validación de Pagos',
-  },
-  {
-    // SOLICITUD/MONITOR DE DEPOSITARIA
-    label: 'Solicitud de Destino Legal para el Bien',
-    link: 'solicitud-monitor-depositaria',
-    menu: 'Solicitud/Monitor de Depositaría',
-  },
-  {
-    // REPORTE DE CÉDULAS DE NOMBRAMIENTO
-    label: 'Reporte de Cédulas de Nombramiento',
-    link: 'reporte-cedulas-nombramiento',
-    menu: 'Reporte de Cédulas de Nombramiento',
-  },
-  {
-    // REPORTE DE BIENES POR DEPOSITARIA
-    label: 'Bienes por Depositaría',
-    link: 'reporte-bienes-depositarias',
-    menu: 'Reporte de Bienes por Depositaría',
-  },
-  {
-    // RELACIÓN DE AMPAROS
-    label: 'Asignación de Bienes por Amparo',
-    link: 'asignacion-bienes-amparo',
-    menu: 'Relación de Amparos',
-  },
-  {
-    // EMISIÓN DE ACUERDOS
-    label: 'Emisión de Acuerdos',
-    link: 'emision-acuerdos',
-    menu: 'Emisión de Acuerdos',
-  },
-  {
-    // NO SE IMPORTA EN EL MENU
-    //HISTORICO SITUACION DEL BIEN
-    label: 'Histórico Situación del Bien',
-    link: 'historico-situacion-bien',
-    menu: 'Histórico Situación del Bien',
-  }, // NO SE IMPORTA EN EL MENU
-  {
-    // Resolución de Recursos de Revisión
-    label: 'Resolución de Recursos de Revisión',
-    link: 'relacion-recursos-revision',
-    menu: 'Resolución de Recursos de Revisión',
-  },
-  {
-    // Comprobación de Documentos para Recursos Revision
-    label: 'Comprobación de Documentos para Recursos Revision',
-    link: 'comprobacion-documentos-recursos-revision',
-    menu: 'Comprobación de Doc. para Recursos Revision',
-  },
-  {
-    // Reporte de Recursos de Revisión
-    label: 'Reporte de Recursos de Revisión',
-    link: 'reporte-recursos-revision',
-    menu: 'Reporte de Recursos de Revisión',
-  },
-  {
-    // Notificaciones por Expediente
-    label: 'Notificaciones por Expediente',
-    link: 'notificacion-expediente',
-    menu: 'Notificaciones por Expediente',
-  },
-  {
-    // Dictaminación Masiva Prog. Desalojo
-    label: 'Dictaminación Masiva',
-    link: 'dictaminacion-masiva-prog-desalojo',
-    menu: 'Dictaminación Masiva Prog. Desalojo',
-  },
-  {
-    // Dictaminación para devolución
-    label: 'Dictaminación para devolución',
-    link: 'return-ruling',
-    menu: 'Dictaminación para devolución',
-  },
-  {
-    // Comprobación de Documentos para Recursos Revision
-    label: 'Oficio de dictaminaciones jurídicas',
-    link: 'legal-opinions-office',
-    menu: 'Oficio de dictaminaciones jurídicas',
-  },
-  {
-    // Comprobación de Documentos para Recursos Revision
-    label: 'Juicios',
-    link: 'trials',
-    menu: 'Juicios',
-  },
-  {
-    // ESPACIO EN BLANCO
-    label: '',
-    link: '',
-    menu: '',
-  },
-  {
-    // Validación de Procesión a Terceros
-    label: 'Validación de Procesión a Terceros',
-    link: 'validacion-pocesion-terceros',
-    menu: 'Validación de Procesión',
-  },
-  // DEPOSITARIA
+  // // DEPOSITARIA
+  // {
+  //   // REGISTRO DE DEPOSITARIA
+  //   label: 'Nombranientos',
+  //   link: 'registro-depositaria',
+  //   menu: 'Registro de Depositaría',
+  // },
+  // {
+  //   // CONCILIACIÓN DE DISPERCIÓN DE PAGOS
+  //   label: 'Conciliación de Pagos Depositarias',
+  //   link: 'conciliacion-pagos-depositaria',
+  //   menu: 'Proceso de Disperción de Pagos',
+  // },
+  // {
+  //   // VALIDACIÓN DE PAGOS
+  //   label: 'Consulta de Pagos Relacionados Depositarias',
+  //   link: 'consulta-pagos-relacionados-depositaria',
+  //   menu: 'Validación de Pagos',
+  // },
+  // {
+  //   // SOLICITUD/MONITOR DE DEPOSITARIA
+  //   label: 'Solicitud de Destino Legal para el Bien',
+  //   link: 'solicitud-monitor-depositaria',
+  //   menu: 'Solicitud/Monitor de Depositaría',
+  // },
+  // {
+  //   // REPORTE DE CÉDULAS DE NOMBRAMIENTO
+  //   label: 'Reporte de Cédulas de Nombramiento',
+  //   link: 'reporte-cedulas-nombramiento',
+  //   menu: 'Reporte de Cédulas de Nombramiento',
+  // },
+  // {
+  //   // REPORTE DE BIENES POR DEPOSITARIA
+  //   label: 'Bienes por Depositaría',
+  //   link: 'reporte-bienes-depositarias',
+  //   menu: 'Reporte de Bienes por Depositaría',
+  // },
+  // {
+  //   // RELACIÓN DE AMPAROS
+  //   label: 'Asignación de Bienes por Amparo',
+  //   link: 'asignacion-bienes-amparo',
+  //   menu: 'Relación de Amparos',
+  // },
+  // {
+  //   // EMISIÓN DE ACUERDOS
+  //   label: 'Emisión de Acuerdos',
+  //   link: 'emision-acuerdos',
+  //   menu: 'Emisión de Acuerdos',
+  // },
+  // {
+  //   // NO SE IMPORTA EN EL MENU
+  //   //HISTORICO SITUACION DEL BIEN
+  //   label: 'Histórico Situación del Bien',
+  //   link: 'historico-situacion-bien',
+  //   menu: 'Histórico Situación del Bien',
+  // }, // NO SE IMPORTA EN EL MENU
+  // {
+  //   // Resolución de Recursos de Revisión
+  //   label: 'Resolución de Recursos de Revisión',
+  //   link: 'relacion-recursos-revision',
+  //   menu: 'Resolución de Recursos de Revisión',
+  // },
+  // {
+  //   // Comprobación de Documentos para Recursos Revision
+  //   label: 'Comprobación de Documentos para Recursos Revision',
+  //   link: 'comprobacion-documentos-recursos-revision',
+  //   menu: 'Comprobación de Doc. para Recursos Revision',
+  // },
+  // {
+  //   // Reporte de Recursos de Revisión
+  //   label: 'Reporte de Recursos de Revisión',
+  //   link: 'reporte-recursos-revision',
+  //   menu: 'Reporte de Recursos de Revisión',
+  // },
+  // {
+  //   // Notificaciones por Expediente
+  //   label: 'Notificaciones por Expediente',
+  //   link: 'notificacion-expediente',
+  //   menu: 'Notificaciones por Expediente',
+  // },
+  // {
+  //   // Dictaminación Masiva Prog. Desalojo
+  //   label: 'Dictaminación Masiva',
+  //   link: 'dictaminacion-masiva-prog-desalojo',
+  //   menu: 'Dictaminación Masiva Prog. Desalojo',
+  // },
+  // {
+  //   // Dictaminación para devolución
+  //   label: 'Dictaminación para devolución',
+  //   link: 'return-ruling',
+  //   menu: 'Dictaminación para devolución',
+  // },
+  // {
+  //   // Comprobación de Documentos para Recursos Revision
+  //   label: 'Oficio de dictaminaciones jurídicas',
+  //   link: 'legal-opinions-office',
+  //   menu: 'Oficio de dictaminaciones jurídicas',
+  // },
+  // {
+  //   // Comprobación de Documentos para Recursos Revision
+  //   label: 'Juicios',
+  //   link: 'trials',
+  //   menu: 'Juicios',
+  // },
+  // {
+  //   // ESPACIO EN BLANCO
+  //   label: '',
+  //   link: '',
+  //   menu: '',
+  // },
+  // {
+  //   // Validación de Procesión a Terceros
+  //   label: 'Validación de Procesión a Terceros',
+  //   link: 'validacion-pocesion-terceros',
+  //   menu: 'Validación de Procesión',
+  // },
+  // // DEPOSITARIA
 ];
 export const MENU_OPTIONS_JURIDICAL_PROCESSES = [
   // PROCESOS JURIDICOS
@@ -174,148 +176,149 @@ export const MENU_OPTIONS_JURIDICAL_PROCESSES = [
       },
       {
         label: 'Depositaría',
-        subItems: [
-          {
-            // REGISTRO DE DEPOSITARIA
-            label: routesJuridicalProcesses[4].menu,
-            link:
-              baseMenu + baseMenuDepositaria + routesJuridicalProcesses[4].link,
-          },
-          // Proceso de Dispersión de Pagos
-          {
-            // CONCILIACIÓN DE DISPERCIÓN DE PAGOS
-            label: routesJuridicalProcesses[5].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              baseMenuProcesoDispercionPagos +
-              routesJuridicalProcesses[5].link,
-          },
-          {
-            // VALIDACIÓN DE PAGOS
-            label: routesJuridicalProcesses[6].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              baseMenuProcesoDispercionPagos +
-              routesJuridicalProcesses[6].link,
-          },
-          // Proceso de Dispersión de Pagos
-          {
-            // SOLICITUD/MONITOR DE DEPOSITARIA
-            label: routesJuridicalProcesses[7].menu,
-            link:
-              baseMenu + baseMenuDepositaria + routesJuridicalProcesses[7].link,
-          },
-          {
-            // REPORTE DE CÉDULAS DE NOMBRAMIENTO
-            label: routesJuridicalProcesses[8].menu,
-            link:
-              baseMenu + baseMenuDepositaria + routesJuridicalProcesses[8].link,
-          },
-          {
-            // REPORTE DE BIENES POR DEPOSITARIA
-            label: routesJuridicalProcesses[9].menu,
-            link:
-              baseMenu + baseMenuDepositaria + routesJuridicalProcesses[9].link,
-          },
-          {
-            // RELACIÓN DE AMPAROS
-            label: routesJuridicalProcesses[10].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[10].link,
-          },
-          {
-            // RELACIÓN DE AMPAROS
-            label: routesJuridicalProcesses[11].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[11].link,
-          },
-          {
-            // Resolución de Recursos de Revisión
-            label: routesJuridicalProcesses[13].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[13].link,
-          },
-          {
-            // Resolución de Recursos de Revisión
-            label: routesJuridicalProcesses[14].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[14].link,
-          },
-          {
-            // Reporte de Recursos de Revisión
-            label: routesJuridicalProcesses[15].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[15].link,
-          },
-          {
-            // Notificación por Expediente
-            label: routesJuridicalProcesses[16].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[16].link,
-          },
-          {
-            // Dictaminación Masiva Prog. Desalojo
-            label: routesJuridicalProcesses[17].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[17].link,
-          },
-          {
-            // Dictaminación para devolución
-            label: routesJuridicalProcesses[18].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[18].link,
-          },
-          {
-            // Comprobación de Documentos para Recursos Revision
-            label: routesJuridicalProcesses[19].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[19].link,
-          },
-          {
-            // Juicios
-            label: routesJuridicalProcesses[20].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[20].link,
-          },
-          {
-            // ESPACIO EN BLANCO
-            label: routesJuridicalProcesses[21].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[21].link,
-          },
-          {
-            // Validación de Procesión a Terceros
-            label: routesJuridicalProcesses[22].menu,
-            link:
-              baseMenu +
-              baseMenuDepositaria +
-              routesJuridicalProcesses[22].link,
-          },
-        ],
+        subItems: CREAR_MENU_DEPOSITARY(),
+        // subItems: [
+        //   {
+        //     // REGISTRO DE DEPOSITARIA
+        //     label: routesJuridicalProcesses[4].menu,
+        //     link:
+        //       baseMenu + baseMenuDepositaria + routesJuridicalProcesses[4].link,
+        //   },
+        //   // Proceso de Dispersión de Pagos
+        //   {
+        //     // CONCILIACIÓN DE DISPERCIÓN DE PAGOS
+        //     label: routesJuridicalProcesses[5].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       baseMenuProcesoDispercionPagos +
+        //       routesJuridicalProcesses[5].link,
+        //   },
+        //   {
+        //     // VALIDACIÓN DE PAGOS
+        //     label: routesJuridicalProcesses[6].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       baseMenuProcesoDispercionPagos +
+        //       routesJuridicalProcesses[6].link,
+        //   },
+        //   // Proceso de Dispersión de Pagos
+        //   {
+        //     // SOLICITUD/MONITOR DE DEPOSITARIA
+        //     label: routesJuridicalProcesses[7].menu,
+        //     link:
+        //       baseMenu + baseMenuDepositaria + routesJuridicalProcesses[7].link,
+        //   },
+        //   {
+        //     // REPORTE DE CÉDULAS DE NOMBRAMIENTO
+        //     label: routesJuridicalProcesses[8].menu,
+        //     link:
+        //       baseMenu + baseMenuDepositaria + routesJuridicalProcesses[8].link,
+        //   },
+        //   {
+        //     // REPORTE DE BIENES POR DEPOSITARIA
+        //     label: routesJuridicalProcesses[9].menu,
+        //     link:
+        //       baseMenu + baseMenuDepositaria + routesJuridicalProcesses[9].link,
+        //   },
+        //   {
+        //     // RELACIÓN DE AMPAROS
+        //     label: routesJuridicalProcesses[10].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[10].link,
+        //   },
+        //   {
+        //     // RELACIÓN DE AMPAROS
+        //     label: routesJuridicalProcesses[11].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[11].link,
+        //   },
+        //   {
+        //     // Resolución de Recursos de Revisión
+        //     label: routesJuridicalProcesses[13].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[13].link,
+        //   },
+        //   {
+        //     // Resolución de Recursos de Revisión
+        //     label: routesJuridicalProcesses[14].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[14].link,
+        //   },
+        //   {
+        //     // Reporte de Recursos de Revisión
+        //     label: routesJuridicalProcesses[15].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[15].link,
+        //   },
+        //   {
+        //     // Notificación por Expediente
+        //     label: routesJuridicalProcesses[16].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[16].link,
+        //   },
+        //   {
+        //     // Dictaminación Masiva Prog. Desalojo
+        //     label: routesJuridicalProcesses[17].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[17].link,
+        //   },
+        //   {
+        //     // Dictaminación para devolución
+        //     label: routesJuridicalProcesses[18].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[18].link,
+        //   },
+        //   {
+        //     // Comprobación de Documentos para Recursos Revision
+        //     label: routesJuridicalProcesses[19].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[19].link,
+        //   },
+        //   {
+        //     // Juicios
+        //     label: routesJuridicalProcesses[20].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[20].link,
+        //   },
+        //   {
+        //     // ESPACIO EN BLANCO
+        //     label: routesJuridicalProcesses[21].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[21].link,
+        //   },
+        //   {
+        //     // Validación de Procesión a Terceros
+        //     label: routesJuridicalProcesses[22].menu,
+        //     link:
+        //       baseMenu +
+        //       baseMenuDepositaria +
+        //       routesJuridicalProcesses[22].link,
+        //   },
+        // ],
       },
     ],
   },
