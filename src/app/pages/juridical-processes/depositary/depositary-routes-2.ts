85432cf32cd38b01b3e6263c1ac96263ce6501cf
aalvarezg1998 <107310910+aalvarezg1998@users.noreply.github.com>
2022-10-28 16:27:04 -0500

    commit

--- src/app/pages/juridical-processes/depositary/depositary-routes-2.ts
+++ src/app/pages/juridical-processes/depositary/depositary-routes-2.ts
@@ -3,13 +3,6 @@ import { DEPOSITARY_ROUTES_2 } from 'src/app/common/constants/juridical-processe
 export const DEPOSITARY_ROUTES_2_ROUTING = [
   // DEPOSITARIA
   {
-    path: DEPOSITARY_ROUTES_2[0].link,
-    loadChildren: async () =>
-      (await import('./return-ruling/jp-d-m-return-ruling.module'))
-        .JpDMReturnRulingModule,
-    data: { title: DEPOSITARY_ROUTES_2[0].label },
-  },
-  {
     path: DEPOSITARY_ROUTES_2[1].link,
     loadChildren: async () =>
       (
@@ -19,20 +12,22 @@ export const DEPOSITARY_ROUTES_2_ROUTING = [
       ).JpDMLegalOpinionsOfficeModule,
     data: { title: DEPOSITARY_ROUTES_2[1].label },
   },
+
   {
     path: DEPOSITARY_ROUTES_2[2].link,
-    loadChildren: async () =>
-      (await import('./trials/jp-d-m-trials.module')).JpDMTrialsModule,
-    data: { title: DEPOSITARY_ROUTES_2[2].label },
-  },
-  {
-    path: DEPOSITARY_ROUTES_2[3].link,
     loadChildren: async () =>
       (
         await import(
           './notice-of-abandonment-by-return/jp-d-m-notice-of-abandonment-by-return.module'
         )
       ).JpDMNoticeOfAbandonmentByReturnModule,
+    data: { title: DEPOSITARY_ROUTES_2[2].label },
+  },
+  {
+    path: DEPOSITARY_ROUTES_2[3].link,
+    loadChildren: async () =>
+      (await import('./depository-fees/jp-d-m-depository-fees.module'))
+        .JpDMDepositoryFeesModule,
     data: { title: DEPOSITARY_ROUTES_2[3].label },
   },
 ];
---
+++ src/app/pages/juridical-processes/depositary/depository-fees/jp-d-df-c-depository-fees/jp-d-df-c-depository-fees.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jp-d-df-c-depository-fees',
+  templateUrl: './jp-d-df-c-depository-fees.component.html',
+  styles: [
+  ]
+})
+export class JpDDfCDepositoryFeesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
