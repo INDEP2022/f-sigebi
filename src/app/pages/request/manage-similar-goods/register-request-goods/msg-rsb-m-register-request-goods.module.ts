c5dc3077eda4e9fafe18a200973e64add57a0a88
jjpu2111 <julian21931500@gmail.com>
2022-11-11 15:53:31 -0400

    associate-request-to-new-and-existing-file

--- src/app/pages/request/manage-similar-goods/register-request-goods/msg-rsb-m-register-request-goods.module.ts
+++ src/app/pages/request/manage-similar-goods/register-request-goods/msg-rsb-m-register-request-goods.module.ts
@@ -5,12 +5,16 @@ import { MsgRsbMRegisterRequestGoodsRoutingModule } from './msg-rsb-m-register-r
 import { MsgRsbCRegisterRequestGoodsComponent } from './register-request-goods/msg-rsb-c-register-request-goods.component';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { RegisterDocumentationComponent } from './tabs/register-documentation/register-documentation.component';
+import { AssociateExistingFileRequestComponent } from './tabs/associate-existing-file-request/associate-existing-file-request.component';
+import { ModalNewCoverComponent } from './tabs/modal-new-cover/modal-new-cover.component';
 
 
 @NgModule({
   declarations: [
     MsgRsbCRegisterRequestGoodsComponent,
-    RegisterDocumentationComponent
+    RegisterDocumentationComponent,
+    AssociateExistingFileRequestComponent,
+    ModalNewCoverComponent
   ],
   imports: [
     CommonModule,
