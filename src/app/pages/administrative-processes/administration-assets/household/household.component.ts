ce4223cccba05966f1af854f07b246ae788253b2
AbnerMendoza <abnerjmendoza@gmail.com>
2022-11-10 22:40:03 -0400

    administrative assets

--- src/app/pages/administrative-processes/administration-assets/household/household.component.ts
+++ src/app/pages/administrative-processes/administration-assets/household/household.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { BasePage } from 'src/app/core/shared/base-page';
 
 @Component({
   selector: 'app-household',
   templateUrl: './household.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class HouseholdComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HouseholdComponent extends BasePage implements OnInit {
+  list: any[] = [];
+  constructor() {
+    super();
+    this.settings.columns = {
+      noGood: {
+        title: 'No Bien',
+        type: 'number',
+        sort: false,
+      },
+      description: {
+        title: 'Descripcion',
+        type: 'string',
+        sort: false,
+      },
+    };
   }
 
+  ngOnInit(): void {}
 }
--- src/app/pages/administrative-processes/administration-assets/secure-data/secure-data.component.ts
+++ src/app/pages/administrative-processes/administration-assets/secure-data/secure-data.component.ts
@@ -1,16 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { BasePage } from 'src/app/core/shared/base-page';
 
 @Component({
   selector: 'app-secure-data',
   templateUrl: './secure-data.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SecureDataComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class SecureDataComponent extends BasePage implements OnInit {
+  list: any[] = [];
+  constructor() {
+    super();
+    this.settings.columns = {
+      policy: {
+        title: 'Póliza',
+        type: 'number',
+        sort: false,
+      },
+      policyDescription: {
+        title: 'Descripion de Póliza',
+        type: 'string',
+        sort: false,
+      },
+      insuranceCarrier: {
+        title: 'Aseguradora',
+        type: 'string',
+        sort: false,
+      },
+      entryDate: {
+        title: 'Fecha Ingreso',
+        type: 'string',
+        sort: false,
+      },
+      lowDate: {
+        title: 'Fecha Baja',
+        type: 'string',
+        sort: false,
+      },
+      amountInsured: {
+        title: 'Suma Asegurada',
+        type: 'string',
+        sort: false,
+      },
+      premiumAmount: {
+        title: 'Monto Prima',
+        type: 'string',
+        sort: false,
+      },
+    };
   }
 
+  ngOnInit(): void {}
 }
