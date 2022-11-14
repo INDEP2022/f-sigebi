4da5f82e97ed54731f5b4052c2ef34c494b3e62f
david <david.lucas@umvel.com>
2022-11-13 20:38:54 -0600

    feat: adds numerary screens and routing

---
+++ src/app/pages/administrative-processes/numerary/deposit-consiliation-tokens/deposit-consiliation-tokens/deposit-conciliations-columns.ts
@@ -0,0 +1,55 @@
+export const NUMERARY_FILE = {
+  noBien: {
+    title: 'No Bien',
+    type: 'string',
+    sort: false,
+  },
+  bank: {
+    title: 'Banco',
+    type: 'string',
+    sort: false,
+  },
+  currency: {
+    title: 'Moneda',
+    type: 'string',
+    sort: false,
+  },
+  date: {
+    title: 'Fecha',
+    type: 'string',
+    sort: false,
+  },
+  import: {
+    title: 'Importe',
+    type: 'string',
+    sort: false,
+  },
+};
+
+export const DEPOSIT_TOKENS = {
+  noBien: {
+    title: 'No Bien',
+    type: 'number',
+    sort: false,
+  },
+  bank: {
+    title: 'Banco',
+    type: 'string',
+    sort: false,
+  },
+  currency: {
+    title: 'Moneda',
+    type: 'string',
+    sort: false,
+  },
+  date: {
+    title: 'Fecha',
+    type: 'string',
+    sort: false,
+  },
+  import: {
+    title: 'Importe',
+    type: 'string',
+    sort: false,
+  },
+};
---
+++ src/app/pages/administrative-processes/numerary/numerary.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numerary',
+  template: `<router-outlet></router-outlet>`,
+  styles: [],
+})
+export class NumeraryComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
