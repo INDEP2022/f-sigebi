dd33e68f5768c2699135e58a84c93524d2966fc7
Fernando <f3rfams@gmail.com>
2022-11-15 18:06:12 -0600

    Solicitudes de Lineamientos de Resarcimiento

--- src/app/pages/request/economic-compensation/gre-m-register-documentation/gre-m-register-documentation.module.ts
+++ src/app/pages/request/economic-compensation/gre-m-register-documentation/gre-m-register-documentation.module.ts
@@ -7,12 +7,12 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { SharedRequestModule } from '../../shared-request/shared-request.module';
 import { GreCRegisterDocumentationMainComponent } from './gre-c-register-documentation-main/gre-c-register-documentation-main.component';
 import { GreMRegisterDocumentationRoutingModule } from './gre-m-register-documentation-routing.module';
-import { RegisterDocumentationFormComponent } from './register-documentation-form/register-documentation-form.component';
+import { RegisterDocumentationInitialFormComponent } from './register-documentation-initial-form/register-documentation-initial-form.component';
 
 @NgModule({
   declarations: [
     GreCRegisterDocumentationMainComponent,
-    RegisterDocumentationFormComponent,
+    RegisterDocumentationInitialFormComponent,
   ],
   imports: [
     CommonModule,
--- src/app/pages/request/shared-request/shared-request.module.ts
+++ src/app/pages/request/shared-request/shared-request.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 import { NgScrollbarModule } from 'ngx-scrollbar';
@@ -8,7 +9,11 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { AssociateFileButtonComponent } from './associate-file/associate-file-button/associate-file-button.component';
 import { AssociateFileComponent } from './associate-file/associate-file.component';
 import { NewFileModalComponent } from './associate-file/new-file-modal/new-file-modal.component';
+import { GoodsListComponent } from './goods-list/goods-list.component';
+import { GuidelinesComponent } from './guidelines/guidelines.component';
+import { RegisterDocumentationFormComponent } from './register-documentation-form/register-documentation-form.component';
 import { RequestFormComponent } from './request-form/request-form.component';
+import { RequestInformationSimilarGoodsComponent } from './request-information-similar-goods/request-information-similar-goods.component';
 import { RequestInformationComponent } from './request-information/request-information.component';
 import { SearchInventoryGoodsComponent } from './search-inventory-goods/search-inventory-goods.component';
 import { SearchRequestsComponent } from './search-requests/search-requests.component';
@@ -17,6 +22,9 @@ import { ReserveGoodModalComponent } from './select-goods/reserve-good-modal/res
 import { SelectGoodsComponent } from './select-goods/select-goods.component';
 import { ViewFileButtonComponent } from './select-goods/view-file-button/view-file-button.component';
 import { UsersSelectedToTurnComponent } from './users-selected-to-turn/users-selected-to-turn.component';
+import { GuidelinesRevisionComponent } from './guidelines/guidelines-revision/guidelines-revision.component';
+import { GuidelinesObservationsComponent } from './guidelines/guidelines-observations/guidelines-observations.component';
+import { GuidelinesRevisionViewComponent } from './guidelines/guidelines-revision-view/guidelines-revision-view.component';
 
 @NgModule({
   declarations: [
@@ -33,6 +41,13 @@ import { UsersSelectedToTurnComponent } from './users-selected-to-turn/users-sel
     AddGoodsButtonComponent,
     ViewFileButtonComponent,
     ReserveGoodModalComponent,
+    RegisterDocumentationFormComponent,
+    GoodsListComponent,
+    GuidelinesComponent,
+    RequestInformationSimilarGoodsComponent,
+    GuidelinesRevisionComponent,
+    GuidelinesObservationsComponent,
+    GuidelinesRevisionViewComponent,
   ],
   imports: [
     CommonModule,
@@ -41,6 +56,7 @@ import { UsersSelectedToTurnComponent } from './users-selected-to-turn/users-sel
     SharedModule,
     NgScrollbarModule,
     CollapseModule.forRoot(),
+    BsDatepickerModule.forRoot(),
   ],
   exports: [
     RequestFormComponent,
@@ -48,6 +64,10 @@ import { UsersSelectedToTurnComponent } from './users-selected-to-turn/users-sel
     RequestInformationComponent,
     AssociateFileComponent,
     SelectGoodsComponent,
+    RegisterDocumentationFormComponent,
+    GoodsListComponent,
+    GuidelinesComponent,
+    RequestInformationSimilarGoodsComponent,
   ],
 })
 export class SharedRequestModule {}
