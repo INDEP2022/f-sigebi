72b1f8b41a2447b4d0036ff852661ab033452051
Isaias Perez <isapzmz@gmail.com>
2022-11-16 10:34:11 -0500

    cambios

---
+++ src/app/pages/judicial-physical-reception/jpr-scheduled-maintenance-2/jpr-scheduled-maintenance.component.ts
@@ -0,0 +1,116 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TABLE_SETTINGS } from 'src/app/common/constants/table-settings';
+import { DefaultSelect } from 'src/app/shared/components/select/default-select';
+
+@Component({
+  selector: 'app-jpr-scheduled-maintenance',
+  templateUrl: './jpr-scheduled-maintenance.component.html',
+  styleUrls: ["scheduled-maintenance.scss"],
+})
+export class JprScheduledMaintenanceComponent implements OnInit {
+
+  itemsSelect = new DefaultSelect();
+  form: FormGroup;
+  settings1 = {... TABLE_SETTINGS,
+    pager: {
+      display: false,
+    },
+    hideSubHeader: true,
+    actions: false,
+    selectedRowIndex: -1,
+    mode: "external",
+    columns: {
+      progRecepcionEntrega: {
+        title: "Programa Recepcion Entrega",
+        type: "string",
+        sort: false,
+      },
+      Fechacaptura: {
+        title: "Fecha Captura",
+        type: Date,
+        sort: false,
+      },
+      ingreso: {
+        title: "Ingreso",
+        type: "string",
+        sort: false,
+      },
+      estatusEvento: {
+        title: "Estatus Evento",
+        type: "number",
+        sort: false,
+      },
+    },
+    noDataMessage: "No se encontrarón registros",
+  };
+
+  data = EXAMPLE_DATA;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.prepareForm();
+  }
+
+  prepareForm() {
+    this.form = this.fb.group({
+      fechaCaptura: [null, [Validators.required]],
+      statusActa: [null, [Validators.required]],
+      acta: [null, [Validators.required]],
+      tipoEvento: [null, [Validators.required]],
+      tipo: [null, [Validators.required]],
+      prog: [null, [Validators.required]],
+      transferente: [null, [Validators.required]],
+      area: [null, [Validators.required]],
+      usuario: [null, [Validators.required]],
+      folio: [null, [Validators.required]],
+      year: [null, [Validators.required]],
+      month: [null, [Validators.required]],
+      claveActa: [null, [Validators.required]],
+      cantidadRegistros: [null, [Validators.required]],
+      cantidadBienes: [null, [Validators.required]],
+      cantidadExpedientes: [null, [Validators.required]],
+      cantidadDictamenes: [null, [Validators.required]],
+    });
+  }
+}
+
+const EXAMPLE_DATA = [
+  {
+    progRecepcionEntrega: "123",
+    Fechacaptura: new Date,
+    ingreso: "ejemplo",
+    estatusEvento: "ingresado",
+  },
+  {
+    progRecepcionEntrega: "123",
+    Fechacaptura: new Date,
+    ingreso: "ejemplo",
+    estatusEvento: "no ingresado",
+  },
+   {
+    progRecepcionEntrega: "123",
+    Fechacaptura: new Date,
+    ingreso: "ejemplo",
+    estatusEvento: "ingresado",
+  },
+  {
+    progRecepcionEntrega: "123",
+    Fechacaptura: new Date,
+    ingreso: "ejemplo",
+    estatusEvento: "no ingresado",
+  },
+   {
+    progRecepcionEntrega: "123",
+    Fechacaptura: new Date,
+    ingreso: "ejemplo",
+    estatusEvento: "ingresado",
+  },
+  {
+    progRecepcionEntrega: "123",
+    Fechacaptura: new Date,
+    ingreso: "ejemplo",
+    estatusEvento: "no ingresado",
+  },
+];
