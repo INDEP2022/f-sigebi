13bbcf79fbd90aad53f0a70e26465b140945f642
Felix Mamani <felixmamanicala@gmail.com>
2022-11-17 17:49:03 -0400

    FMTOPAQUETE_0001
    FMTOPAQUETE_0001

---
+++ src/app/pages/administrative-processes/unit-conversion-packages/massive-conversion-permissions/massive-conversion-permissions-columns.ts
@@ -0,0 +1,45 @@
+export const PERMISSIONSUSER_COLUMNS = {
+  user: {
+    title: 'Usuario',
+    sort: false,
+  },
+  name: {
+    title: 'Nombre',
+    sort: false,
+  },
+  s: {
+    title: 'S',
+    sort: false,
+  },
+  n: {
+    title: 'N',
+    sort: false,
+  },
+};
+export const PRIVILEGESUSER_COLUMNS = {
+  proy: {
+    title: 'PROY.',
+    width: '5%',
+    sort: false,
+  },
+  val: {
+    title: 'VAL.',
+    width: '5%',
+    sort: false,
+  },
+  aut: {
+    title: 'AUT.',
+    width: '5%',
+    sort: false,
+  },
+  cerr: {
+    title: 'CERR.',
+    width: '5%',
+    sort: false,
+  },
+  can: {
+    title: 'CAN.',
+    width: '5%',
+    sort: false,
+  },
+};
