5f36f1482d69bca8057e8ae6c852b2229ab2c307
Henry Giovanni Sanchez Solis <sshenrygiovanni@gmail.com>
2022-11-18 12:01:32 -0600

    pantalla de publicacion de fotografias agregada

--- src/app/pages/commercialization/c-pdf-m-publication-photographs/c-pdf-m-publication-photographs.module.ts
+++ src/app/pages/commercialization/c-pdf-m-publication-photographs/c-pdf-m-publication-photographs.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 import { CPdfCPublicationPhotographsComponent } from './c-pdf-c-publication-photographs/c-pdf-c-publication-photographs.component';
 import { CPdfMPublicationPhotographsRoutingModule } from './c-pdf-m-publication-photographs-routing.module';
 
@@ -11,6 +13,7 @@ import { CPdfMPublicationPhotographsRoutingModule } from './c-pdf-m-publication-
     CommonModule,
     CPdfMPublicationPhotographsRoutingModule,
     SharedModule,
+    CarouselModule.forRoot(),
   ],
 })
 export class CPdfMPublicationPhotographsModule {}
