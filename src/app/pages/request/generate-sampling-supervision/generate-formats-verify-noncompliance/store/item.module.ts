b61222557bd1505339bbb02768e1868d8ebf107b
Edson Percy Lopez Cortez <edson_lopez@hotmail.com.ar>
2022-11-25 14:13:32 -0400

    añadir ngrx a noncompliance

--- src/app/pages/request/generate-sampling-supervision/generate-formats-verify-noncompliance/store/item.module.ts
+++ src/app/pages/request/generate-sampling-supervision/generate-formats-verify-noncompliance/store/item.module.ts
@@ -1,3 +1,13 @@
 export interface Item {
-  item1: [];
+  ichecked: boolean;
+  description: string;
+  id: number;
+  noInventory: string;
+  noManagement: string;
+  observationsAsset: string;
+  quantity: string;
+  quantityMissingDamaged: string;
+  resultEvaluation: string;
+  statusAsset: string;
+  unity: string;
 }
--- src/app/pages/request/transfer-request/registration-of-requests/registration-of-requests.component.ts
+++ src/app/pages/request/transfer-request/registration-of-requests/registration-of-requests.component.ts
@@ -35,11 +35,11 @@ export class RegistrationOfRequestsComponent
   tab6: string = '';
 
   //registro de solicitudos o bienes
-  requestRegistration: boolean = false;
+  requestRegistration: boolean = true;
   //verificacion de cumplimientos tab
   complianceVerifi: boolean = false;
   //clasificacion de bienes
-  classifyAssets: boolean = true;
+  classifyAssets: boolean = false;
   //validar destino del bien(documento)
   validateDocument: boolean = false;
   //notificar aclaraciones o improcedencias
