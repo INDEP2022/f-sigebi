f99b426b861d1f49cfe99c32d9d9a0c1abede453
Felix Mamani <felixmamanicala@gmail.com>
2022-11-28 16:27:58 -0400

    SIG-778-mantenimiento-programacion
    SIG-778-mantenimiento-programacion

---
+++ src/app/pages/siab-web/sami/schedule-maintenance/schedule-maintenance/schedule-maintenance-columns.ts
@@ -0,0 +1,46 @@
+export const SCHEDULE_MANINTENANCE_COLUMNS = {
+  level: {
+    title: 'Nivel',
+    sort: false,
+  },
+  descriptionFraction: {
+    title: 'Descripción Fracción',
+    sort: false,
+  },
+  sorterSiab: {
+    title: 'Clasificador SIAB',
+    sort: false,
+  },
+};
+export const CURRENT_REGIONAL_OFFICE_COLUMNS = {
+  request: {
+    title: 'Solicitud',
+    sort: false,
+  },
+  regionalOffice: {
+    title: 'Oficina reginal',
+    sort: false,
+  },
+  addressRegionalOffice: {
+    title: 'Domicilio Oficina reginal',
+    sort: false,
+  },
+  regionalOfficeFGR: {
+    title: 'Oficina reginal FGR',
+    sort: false,
+  },
+};
+export const STATUS_COLUMNS = {
+  request: {
+    title: 'Solicitud',
+    sort: false,
+  },
+  noGood: {
+    title: 'No.Bien',
+    sort: false,
+  },
+  status: {
+    title: 'Estatus',
+    sort: false,
+  },
+};
