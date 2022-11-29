5f4d2f9acd75e0ba27c5d188ec44205df29d81e1
Henry Giovanni Sanchez Solis <sshenrygiovanni@gmail.com>
2022-11-29 16:06:07 -0600

    agregando componente de siabweb

--- src/app/pages/siab-web/appraisals/sw-avaluos-m-res-cancel-valuation/sw-avaluos-c-res-cancel-valuation/sw-avaluos-c-res-cancel-valuation.component.ts
+++ src/app/pages/siab-web/appraisals/sw-avaluos-m-res-cancel-valuation/sw-avaluos-c-res-cancel-valuation/sw-avaluos-c-res-cancel-valuation.component.ts
@@ -1,19 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+import { ListParams } from 'src/app/common/repository/interfaces/list-params';
 import { maxDate } from 'src/app/common/validations/date.validators';
+import { BasePage } from 'src/app/core/shared/base-page';
+import { RES_CANCEL_VALUATION_COLUMS } from './res-cancel-valuation-columns';
 
 @Component({
   selector: 'app-sw-avaluos-c-res-cancel-valuation',
   templateUrl: './sw-avaluos-c-res-cancel-valuation.component.html',
   styles: [],
 })
-export class SwAvaluosCResCancelValuationComponent implements OnInit {
+export class SwAvaluosCResCancelValuationComponent
+  extends BasePage
+  implements OnInit
+{
   form: FormGroup = new FormGroup({});
 
-  constructor(private fb: FormBuilder) {}
+  columns: any[] = [];
+  totalItems: number = 0;
+  params = new BehaviorSubject<ListParams>(new ListParams());
+
+  constructor(private fb: FormBuilder) {
+    super();
+    this.settings = {
+      ...this.settings,
+      actions: false,
+      selectMode: 'multi',
+      columns: { ...RES_CANCEL_VALUATION_COLUMS },
+    };
+  }
 
   ngOnInit(): void {
     this.prepareForm();
+    this.getPagination();
   }
 
   private prepareForm() {
@@ -32,5 +52,34 @@ export class SwAvaluosCResCancelValuationComponent implements OnInit {
       addressee: [null, [Validators.required]],
       addresseeTxt: [null, [Validators.required]],
     });
+  }
+
+  data = [
+    {
+      noBien: 564,
+      description: 'Descripción del 564',
+      amount: '$41,151.00',
+      status: 'Disponible',
+      motive: 'Motivo 564',
+    },
+    {
+      noBien: 45,
+      description: 'Descripción del 45',
+      amount: '$1,500.00',
+      status: 'No Disponible',
+      motive: 'Motivo 45',
+    },
+    {
+      noBien: 785,
+      description: 'Descripción del 785',
+      amount: '$201,500.00',
+      status: 'Disponible',
+      motive: 'Motivo 785',
+    },
+  ];
+
+  getPagination() {
+    this.columns = this.data;
+    this.totalItems = this.columns.length;
   }
 }
