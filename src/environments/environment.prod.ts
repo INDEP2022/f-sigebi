4f9cd286e596297056acdf5cbdc1d03225757f10
Raul Legaspi <legaspi081213@gmail.com>
2022-11-29 17:37:59 -0600

    generate-image

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,19 @@
+/*
+ * Type window Object()
+ */
+declare const window: any;
+
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:3000/api/v1/',
-  api_external_token:
-    'http://52.142.23.169/realms/indep/protocol/openid-connect/token',
-  api_external_userInfo:
-    'http://52.142.23.169/realms/indep/protocol/openid-connect/userinfo',
-  api_external_typeUser: 'http://168.61.50.112:3003/api/v1/user/userType/',
+  API_URL: window.env.API_URL,
+  api_external_token: window.env.api_external_token,
+  api_external_userInfo: window.env.api_external_userInfo,
+  api_external_typeUser: window.env.api_external_typeUser,
 };
+/*export const environment = {
+  production: true,
+  API_URL:'http://localhost:3000/api/v1/',
+  api_external_token:'http://52.142.23.169/realms/indep/protocol/openid-connect/token',
+  api_external_userInfo:'http://52.142.23.169/realms/indep/protocol/openid-connect/userinfo',
+  api_external_typeUser: 'http://168.61.50.112:3003/api/v1/user/userType/',
+};*/
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,17 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+/*
+ * Type window Object()
+ */
+declare const window: any;
+
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:3000/api/v1/',
-  api_external_token:
-    'http://52.142.23.169/realms/indep/protocol/openid-connect/token',
-  api_external_userInfo:
-    'http://52.142.23.169/realms/indep/protocol/openid-connect/userinfo',
-  api_external_typeUser: 'http://168.61.50.112:3003/api/v1/user/userType/',
+  API_URL: window.env.API_URL,
+  api_external_token: window.env.api_external_token,
+  api_external_userInfo: window.env.api_external_userInfo,
+  api_external_typeUser: window.env.api_external_typeUser,
 };
 
 /*
