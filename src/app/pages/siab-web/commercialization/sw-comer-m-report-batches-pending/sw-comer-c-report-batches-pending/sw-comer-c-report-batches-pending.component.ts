3a117b76db143a35fd50ff6370a79f187b54773d
Henry Giovanni Sanchez Solis <sshenrygiovanni@gmail.com>
2022-11-30 17:07:16 -0600

    lotes pendientes de pago agregado

--- src/app/pages/siab-web/commercialization/sw-comer-m-report-batches-pending/sw-comer-c-report-batches-pending/sw-comer-c-report-batches-pending.component.ts
+++ src/app/pages/siab-web/commercialization/sw-comer-m-report-batches-pending/sw-comer-c-report-batches-pending/sw-comer-c-report-batches-pending.component.ts
@@ -1,12 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BasePage } from 'src/app/core/shared/base-page';
+import {
+  CAPTURE_LINES,
+  CAPTURE_LINES_CLIENTS,
+} from './report-batches-pending-colums';
 
 @Component({
   selector: 'app-sw-comer-c-report-batches-pending',
   templateUrl: './sw-comer-c-report-batches-pending.component.html',
   styles: [],
 })
-export class SwComerCReportBatchesPendingComponent implements OnInit {
-  constructor() {}
+export class SwComerCReportBatchesPendingComponent
+  extends BasePage
+  implements OnInit
+{
+  form: FormGroup = new FormGroup({});
+  show: boolean = false;
 
-  ngOnInit(): void {}
+  settings2;
+  constructor(private fb: FormBuilder) {
+    super();
+
+    this.settings = {
+      ...this.settings,
+      actions: false,
+      columns: { ...CAPTURE_LINES },
+    };
+
+    this.settings2 = {
+      ...this.settings,
+      actions: false,
+      columns: { ...CAPTURE_LINES_CLIENTS },
+    };
+  }
+
+  ngOnInit(): void {
+    this.prepareForm();
+  }
+
+  private prepareForm() {
+    this.form = this.fb.group({
+      event: [null, [Validators.required]],
+    });
+  }
+
+  data: any;
 }
