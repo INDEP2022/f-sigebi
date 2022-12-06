468526a7a3d00eb7b2f618620679d2b387c9c390
Henry Giovanni Sanchez Solis <sshenrygiovanni@gmail.com>
2022-12-06 14:20:02 -0600

    componente monitoreo en siab web comer

--- src/app/pages/siab-web/commercialization/batch-status-monitoring/batch-status-monitoring/batch-status-monitoring.component.ts
+++ src/app/pages/siab-web/commercialization/batch-status-monitoring/batch-status-monitoring/batch-status-monitoring.component.ts
@@ -1,12 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BasePage } from 'src/app/core/shared/base-page';
+import { GRV_DETALLES_COLUMNS, GV_LOTES_COLUMNS } from './columns';
 
 @Component({
   selector: 'app-batch-status-monitoring',
   templateUrl: './batch-status-monitoring.component.html',
   styles: [],
 })
-export class BatchStatusMonitoringComponent implements OnInit {
-  constructor() {}
+export class BatchStatusMonitoringComponent extends BasePage implements OnInit {
+  form: FormGroup = new FormGroup({});
+  form2: FormGroup = new FormGroup({});
 
-  ngOnInit(): void {}
+  show: boolean = false;
+  show2: boolean = false;
+
+  settings2 = {
+    ...this.settings,
+    actions: false,
+  };
+
+  constructor(private fb: FormBuilder) {
+    super();
+    this.settings = {
+      ...this.settings,
+      actions: false,
+      columns: { ...GRV_DETALLES_COLUMNS },
+    };
+
+    this.settings2.columns = GV_LOTES_COLUMNS;
+  }
+
+  ngOnInit(): void {
+    this.prepareForm();
+    this.prepareForm2();
+  }
+
+  private prepareForm() {
+    this.form = this.fb.group({
+      typeGood: [null, [Validators.required]],
+      event: [null, [Validators.required]],
+      transferee: [null, [Validators.required]],
+      allotment: [null, [Validators.required]],
+    });
+  }
+
+  private prepareForm2() {
+    this.form2 = this.fb.group({
+      radio: [null, [Validators.required]],
+    });
+  }
+
+  chargeFile(event: any) {}
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.form.reset();
+    }
+    console.warn('Your order has been submitted');
+  }
+
+  data: any;
 }
