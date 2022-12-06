888ba8731d3d984a309c3aee8dc985e29f8b8e18
Fernando <f3rfams@gmail.com>
2022-12-06 17:42:31 -0600

    Cambio de nombre de archivos modulo Procesos Ejecutivos

---
+++ src/app/pages/executive-processes/cumulative-goods/cumulative-goods/cumulative-goods.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
+import {
+  BsDatepickerConfig,
+  BsDatepickerViewMode,
+} from 'ngx-bootstrap/datepicker';
+import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
+import { PreviewDocumentsComponent } from 'src/app/@standalone/preview-documents/preview-documents.component';
+import { maxDate } from 'src/app/common/validations/date.validators';
+import { DefaultSelect } from 'src/app/shared/components/select/default-select';
+
+@Component({
+  selector: 'app-cumulative-goods',
+  templateUrl: './cumulative-goods.component.html',
+  styles: [],
+})
+export class CumulativeGoodsComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  select = new DefaultSelect();
+
+  pdfurl = 'https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf';
+
+  modeMonth: BsDatepickerViewMode = 'month'; // change for month:year
+  bsConfigFromMonth: Partial<BsDatepickerConfig>;
+  bsConfigToMonth: Partial<BsDatepickerConfig>;
+
+  constructor(
+    private modalService: BsModalService,
+    private fb: FormBuilder,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  ngOnInit(): void {
+    this.prepareForm();
+    this.bsConfigFromMonth = Object.assign(
+      {},
+      {
+        minMode: this.modeMonth,
+        dateInputFormat: 'MMMM',
+      }
+    );
+    this.bsConfigToMonth = Object.assign(
+      {},
+      {
+        minMode: this.modeMonth,
+        dateInputFormat: 'MMMM',
+      }
+    );
+  }
+
+  private prepareForm() {
+    this.form = this.fb.group({
+      delegation: [''],
+      subdelegation: [''],
+      toMonth: [null, [Validators.required, maxDate(new Date())]],
+      fromMonth: [null, [Validators.required, maxDate(new Date())]],
+    });
+  }
+
+  openPrevPdf() {
+    let config: ModalOptions = {
+      initialState: {
+        documento: {
+          urlDoc: this.sanitizer.bypassSecurityTrustResourceUrl(this.pdfurl),
+          type: 'pdf',
+        },
+        callback: (data: any) => {
+          console.log(data);
+        },
+      }, //pasar datos por aca
+      class: 'modal-lg modal-dialog-centered', //asignar clase de bootstrap o personalizado
+      ignoreBackdropClick: true, //ignora el click fuera del modal
+    };
+    this.modalService.show(PreviewDocumentsComponent, config);
+  }
+}
