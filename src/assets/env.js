8abe790fd293da7dd554e899e8a4f510cc2aae44
Raul Legaspi <legaspi081213@gmail.com>
2022-12-08 10:56:51 -0600

    get token

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,7 +1,7 @@
 (function (window) {
   window["env"] = window["env"] || {};
   window["env"].EXTERNAL_INTEGRATION_URL = 'http://www.indep.gob.mx/firmaelectronica'; // not actualized, for local testing
-  window["env"].API_BACKEND_URL = 'http://localhost:3000/api/v1/'; // not actualized, for local testing
+  window["env"].API_BACKEND_URL = 'http://20.119.126.102:3000/api/v1/';//'http://localhost:3000/api/v1/'; // not actualized, for local testing
   
   window["env"].api_external_token = 'http://52.142.23.169/realms/indep/protocol/openid-connect/token';
   window["env"].api_external_userInfo ='http://52.142.23.169/realms/indep/protocol/openid-connect/userinfo';
