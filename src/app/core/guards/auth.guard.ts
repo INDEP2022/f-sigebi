241e081674b5026a90339d86acd1faac74b6f701
genaroadrian <al221711695@gmail.com>
2022-12-20 16:39:49 -0600

    Permissions guard y directiva de permisos para manejar los permisos

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,85 +1,53 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { PermissionsService } from 'src/app/common/services/permissions.service';
 import { AuthService } from '../services/authentication/auth.service';
-
+const READ_PERMISSION = 'read';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(
     private readonly authService: AuthService,
-    private readonly router: Router
+    private readonly router: Router,
+    private permissionsService: PermissionsService
   ) {}
 
   canActivate(
-    next: ActivatedRouteSnapshot
+    route: ActivatedRouteSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.verify(route);
+  }
+
+  canLoad(route: Route): boolean {
+    return this.verify(route);
+  }
+
+  verify(route: Route) {
     this.authService.getTokenExpiration();
     if (this.authService.existToken() && !this.authService.isTokenExpired()) {
-      return true; //this.checkRoles(next) ? true : false;
+      return this.checkRoles(route);
     } else {
       this.router.navigate(['/auth/login']);
       return false;
     }
   }
 
-  checkRoles(route?: ActivatedRouteSnapshot): Promise<boolean> | boolean {
-    if (this.authService.hasRoles()) {
-      //return route.data['screen'] ? true : true;//false
-      let currentRoute = route.root;
-      let screen = '';
-      do {
-        const childrenRoutes = currentRoute.children;
-        currentRoute = null;
-        childrenRoutes.forEach(routes => {
-          if (routes.outlet === 'primary') {
-            screen = route.data['screen'];
-            currentRoute = routes;
-          }
-        });
-      } while (currentRoute);
-
-      if (screen !== undefined) {
-        //console.log('screen')
-        //console.log(route.data['screen'])
-        //console.log(this.authService.accessRoles());
-        let roles = this.authService
-          .accessRoles()
-          .filter((rol: any) => rol.name == 'catologos');
-        //console.log(roles[0].menus.indexOf((menu:any)=>menu.screen == screen));
-        /*this.authService.accessRoles().map((rol:any)=>{
-          console.log(rol.menus.indexOf((menu:any)=>menu.screen == screen));
-        })*/
-        return true;
-      } else {
-        //console.log('no tiene ')
-        //
-        return true;
-      }
-    } else {
-      return false;
-    }
-    /*return  this.authFS.getProfile().then((profile:any)=>{
-      const userRole = profile.perfil;
-      if(route.data.role && route.data.role.indexOf(userRole) === -1) {
-        this.router.navigate(['/inicio']);
-        return false;
-      }
-        return true;
-    })*/
+  checkRoles(route: ActivatedRouteSnapshot | Route): boolean {
+    // TODO: Cambiar "any" por la interfaz
+    const screenId = !!route && route.data ? route.data['screen'] : '';
+    // TODO: quitar este if cuando todos los menus tengan sus permisos
+    if (!screenId) return true;
+    return this.permissionsService.hasPermissionOnScreen(
+      READ_PERMISSION,
+      screenId
+    );
   }
-
-  /*checkUserLogin(route: ActivatedRouteSnapshot, url: any):  Promise<boolean | UrlTree> {
-    return new Promise((resolve, reject) => {
-      this.authFS.isLogged().subscribe(user=>{
-        if(!user){
-          this.router.navigate(['/auth/login']);
-          resolve(false)
-        }else{
-          resolve(true)
-        }
-      })
-    });
-  }*/
 }
