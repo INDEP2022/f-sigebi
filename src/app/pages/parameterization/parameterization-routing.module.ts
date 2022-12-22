b2ae9514ecfe3964e5bd40a00296d2df5d999517
Henry Giovanni Sanchez Solis <sshenrygiovanni@gmail.com>
2022-12-22 14:04:14 -0600

    agregando propiedad en routing henry

--- src/app/pages/parameterization/parameterization-routing.module.ts
+++ src/app/pages/parameterization/parameterization-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  //Henry
+  //Henryy
   {
     path: 'cat-financial-information-attributes',
     loadChildren: async () =>
