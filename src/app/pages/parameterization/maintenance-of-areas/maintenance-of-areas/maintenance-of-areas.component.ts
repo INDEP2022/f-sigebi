7c5eb2a974edf2075869881a64c4878cca4e9176
aalvarezg1998 <107310910+aalvarezg1998@users.noreply.github.com>
2023-01-19 13:59:27 -0500

    Falta concluir el filtro

--- src/app/pages/parameterization/maintenance-of-areas/maintenance-of-areas/maintenance-of-areas.component.ts
+++ src/app/pages/parameterization/maintenance-of-areas/maintenance-of-areas/maintenance-of-areas.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BsModalService } from 'ngx-bootstrap/modal';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, takeUntil } from 'rxjs';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import { IDelegation } from 'src/app/core/models/catalogs/delegation.model';
+import { IDepartment } from 'src/app/core/models/catalogs/department.model';
+import { ISubdelegation } from 'src/app/core/models/catalogs/subdelegation.model';
+import { DelegationService } from 'src/app/core/services/catalogs/delegation.service';
+import { DepartamentService } from 'src/app/core/services/catalogs/departament.service';
+import { SubdelegationService } from 'src/app/core/services/catalogs/subdelegation.service';
 import { BasePage } from 'src/app/core/shared/base-page';
+import { DefaultSelect } from 'src/app/shared/components/select/default-select';
 import { MaintenanceOfAreasModalComponent } from '../maintenance-of-areas-modal/maintenance-of-areas-modal.component';
 import { COLUMNS } from './columns';
 
@@ -13,12 +20,24 @@ import { COLUMNS } from './columns';
   styles: [],
 })
 export class MaintenanceOfAreasComponent extends BasePage implements OnInit {
-  form: FormGroup = new FormGroup({});
+  delegation: IDelegation;
+  subDelegation: ISubdelegation;
+  departments: IDepartment[] = [];
+  subdelegations = new DefaultSelect<ISubdelegation>();
+  delegations = new DefaultSelect<IDelegation>();
+
   columns: any[] = [];
   totalItems: number = 0;
-  params = new BehaviorSubject<ListParams>(new ListParams());
 
-  constructor(private modalService: BsModalService, private fb: FormBuilder) {
+  params = new BehaviorSubject<ListParams>(new ListParams());
+  form: FormGroup = new FormGroup({});
+  constructor(
+    private modalService: BsModalService,
+    private fb: FormBuilder,
+    private departmentService: DepartamentService,
+    private service: DelegationService,
+    private serviceSubDeleg: SubdelegationService
+  ) {
     super();
     this.settings = {
       ...this.settings,
@@ -31,29 +50,6 @@ export class MaintenanceOfAreasComponent extends BasePage implements OnInit {
       columns: { ...COLUMNS },
     };
   }
-
-  data = [
-    {
-      keyAdmi: '0',
-      area: 'DAB',
-      description: 'Descripcion 1',
-    },
-    {
-      keyAdmi: '0',
-      area: 'DAB',
-      description: 'Descripcion 1',
-    },
-    {
-      keyAdmi: '0',
-      area: 'DAB',
-      description: 'Descripcion 1',
-    },
-    {
-      keyAdmi: '0',
-      area: 'DAB',
-      description: 'Descripcion 1',
-    },
-  ];
 
   ngOnInit(): void {
     this.buildForm();
@@ -82,19 +78,63 @@ export class MaintenanceOfAreasComponent extends BasePage implements OnInit {
       ignoreBackdropClick: true,
     });
     modalRef.content.refresh.subscribe(next => {
-      if (next) this.getData();
+      /* if (next) this.getDepartment(); */
     });
   }
 
-  getData() {
+  getDepartment(idDelegation: number, idSubDelegation: number) {
+    this.departments = [];
+    this.loading = true;
+    this.departmentService.getAll(this.params.getValue()).subscribe({
+      next: response => {
+        this.totalItems = response.count;
+        this.loading = false;
+        this.departments = response.data.filter(
+          element =>
+            element.numDelegation === idDelegation ||
+            element.numSubDelegation === idSubDelegation
+        );
+      },
+      error: error => (this.loading = false),
+    });
+  }
+
+  delete(id: number) {
+    this.departmentService.remove(id).subscribe({
+      /* next: () => this.getDepartment(), */
+    });
+  }
+
+  /*   getData() {
     this.loading = true;
     this.columns = this.data;
     this.totalItems = this.data.length;
     this.loading = false;
-  }
+  } */
 
   getPagination() {
-    this.columns = this.data;
+    this.columns = this.departments;
     this.totalItems = this.columns.length;
+  }
+
+  onDelegationsChange() {
+    //console.log('Este es el valor', this.form.get('delegation').value);
+    /* this.delegation = delegation;
+    this.params
+      .pipe(takeUntil(this.$unSubscribe))
+      .subscribe(() => this.getDepartment()); */
+  }
+
+  onSubDelegationChange() {
+    console.log('Este es el valor', this.form.get('subdelegation').value);
+    /* this.subDelegation = subDelegation; */
+    this.params
+      .pipe(takeUntil(this.$unSubscribe))
+      .subscribe(() =>
+        this.getDepartment(
+          this.form.get('delegation').value,
+          this.form.get('subdelegation').value
+        )
+      );
   }
 }
