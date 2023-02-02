d79c03ea083e81d725ea41841713bc868365a79d
gus2612 <gustavoangelsantosclemente@gmail.com>
2023-02-02 09:37:57 -0600

    solve error service

--- src/app/pages/request/programming-request-components/service/programming-good.service.ts
+++ src/app/pages/request/programming-request-components/service/programming-good.service.ts
@@ -1,20 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ENDPOINT_LINKS } from 'src/app/common/constants/endpoints';
+import { ProgrammingGoodEndpoints } from 'src/app/common/constants/endpoints/ms-programming-good-enpoints';
 import { ICrudMethods } from 'src/app/common/repository/interfaces/crud-methods';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
-import { ProgrammingGoodRepository } from 'src/app/common/repository/repositories/ms-goods-programming.repository';
 import { IListResponse } from 'src/app/core/interfaces/list-response.interface';
 import { IGoodProgramming } from 'src/app/core/models/good-programming/good-programming';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class ProgrammingGoodService implements ICrudMethods<IGoodProgramming> {
-  private readonly route: string = ENDPOINT_LINKS.ProgrammingGood;
-  constructor(
-    private progGoodRepository: ProgrammingGoodRepository<IGoodProgramming>
-  ) {}
+  private readonly route: string = ProgrammingGoodEndpoints.ProgrammingGood;
+  constructor(private httpClient: HttpClient) {}
 
   getAll(params?: ListParams): Observable<IListResponse<IGoodProgramming>> {
-    return this.progGoodRepository.getAllPaginated(this.route, params);
+    return this.httpClient.get<IListResponse<IGoodProgramming>>(
+      `${environment.API_URL}${this.route}api/v1/programming`
+    );
   }
 }
