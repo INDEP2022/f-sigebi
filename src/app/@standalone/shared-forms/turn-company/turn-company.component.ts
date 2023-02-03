1c34b6ecd7d938948139bfb2d1d82d284a205cf7
Jose Eduardo <joseeduardo.jeg37@gmail.com>
2023-02-02 18:05:11 -0600

    servicios implementados, catalogos terminados

--- src/app/@standalone/shared-forms/turn-company/turn-company.component.ts
+++ src/app/@standalone/shared-forms/turn-company/turn-company.component.ts
@@ -1,42 +1,45 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { AbstractControl, FormGroup } from '@angular/forms';
-import { ListParams } from 'src/app/common/repository/interfaces/list-params';
-import { IFederative } from 'src/app/core/models/administrative-processes/siab-sami-interaction/federative.model';
-import { EntFedService } from 'src/app/core/services/catalogs/entfed.service';
+import { IGiro } from 'src/app/core/models/parameterization/giro.model';
+import { DynamicTablesService } from 'src/app/core/services/dynamic-catalogs/dynamic-tables.service';
 import { BasePage } from 'src/app/core/shared/base-page';
 import { DefaultSelect } from 'src/app/shared/components/select/default-select';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
-  selector: 'app-federative-shared',
+  selector: 'app-turn-company',
+  templateUrl: './turn-company.component.html',
   standalone: true,
   imports: [CommonModule, SharedModule],
-  templateUrl: './federative-shared.component.html',
-  styles: [],
 })
-export class FederativeSharedComponent extends BasePage implements OnInit {
+export class TurnCompanyComponent extends BasePage implements OnInit {
   @Input() form: FormGroup;
-  @Input() federativeField: string = 'federative';
+  @Input() turnField: string = 'keyOperation';
 
-  @Input() showFederative: boolean = true;
+  @Input() showTurn: boolean = true;
 
-  federative = new DefaultSelect<IFederative>();
+  turn: any = new DefaultSelect<IGiro>();
 
-  constructor(private service: EntFedService) {
+  constructor(private service: DynamicTablesService) {
     super();
   }
 
   get Federative() {
-    return this.form.get(this.federativeField);
+    return this.form.get(this.turnField);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getTurn();
+  }
 
-  getFederative(params: ListParams) {
-    this.service.getAll(params).subscribe({
+  getTurn() {
+    this.service.getByIdData(8).subscribe({
       next: data =>
-        (this.federative = new DefaultSelect(data.data, data.count)),
+        (this.turn = new DefaultSelect(
+          data.table.data,
+          data.table.data.lenght
+        )),
       error: err => {
         let error = '';
         if (err.status === 0) {
@@ -48,8 +51,10 @@ export class FederativeSharedComponent extends BasePage implements OnInit {
       },
     });
   }
-  onFederativeChange(type: any) {
-    this.federative = new DefaultSelect();
+  onTurnChange(type: any) {
+    this.turn = new DefaultSelect();
+    this.getTurn();
+    this.form.updateValueAndValidity();
     // this.form.updateValueAndValidity();
   }
 
