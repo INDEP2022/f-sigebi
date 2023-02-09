cf314d1984b90c99886cc494ac309349a9c9ca2b
JRicardoHernandez <riiiicho@gmail.com>
2023-02-09 13:05:36 -0600

    Agregar muestra de errores en carga masiva de bienes SAT_SAE y PGR_SAE

--- src/app/pages/documents-reception/goods-bulk-load/constants/good-bulk-load-data.ts
+++ src/app/pages/documents-reception/goods-bulk-load/constants/good-bulk-load-data.ts
@@ -56,3 +56,40 @@ export const GOODS_BULK_LOAD_TARGETS = [
     value: 'pgr',
   },
 ];
+export const SAT_SAE_MUEBLES_PROCESO_4: any = {
+  listSearchExist: ['BLINDAJE', 'NUMERO', 'MOTOR'],
+  listEqualExist: [
+    'ENTIDAD FEDERATIVA',
+    'ESTADO FISICO',
+    'MARCA',
+    'SUBMARCA',
+    'MODELO',
+    'NUMERO DE PLACAS',
+    'NUMERO DE SERIE',
+    'PROCEDENCIA',
+    'UBICACION',
+    'TIPO',
+  ],
+};
+export const SAT_SAE_INMUEBLES_PROCESO_4: any = {
+  listSearchExist: [
+    'COLONIA',
+    'ESTADO FISICO',
+    'NOMBRE',
+    'CALLE',
+    'NUMERO EXTERIOR',
+  ],
+  listEqualExist: [
+    'OFICIO SOLICITUD EN EL RPP',
+    'FECHA DE SOLICITUD AL RPP',
+    'RPP',
+    'MENAJE',
+    'DOMICILIO',
+    'DELEGACION O MUNICIPIO',
+    'ESTADO',
+    'SUPERFICIE DEL TERRENO',
+    'SUPERFICIE CONSTRUIDA',
+    'HABITADO',
+    'AGUA',
+  ],
+};
