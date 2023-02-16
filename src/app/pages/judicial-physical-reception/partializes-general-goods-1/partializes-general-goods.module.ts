574ff749292bf09e86480be3d17310bcedc2ca88
Angelo <angelo.rodas@umatec.pe>
2023-02-16 09:34:33 -0500

    parcializar-bienes

--- src/app/pages/judicial-physical-reception/partializes-general-goods-1/partializes-general-goods.module.ts
+++ src/app/pages/judicial-physical-reception/partializes-general-goods-1/partializes-general-goods.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GoodsClasificationSharedComponent } from 'src/app/@standalone/shared-forms/goods-classification-shared/goods-classification-shared.component';
 import { GoodsStatusSharedComponent } from 'src/app/@standalone/shared-forms/goods-status-shared/goods-status-shared.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { PartializesGeneralGoodsRoutingModule } from './partializes-general-goods-routing.module';
@@ -17,6 +18,7 @@ import { PartializesGeneralGoodsComponent } from './partializes-general-goods.co
     ModalModule.forChild(),
     TabsModule,
     GoodsStatusSharedComponent,
+    GoodsClasificationSharedComponent,
   ],
 })
 export class PartializesGeneralGoodsModule {}
--- src/app/pages/judicial-physical-reception/scheduled-maintenance-1/scheduled-maintenance.component.ts
+++ src/app/pages/judicial-physical-reception/scheduled-maintenance-1/scheduled-maintenance.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TABLE_SETTINGS } from 'src/app/common/constants/table-settings';
+import { BasePage } from 'src/app/core/shared/base-page';
 import { STRING_PATTERN } from 'src/app/core/shared/patterns';
 import { DefaultSelect } from 'src/app/shared/components/select/default-select';
+import { ProceedingsDeliveryReceptionTsService } from './../../../core/services/ms-proceedings/proceedings-delivery-reception.ts.service';
 
 @Component({
   selector: 'app-scheduled-maintenance',
   templateUrl: './scheduled-maintenance.component.html',
   styleUrls: ['scheduled-maintenance.scss'],
 })
-export class ScheduledMaintenanceComponent implements OnInit {
+export class ScheduledMaintenanceComponent extends BasePage implements OnInit {
   itemsSelect = new DefaultSelect();
   form: FormGroup;
   settings1 = {
@@ -45,20 +47,47 @@ export class ScheduledMaintenanceComponent implements OnInit {
     },
     noDataMessage: 'No se encontrarón registros',
   };
-
+  statusList = [
+    { id: 0, description: 'Abierto' },
+    { id: 1, description: 'Cerrado' },
+    { id: 2, description: 'Todos' },
+  ];
+  statusEvent = new DefaultSelect(this.statusList, 3);
   data = EXAMPLE_DATA;
-
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private proceedingService: ProceedingsDeliveryReceptionTsService
+  ) {
+    super();
+  }
 
   ngOnInit(): void {
     this.prepareForm();
+    // this.proceedingService.getAll(new ListParams()).subscribe(x => {
+    //   console.log(x);
+    // });
+    // this.getStatus();
+  }
+
+  getStatus(event: any) {
+    console.log(event);
+    this.statusEvent = new DefaultSelect(
+      this.statusList.filter(status =>
+        status.description.toLowerCase().trim().includes(event.text)
+      )
+    );
+  }
+
+  onEventsChange(type: any) {
+    this.form.updateValueAndValidity();
+    // this.statusSelect = new DefaultSelect();
+    // this.emitTevents.emit(type);
   }
 
   prepareForm() {
     this.form = this.fb.group({
       tipoEvento: [null, [Validators.required]],
-      fechapage: [null, [Validators.required]],
-      fechaFin: [null, [Validators.required]],
+      fechapageFin: [null, [Validators.required]],
       statusEvento: [null, [Validators.required]],
       coordRegional: [
         null,
