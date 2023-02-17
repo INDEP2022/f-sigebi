6a342e819c385db7675d13bdff6f420c17538421
JRicardoHernandez <riiiicho@gmail.com>
2023-02-17 07:51:15 -0600

    Obteniendo las propiedades para crear el expediente para guardar

--- src/app/pages/documents-reception/goods-bulk-load/services/goods-bulk-load.table.ts
+++ src/app/pages/documents-reception/goods-bulk-load/services/goods-bulk-load.table.ts
@@ -7,9 +7,9 @@ import { ListParams } from 'src/app/common/repository/interfaces/list-params';
 import { AuthorityService } from 'src/app/core/services/catalogs/authority.service';
 import { GoodSssubtypeService } from 'src/app/core/services/catalogs/good-sssubtype.service';
 import { IssuingInstitutionService } from 'src/app/core/services/catalogs/issuing-institution.service';
-import { ExpedientService } from 'src/app/core/services/expedients/expedient.service';
 import { GoodService } from 'src/app/core/services/good/good.service';
 import { GoodsQueryService } from 'src/app/core/services/goodsquery/goods-query.service';
+import { ExpedientService } from 'src/app/core/services/ms-expedient/expedient.service';
 
 @Injectable({
   providedIn: 'root',
