f11bb4b1bf21341ae2a51b691dac8e471c4cef4e
gus2612 <gustavoangelsantosclemente@gmail.com>
2023-02-20 22:34:59 -0600

    acept programming start date and end date complete working in view acept programming

--- src/app/pages/request/programming-request-components/schedule-reception/search-user-form/search-user-form.component.ts
+++ src/app/pages/request/programming-request-components/schedule-reception/search-user-form/search-user-form.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalDataSource } from 'ng2-smart-table';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { BehaviorSubject, takeUntil } from 'rxjs';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
-import { IUser } from 'src/app/core/models/catalogs/user.model';
 import { ProgrammingGoodService } from 'src/app/core/services/ms-programming-request/programming-good.service';
 import { BasePage } from 'src/app/core/shared/base-page';
+import { CheckboxElementComponent } from 'src/app/shared/components/checkbox-element-smarttable/checkbox-element';
 import { USER_COLUMNS } from '../../acept-programming/columns/users-columns';
 
 @Component({
@@ -13,11 +14,14 @@ import { USER_COLUMNS } from '../../acept-programming/columns/users-columns';
   styles: [],
 })
 export class SearchUserFormComponent extends BasePage implements OnInit {
-  usersData: IUser[] = [];
+  usersData: LocalDataSource = new LocalDataSource();
+  loadUsersData: LocalDataSource = new LocalDataSource();
   params = new BehaviorSubject<ListParams>(new ListParams());
+  usersSelected: LocalDataSource;
   totalItems: number = 0;
   typeUser: string = '';
-  userInfo: IUser[];
+  userInfo: any[] = [];
+  itemsInTable: number = 0;
   textButton: string = 'Seleccionar';
   constructor(
     private modalRef: BsModalRef,
@@ -27,12 +31,41 @@ export class SearchUserFormComponent extends BasePage implements OnInit {
     this.settings = {
       ...this.settings,
       actions: false,
-      columns: USER_COLUMNS,
-      selectMode: 'multi',
+      columns: {
+        ...USER_COLUMNS,
+        name: {
+          title: 'Selección usuario',
+          sort: false,
+          type: 'custom',
+          valuePrepareFunction: (user: any, row: any) =>
+            this.isUserSelected(row),
+          renderComponent: CheckboxElementComponent,
+          onComponentInitFunction: (instance: CheckboxElementComponent) =>
+            this.onUserChange(instance),
+        },
+      },
     };
   }
 
+  onUserChange(instance: CheckboxElementComponent) {
+    instance.toggle.pipe(takeUntil(this.$unSubscribe)).subscribe({
+      next: data => this.sendUser(data.row, data.toggle),
+    });
+  }
+  isUserSelected(user: any) {
+    const exist = this.userInfo.find(
+      _user =>
+        _user.programmingId == user.programmingId && user.email == _user.email
+    );
+    if (!exist) return false;
+    return true;
+  }
+
   ngOnInit(): void {
+    /*this.usersSelected.getElements().then(items => {
+      this.itemsInTable = items.length;
+    }); */
+
     this.params
       .pipe(takeUntil(this.$unSubscribe))
       .subscribe(() => this.getUsers());
@@ -46,22 +79,29 @@ export class SearchUserFormComponent extends BasePage implements OnInit {
       .getUsersProgramming(this.params.getValue())
       .subscribe({
         next: response => {
-          this.usersData = response.data;
-          console.log(this.usersData);
+          this.usersData.load(response.data);
           this.totalItems = response.count;
           this.loading = false;
         },
       });
   }
 
-  userSelect(event: any) {
-    this.userInfo = event.selected;
+  sendUser(user: any, selected: boolean) {
+    if (selected) {
+      this.userInfo.push(user);
+      console.log(this.userInfo);
+    } else {
+      this.userInfo = this.userInfo.filter(
+        _user => _user.wheelNumber != _user.wheelNumber
+      );
+    }
   }
 
   confirm() {
     if (this.userInfo) {
+      this.removeUsersSelected(this.userInfo);
       this.modalRef.content.callback(this.userInfo);
-      this.modalRef.hide();
+      this.close();
     } else {
       this.onLoadToast(
         'warning',
@@ -69,6 +109,14 @@ export class SearchUserFormComponent extends BasePage implements OnInit {
         'Debes seleccionar al menos un usuario'
       );
     }
+  }
+
+  removeUsersSelected(userInfo: any) {
+    this.usersData.getElements().then(items => {
+      userInfo.map((itemsRemove: any) => {
+        this.usersData.remove(itemsRemove);
+      });
+    });
   }
 
   close() {
