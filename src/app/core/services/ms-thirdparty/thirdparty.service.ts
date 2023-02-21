bed3df1939b35b0f0b1bb81921be6e01be00ba99
henrygiovannisanchezsolis <sshenrygiovanni@gmail.com>
2023-02-21 15:58:36 -0600

    integrando FCOMER063

---
+++ src/app/core/services/ms-thirdparty/thirdparty.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ThirdPartyEndpoints } from 'src/app/common/constants/endpoints/ms-third-party-endpoint';
+import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import { HttpService } from 'src/app/common/services/http.service';
+import { IListResponse } from '../../interfaces/list-response.interface';
+import { IThirdParty } from '../../models/ms-thirdparty/third-party.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class ThirdPartyService extends HttpService {
+    
+    constructor(){
+        super();
+        this.microservice = ThirdPartyEndpoints.BasePath;
+    }
+
+    getAll(params?: ListParams | string): Observable<IListResponse<IThirdParty>> {
+    return this.get<IListResponse<IThirdParty>>(ThirdPartyEndpoints.ThirdParty, params);
+  }
+
+
+
+
+}
---
+++ src/app/pages/commercialization/movable-property/third-party-marketers/third-party-marketers/columns.ts
@@ -0,0 +1,54 @@
+export const THIRD_COLUMNS = {
+  id: {
+    title: 'ID',
+    sort: false,
+  },
+  nameReason: {
+    title: 'nombre Razón',
+    sort: false,
+  },
+  calculationRoutine: {
+    title: 'Rutina Cálculo',
+    sort: false,
+  }
+};
+
+export const TYPE_EVENT_THIRD_COLUMNS = {
+    thirdPartyId: {
+      title: 'ID 3ro comercializador',
+      sort: false,
+      
+  },
+    typeEventId: {
+      title: 'Tipo de evento',
+      sort: false,
+  }
+}
+
+export const COMI_XTHIRC_COLUMNS  = {
+  /*idComiXThird: {
+      title: 'ID comer',
+      sort: false,
+      
+  },
+    idThirdParty: {
+      title: 'ID 3ro comercializador',
+      sort: false,
+      
+  },*/
+    startingAmount: {
+      title: 'Monto inicial',
+      sort: false,
+      
+  },
+    pctCommission: {
+      title: 'Pct comisión',
+      sort: false,
+      
+  },
+    finalAmount: {
+      title: 'Monto Final',
+      sort: false,
+      
+  },
+}
