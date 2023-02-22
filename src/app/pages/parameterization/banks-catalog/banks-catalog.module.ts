9ddf534e18b79c3c8cc4a223e09ff315e8f3b5e3
Jose Eduardo <joseeduardo.jeg37@gmail.com>
2023-02-22 14:15:04 -0600

    catalogos bancos y cuentas bancos listos, crud cuentas bancos

--- src/app/pages/parameterization/banks-catalog/banks-catalog.module.ts
+++ src/app/pages/parameterization/banks-catalog/banks-catalog.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BanksSharedComponent } from 'src/app/@standalone/shared-forms/banks-shared/banks-shared.component';
 import { DelegationSharedComponent } from 'src/app/@standalone/shared-forms/delegation-shared/delegation-shared.component';
 
+import { AccountBanksSharedComponent } from 'src/app/@standalone/shared-forms/account-banks-shared/account-banks-shared.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { BanksCatalogRoutingModule } from './banks-catalog-routing.module';
 import { BanksCatalogComponent } from './banks-catalog/banks-catalog.component';
@@ -15,6 +16,7 @@ import { ListBanksComponent } from './list-banks/list-banks.component';
     BanksCatalogRoutingModule,
     SharedModule,
     BanksSharedComponent,
+    AccountBanksSharedComponent,
     DelegationSharedComponent,
   ],
 })
