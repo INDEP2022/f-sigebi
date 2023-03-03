8c8ad62ed9142df403a66ffd627fec4ad4a73812
Jessica Orozco <jessicaorozco@gmail.com>
2023-03-03 09:32:32 -0500

    add change into component HTML disponsal-record

--- src/app/pages/commercialization/disposal-record-report/disposal-record-report/disposal-record-report.component.ts
+++ src/app/pages/commercialization/disposal-record-report/disposal-record-report/disposal-record-report.component.ts
@@ -3,15 +3,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { PreviewDocumentsComponent } from 'src/app/@standalone/preview-documents/preview-documents.component';
-import { maxDate } from 'src/app/common/validations/date.validators';
-import { STRING_PATTERN } from 'src/app/core/shared/patterns';
-
+import { BasePage } from 'src/app/core/shared/base-page';
 @Component({
   selector: 'app-disposal-record-report',
   templateUrl: './disposal-record-report.component.html',
   styles: [],
 })
-export class DisposalRecordReportComponent implements OnInit {
+export class DisposalRecordReportComponent extends BasePage implements OnInit {
   form: FormGroup = new FormGroup({});
 
   today: Date;
@@ -25,6 +23,7 @@ export class DisposalRecordReportComponent implements OnInit {
     private modalService: BsModalService,
     private sanitizer: DomSanitizer
   ) {
+    super();
     this.today = new Date();
     this.minDate = new Date(this.today.getFullYear(), this.today.getMonth(), 2);
   }
@@ -35,20 +34,30 @@ export class DisposalRecordReportComponent implements OnInit {
 
   private prepareForm() {
     this.form = this.fb.group({
-      delegation: [
-        null,
-        [Validators.required, Validators.pattern(STRING_PATTERN)],
-      ],
-      subDelegation: [
-        null,
-        [Validators.required, Validators.pattern(STRING_PATTERN)],
-      ],
+      delegation: [null, [Validators.required]],
+      subdelegation: [null, [Validators.required]],
       noFile: [null, [Validators.required]],
-      rangeDateFile: [null, [Validators.required, maxDate(new Date())]],
+      PN_EXPINI: [null, [Validators.required]],
+      PN_EXPFIN: [null, [Validators.required]],
       noActa: [null, [Validators.required]],
-      rangeDateActa: [null, [Validators.required, maxDate(new Date())]],
-      rangeDate: [null, [Validators.required, maxDate(new Date())]],
+      PF_ELABIN: [null, [Validators.required]],
+      PF_ELABFIN: [null, [Validators.required]],
+      PN_ACTAINI: [null, [Validators.required]],
+      PN_ACTAFIN: [null, [Validators.required]],
     });
+  }
+  confirm() {
+    let params = {
+      PN_DELEGACION: this.form.controls['delegation'].value,
+      PN_SUBDELEGACION: this.form.controls['subdelegation'].value,
+      PN_EXPINI: this.form.controls['PN_EXPINI'].value,
+      PN_EXPFIN: this.form.controls['PN_EXPFIN'].value,
+      PC_ACTA: this.form.controls['noActa'].value,
+      PF_ELABIN: this.form.controls['PF_ELABIN'].value,
+      PF_ELABFIN: this.form.controls['PF_ELABFIN'].value,
+      PN_ACTAINI: this.form.controls['PN_ACTAINI'].value,
+      PN_ACTAFIN: this.form.controls['PN_ACTAFIN'].value,
+    };
   }
 
   openPrevPdf() {
