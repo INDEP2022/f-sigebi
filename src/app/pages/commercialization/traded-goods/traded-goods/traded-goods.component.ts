45dcb8cd0cf8501d9136393a1463caa82a7d1ca7
Jessica Orozco <jessicaorozco@gmail.com>
2023-03-04 21:36:39 -0500

    add changes into traded-goods

--- src/app/pages/commercialization/traded-goods/traded-goods/traded-goods.component.ts
+++ src/app/pages/commercialization/traded-goods/traded-goods/traded-goods.component.ts
@@ -3,15 +3,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { PreviewDocumentsComponent } from 'src/app/@standalone/preview-documents/preview-documents.component';
-import { maxDate } from 'src/app/common/validations/date.validators';
-import { STRING_PATTERN } from 'src/app/core/shared/patterns';
+import { BasePage } from 'src/app/core/shared/base-page';
 
 @Component({
   selector: 'app-traded-goods',
   templateUrl: './traded-goods.component.html',
   styles: [],
 })
-export class TradedGoodsComponent implements OnInit {
+export class TradedGoodsComponent extends BasePage implements OnInit {
   form: FormGroup = new FormGroup({});
 
   today: Date;
@@ -25,6 +24,7 @@ export class TradedGoodsComponent implements OnInit {
     private modalService: BsModalService,
     private sanitizer: DomSanitizer
   ) {
+    super();
     this.today = new Date();
     this.minDate = new Date(this.today.getFullYear(), this.today.getMonth(), 2);
   }
@@ -35,17 +35,58 @@ export class TradedGoodsComponent implements OnInit {
 
   private prepareForm() {
     this.form = this.fb.group({
-      delegation: [
-        null,
-        [Validators.required, Validators.pattern(STRING_PATTERN)],
-      ],
-      subDelegation: [
-        null,
-        [Validators.required, Validators.pattern(STRING_PATTERN)],
-      ],
+      delegation: [null, [Validators.required]],
+      subdelegation: [null, [Validators.required]],
       typeEvent: [null, [Validators.required]],
-      rangeDate: [null, [Validators.required, maxDate(new Date())]],
+      PF_FECINI: [null, [Validators.required]],
+      PF_FECFIN: [null, [Validators.required]],
     });
+  }
+  confirm() {
+    let params = {
+      PN_DELEGACION: this.form.controls['delegation'].value,
+      PN_SUBDELEGACION: this.form.controls['subdelegation'].value,
+      TIPO: this.form.controls['typeEvent'].value,
+      PF_FECINI: this.form.controls['PF_FECINI'].value,
+      PF_FECFIN: this.form.controls['PF_FECFIN'].value,
+    };
+    console.log(params);
+
+    const start = new Date(this.form.get('PF_FECINI').value);
+    const end = new Date(this.form.get('PF_FECFIN').value);
+
+    const startTemp = `${start.getFullYear()}-0${
+      start.getUTCMonth() + 1
+    }-0${start.getDate()}`;
+    const endTemp = `${end.getFullYear()}-0${
+      end.getUTCMonth() + 1
+    }-0${end.getDate()}`;
+
+    if (end < start) {
+      this.onLoadToast(
+        'warning',
+        'advertencia',
+        'fecha de final no puede ser menor a fecha inicial'
+      );
+      return;
+    }
+
+    setTimeout(() => {
+      this.onLoadToast('success', 'procesando', '');
+    }, 1000);
+    //const pdfurl = `http://reportsqa.indep.gob.mx/jasperserver/rest_v2/reports/SIGEBI/Reportes/SIAB/RGERDESBIECOMERCI.pdf?PN_DELEGACION=${params.PN_DELEGACION}&PN_SUBDELEGACION=${params.PN_SUBDELEGACION}&TIPO=${params.TIPO}&PF_FECINI=${params.PF_FECINI}&PF_FECFIN=${params.PF_FECFIN}`;
+    const pdfurl = `https://drive.google.com/file/d/1o3IASuVIYb6CPKbqzgtLcxx3l_V5DubV/view?usp=sharing`; //window.URL.createObjectURL(blob);
+    window.open(pdfurl, 'RGERDESBIECOMERCI.pdf');
+    setTimeout(() => {
+      this.onLoadToast('success', 'Reporte generado', '');
+    }, 2000);
+
+    this.loading = false;
+    this.cleanForm();
+  }
+
+  cleanForm(): void {
+    this.form.reset();
   }
 
   openPrevPdf() {
