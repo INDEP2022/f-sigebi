e9e908a3ef6a8850ae2352f64fbf94e618c5c8ce
Jessica Orozco <jessicaorozco@gmail.com>
2023-03-15 15:58:14 -0400

    update moal publication-photo

---
+++ src/app/pages/commercialization/publication-photographs/publication-photographs-modal/publication-photographs-modal.component.ts
@@ -0,0 +1,131 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalRef } from 'ngx-bootstrap/modal';
+import { BehaviorSubject } from 'rxjs';
+import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import {
+  IComerLayouts,
+  IL,
+} from 'src/app/core/models/ms-parametercomer/parameter';
+import { LayoutsConfigService } from 'src/app/core/services/ms-parametercomer/layouts-config.service';
+import { BasePage } from 'src/app/core/shared/base-page';
+import { STRING_PATTERN } from 'src/app/core/shared/patterns';
+
+@Component({
+  selector: 'app-publication-photographs-modal',
+  templateUrl: './publication-photographs-modal.component.html',
+  styles: [],
+})
+export class PublicationPhotographsModalComponent
+  extends BasePage
+  implements OnInit
+{
+  title: string = 'Configuración de Layouts';
+  provider: any;
+  params = new BehaviorSubject<ListParams>(new ListParams());
+  totalItems: number = 0;
+  edit: boolean = false;
+  structureLayoutSelected: any;
+  providerForm: FormGroup = new FormGroup({});
+  id: number = 0;
+  layoutsT: IComerLayouts;
+  layout: IL;
+  layoutList: IComerLayouts[] = [];
+  @Output() onConfirm = new EventEmitter<any>();
+  @Input() structureLayout: any;
+
+  constructor(
+    private modalRef: BsModalRef,
+    private fb: FormBuilder,
+    private layoutsConfigService: LayoutsConfigService
+  ) {
+    super();
+  }
+  ngOnInit(): void {
+    this.prepareForm();
+    this.structureLayoutSelected = this.structureLayout;
+    // this.inpuLayout = this.idLayout.toString().toUpperCase();
+    console.log(this.structureLayoutSelected);
+  }
+
+  private prepareForm(): void {
+    this.providerForm = this.fb.group({
+      idLayout: [null],
+      idConsec: [null],
+      position: [
+        null,
+        [Validators.required, Validators.pattern(STRING_PATTERN)],
+      ],
+      column: [null, [Validators.required, Validators.pattern(STRING_PATTERN)]],
+      // mes: [null, [Validators.required, Validators.pattern(STRING_PATTERN)]],
+      type: [null, [Validators.required]],
+      length: [new Date()],
+      constant: [null, [Validators.required]],
+      carFilling: [null, [Validators.required]],
+      justification: [null, [Validators.required]],
+      decimal: [null, [Validators.required]],
+      dateFormat: [null, [Validators.required]],
+      registryNumber: [null, [Validators.required]],
+    });
+    if (this.provider !== undefined) {
+      this.edit = true;
+      this.providerForm.patchValue(this.provider);
+    } else {
+      this.edit = false;
+    }
+  }
+
+  close() {
+    this.modalRef.hide();
+  }
+
+  confirm() {
+    this.edit ? this.update() : this.create();
+  }
+
+  create() {
+    try {
+      this.loading = false;
+      this.layoutsConfigService.create(this.structureLayout).subscribe({
+        next: data => this.handleSuccess(),
+        error: error => {
+          this.loading = false;
+          this.onLoadToast('error', 'No se puede duplicar layout!!', '');
+          return;
+        },
+      });
+    } catch {
+      console.error('Layout no existe');
+    }
+  }
+  update() {
+    this.alertQuestion(
+      'warning',
+      'Actualizar',
+      'Desea actualizar este layout?'
+    ).then(question => {
+      if (question.isConfirmed) {
+        this.layoutsConfigService
+          .update(this.provider.id, this.providerForm.value)
+          .subscribe({
+            next: data => this.handleSuccess(),
+            error: error => {
+              this.onLoadToast('error', 'layout', '');
+              this.loading = false;
+            },
+          });
+      }
+    });
+  }
+
+  handleSuccess() {
+    const message: string = this.edit ? 'Actualizado' : 'Guardado';
+    setTimeout(() => {
+      this.onLoadToast('success', this.title, `${message} Correctamente`);
+    }, 2000);
+    this.loading = false;
+    this.onConfirm.emit(true);
+    this.modalRef.content.callback(true);
+    this.close();
+  }
+}
--- src/app/pages/commercialization/publication-photographs/publication-photographs/publication-photographs.component.ts
+++ src/app/pages/commercialization/publication-photographs/publication-photographs/publication-photographs.component.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LocalDataSource } from 'ng2-smart-table';
-import { BsModalRef } from 'ngx-bootstrap/modal';
+import { BsModalService } from 'ngx-bootstrap/modal';
 import { BehaviorSubject } from 'rxjs';
+import { MODAL_CONFIG } from 'src/app/common/constants/modal-config';
 import { TABLE_SETTINGS } from 'src/app/common/constants/table-settings';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
 import { IPhotographMedia } from 'src/app/core/models/catalogs/photograph-media.model';
@@ -11,6 +12,7 @@ import { GoodSubtypeService } from 'src/app/core/services/catalogs/good-subtype.
 import { PhotographMediaService } from 'src/app/core/services/catalogs/photograph-media.service';
 import { BasePage } from 'src/app/core/shared/base-page';
 import { DefaultSelect } from 'src/app/shared/components/select/default-select';
+import { PublicationPhotographsModalComponent } from '../publication-photographs-modal/publication-photographs-modal.component';
 import {
   dataBatchColum,
   numStoreColumn,
@@ -38,6 +40,7 @@ export class PublicationPhotographsComponent
   form: FormGroup = new FormGroup({});
   dataBatch: any;
   subtype: any;
+  title = 'Fotografías';
   params = new BehaviorSubject<ListParams>(new ListParams());
   batchList: any;
   selectedCve: any = null;
@@ -67,7 +70,7 @@ export class PublicationPhotographsComponent
     private fb: FormBuilder,
     private batchService: BatchService,
     private photographMediaService: PhotographMediaService,
-    private modalRef: BsModalRef,
+    private modalService: BsModalService,
     private goodSubtypeService: GoodSubtypeService
   ) {
     super();
@@ -187,34 +190,53 @@ export class PublicationPhotographsComponent
     });
   }
 
-  confirm() {
-    this.loading = false;
-    this.photographMediaService.create(this.form.value).subscribe({
-      next: data => {
-        this.picture = data;
-        console.log(this.picture);
-        this.rowSelected = true;
-        this.handleSuccess();
+  openForm(provider?: IPhotographMedia) {
+    const modalConfig = MODAL_CONFIG;
+    modalConfig.initialState = {
+      provider,
+      callback: (next: boolean) => {
+        if (next) this.getBatch();
       },
-      error: error => {
-        this.loading = false;
-        this.onLoadToast('error', 'No se puede duplicar layout!!', '');
-        return;
-      },
-    });
+    };
+    this.modalService.show(PublicationPhotographsModalComponent, modalConfig);
   }
 
-  handleSuccess() {
-    setTimeout(() => {
-      this.onLoadToast('success', 'fotografía creada correctamente', '');
-    }, 2000);
-    this.loading = false;
-    this.onConfirm.emit(true);
-    this.modalRef.content.callback(true);
-    this.close();
+  openModal(context?: Partial<PublicationPhotographsModalComponent>) {
+    const modalRef = this.modalService.show(
+      PublicationPhotographsModalComponent,
+      {
+        initialState: { ...context },
+        class: 'modal-lg modal-dialog-centered',
+        ignoreBackdropClick: true,
+      }
+    );
   }
-  close() {
-    this.modalRef.hide();
+
+  showIdLayout(event: any) {
+    //enseña lo que elegistes en el input
+    console.log(event.idLayout);
+    console.log(event.idLayout);
+  }
+  showDeleteAlert(id: number) {
+    this.alertQuestion(
+      'warning',
+      'Eliminar',
+      'Desea eliminar este registro?'
+    ).then(question => {
+      if (question.isConfirmed) {
+        this.photographMediaService.remove(id).subscribe({
+          next: data => {
+            this.loading = false;
+            this.onLoadToast('success', 'Layout eliminado', '');
+            this.getBatch();
+          },
+          error: error => {
+            this.onLoadToast('error', 'No se puede eliminar registro', '');
+            this.loading = false;
+          },
+        });
+      }
+    });
   }
 
   //Carrusel de fotografías
