d5b61914d7a7a6130be900523f66865a675fbee9
afandresHerrera <afandres19@gmail.com>
2023-03-15 19:16:02 -0500

    Add report rResumenCostos

--- src/app/core/services/ms-spent/good-spent.service.ts
+++ src/app/core/services/ms-spent/good-spent.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListParams } from 'src/app/common/repository/interfaces/list-params';
 import { HttpService, _Params } from 'src/app/common/services/http.service';
 import { IListResponse } from '../../interfaces/list-response.interface';
+import { IExpenseConcept } from '../../models/ms-spent/expense-concept.model';
 import { IGoodSpent } from '../../models/ms-spent/good-spent.model';
 
 @Injectable({
@@ -14,5 +17,11 @@ export class GoodSpentService extends HttpService {
 
   getGoodSpents(body: Object, params: _Params) {
     return this.post<IListResponse<IGoodSpent>>('good-spent', body, params);
+  }
+
+  getExpenseConcept(
+    params?: ListParams
+  ): Observable<IListResponse<IExpenseConcept>> {
+    return this.get<IListResponse<IExpenseConcept>>(`/expense-concept`, params);
   }
 }
