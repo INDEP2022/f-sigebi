b50d9aeaed3785875ae4ad4f38bfba27ee925c0b
genaroadrian <al221711695@gmail.com>
2023-03-20 19:14:26 -0600

    buzon de tramite genaro

--- src/app/pages/general-processes/work-mailbox/work-mailbox.module.ts
+++ src/app/pages/general-processes/work-mailbox/work-mailbox.module.ts
@@ -4,11 +4,12 @@ import { SharedModule } from 'src/app/shared/shared.module';
 //@Standalone Components
 import { ManagementAreaSharedComponent } from 'src/app/@standalone/shared-forms/management-area-shared/management-area-shared.component';
 
+import { MailboxModalTableComponent } from './components/mailbox-modal-table/mailbox-modal-table.component';
 import { WorkMailboxRoutingModule } from './work-mailbox-routing.module';
 import { WorkMailboxComponent } from './work-mailbox/work-mailbox.component';
 
 @NgModule({
-  declarations: [WorkMailboxComponent],
+  declarations: [WorkMailboxComponent, MailboxModalTableComponent],
   imports: [
     CommonModule,
     WorkMailboxRoutingModule,
