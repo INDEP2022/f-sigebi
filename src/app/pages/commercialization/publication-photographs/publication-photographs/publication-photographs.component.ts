3bf8c5bc2bb96dd6af5de972ba03e58af06b9af0
Jessica Orozco <jessicaorozco@gmail.com>
2023-03-23 16:48:11 -0400

    update find by loteid

--- src/app/pages/commercialization/publication-photographs/publication-photographs/publication-photographs.component.ts
+++ src/app/pages/commercialization/publication-photographs/publication-photographs/publication-photographs.component.ts
@@ -1,27 +1,23 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LocalDataSource } from 'ng2-smart-table';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BehaviorSubject } from 'rxjs';
-import { MODAL_CONFIG } from 'src/app/common/constants/modal-config';
 import { TABLE_SETTINGS } from 'src/app/common/constants/table-settings';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
-import { IPhotographMedia } from 'src/app/core/models/catalogs/photograph-media.model';
+
 import {
   IComerLot,
   IGoodPhoto,
 } from 'src/app/core/models/ms-parametercomer/parameter';
-import { BatchService } from 'src/app/core/services/catalogs/batch.service';
-import { GoodSubtypeService } from 'src/app/core/services/catalogs/good-subtype.service';
-import { PhotographMediaService } from 'src/app/core/services/catalogs/photograph-media.service';
+
 import { BasePage } from 'src/app/core/shared/base-page';
 import { DefaultSelect } from 'src/app/shared/components/select/default-select';
-import { PublicationPhotographsModalComponent } from '../publication-photographs-modal/publication-photographs-modal.component';
 import {
+  GoodPhoto,
+  Lot,
   PUBLICATION_PHOTO1,
   PUBLICATION_PHOTO2,
-  SUBTYPE,
-  TYPE,
 } from './publication-photographs-columns';
 
 import { ComerLotService } from 'src/app/core/services/ms-parametercomer/comer-lot.service';
@@ -55,7 +51,6 @@ export class PublicationPhotographsComponent
   selectedCve: any = null;
   cveItems = new DefaultSelect();
   totalItems: number = 0;
-  picture: IPhotographMedia;
   data1: LocalDataSource = new LocalDataSource();
   // dataAllotment = DATA;
   idLot: number = 0;
@@ -77,10 +72,7 @@ export class PublicationPhotographsComponent
 
   constructor(
     private fb: FormBuilder,
-    private batchService: BatchService,
-    private photographMediaService: PhotographMediaService,
     private modalService: BsModalService,
-    private goodSubtypeService: GoodSubtypeService,
     private comerLotService: ComerLotService
   ) {
     super();
@@ -98,14 +90,14 @@ export class PublicationPhotographsComponent
 
     this.settings4 = {
       ...TABLE_SETTINGS,
-      columns: { ...SUBTYPE },
+      columns: { ...Lot },
       noDataMessage: 'No se encontrarón registros',
     };
 
     this.settings3 = {
       ...TABLE_SETTINGS,
       actions: false,
-      columns: { ...TYPE },
+      columns: { ...GoodPhoto },
       noDataMessage: 'No se encontrarón registros',
     };
   }
@@ -117,11 +109,12 @@ export class PublicationPhotographsComponent
     this.settings4.actions.delete = true;
     this.settings4.actions.edit = true;
     this.getAllLot();
+    this.getAllLot();
   }
 
   prepareForm() {
     this.form = this.fb.group({
-      id: [null],
+      id: [null, [Validators.required]],
       address: [null],
       failureDate: [null],
       place: [null],
@@ -155,9 +148,7 @@ export class PublicationPhotographsComponent
 
   findEvent(x: any) {
     // this.form.value.price = this.form.controls['price'].value;
-
     let eventId = this.form.value.id;
-
     if (this.form.value.eventId !== null) {
       this.comerLotService.getById(eventId).subscribe({
         next: data => {
@@ -169,34 +160,34 @@ export class PublicationPhotographsComponent
     }
   }
 
-  getBatch() {
-    this.loading = true;
-    this.batchService.getAll(this.params.getValue()).subscribe({
-      next: data => {
-        this.batchList = data;
-        this.dataBatch = this.batchList.data;
-        this.totalItems = data.count;
-        console.log(this.dataBatch);
-        this.loading = false;
-      },
-      error: error => (this.loading = false),
-    });
-  }
-  getSubtype() {
-    this.loading = true;
-    this.goodSubtypeService.getAll(this.params.getValue()).subscribe({
-      next: data => {
-        this.subtype = data;
-        this.dataBatch = this.subtype.data;
-        this.totalItems = data.count;
-        console.log(this.dataBatch);
-        this.loading = false;
-      },
-      error: error => (this.loading = false),
-    });
-  }
+  // getBatch() {
+  //   this.loading = true;
+  //   this.batchService.getAll(this.params.getValue()).subscribe({
+  //     next: data => {
+  //       this.batchList = data;
+  //       this.dataBatch = this.batchList.data;
+  //       this.totalItems = data.count;
+  //       console.log(this.dataBatch);
+  //       this.loading = false;
+  //     },
+  //     error: error => (this.loading = false),
+  //   });
+  // }
+  // getSubtype() {
+  //   this.loading = true;
+  //   this.goodSubtypeService.getAll(this.params.getValue()).subscribe({
+  //     next: data => {
+  //       this.subtype = data;
+  //       this.dataBatch = this.subtype.data;
+  //       this.totalItems = data.count;
+  //       console.log(this.dataBatch);
+  //       this.loading = false;
+  //     },
+  //     error: error => (this.loading = false),
+  //   });
+  // }
   getAllLot() {
-    this.comerLotService.getAll(this.params.getValue()).subscribe({
+    this.comerLotService.getAll().subscribe({
       next: data => {
         this.lotList = data.data;
         this.totalItems = data.count;
@@ -232,54 +223,49 @@ export class PublicationPhotographsComponent
 
   getAllGoodPhoto() {}
 
-  openForm(provider?: IPhotographMedia) {
-    const modalConfig = MODAL_CONFIG;
-    modalConfig.initialState = {
-      provider,
-      callback: (next: boolean) => {
-        if (next) this.getBatch();
-      },
-    };
-    this.modalService.show(PublicationPhotographsModalComponent, modalConfig);
-  }
+  // openForm(provider?: IPhotographMedia) {
+  //   const modalConfig = MODAL_CONFIG;
+  //   modalConfig.initialState = {
+  //     provider,
+  //     callback: (next: boolean) => {
+  //       if (next) this.getBatch();
+  //     },
+  //   };
+  //   this.modalService.show(PublicationPhotographsModalComponent, modalConfig);
+  // }
 
-  openModal(context?: Partial<PublicationPhotographsModalComponent>) {
-    const modalRef = this.modalService.show(
-      PublicationPhotographsModalComponent,
-      {
-        initialState: { ...context },
-        class: 'modal-lg modal-dialog-centered',
-        ignoreBackdropClick: true,
-      }
-    );
-  }
+  // openModal(context?: Partial<PublicationPhotographsModalComponent>) {
+  //   const modalRef = this.modalService.show(
+  //     PublicationPhotographsModalComponent,
+  //     {
+  //       initialState: { ...context },
+  //       class: 'modal-lg modal-dialog-centered',
+  //       ignoreBackdropClick: true,
+  //     }
+  //   );
+  // }
 
-  showIdLayout(event: any) {
-    //enseña lo que elegistes en el input
-    console.log(event.idLayout);
-    console.log(event.idLayout);
-  }
-  showDeleteAlert(id: number) {
-    this.alertQuestion(
-      'warning',
-      'Eliminar',
-      'Desea eliminar este registro?'
-    ).then(question => {
-      if (question.isConfirmed) {
-        this.photographMediaService.remove(id).subscribe({
-          next: data => {
-            this.loading = false;
-            this.onLoadToast('success', 'Layout eliminado', '');
-            this.getBatch();
-          },
-          error: error => {
-            this.onLoadToast('error', 'No se puede eliminar registro', '');
-            this.loading = false;
-          },
-        });
-      }
-    });
-  }
+  // showDeleteAlert(id: number) {
+  //   this.alertQuestion(
+  //     'warning',
+  //     'Eliminar',
+  //     'Desea eliminar este registro?'
+  //   ).then(question => {
+  //     if (question.isConfirmed) {
+  //       this.photographMediaService.remove(id).subscribe({
+  //         next: data => {
+  //           this.loading = false;
+  //           this.onLoadToast('success', 'Layout eliminado', '');
+  //           this.getBatch();
+  //         },
+  //         error: error => {
+  //           this.onLoadToast('error', 'No se puede eliminar registro', '');
+  //           this.loading = false;
+  //         },
+  //       });
+  //     }
+  //   });
+  // }
 
   //Carrusel de fotografías
   itemsPerSlide = 5;
