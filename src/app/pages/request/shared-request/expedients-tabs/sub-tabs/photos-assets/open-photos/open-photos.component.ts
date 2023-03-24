22d3cf3906d0e64336137cd99dc0b52895ca6ced
genaroadrian <al221711695@gmail.com>
2023-03-23 20:47:27 -0600

    Show images and determine the mime type from the base 64

--- src/app/pages/request/shared-request/expedients-tabs/sub-tabs/photos-assets/open-photos/open-photos.component.ts
+++ src/app/pages/request/shared-request/expedients-tabs/sub-tabs/photos-assets/open-photos/open-photos.component.ts
@@ -42,7 +42,7 @@ export class OpenPhotosComponent extends BasePage implements OnInit {
       onComponentInitFunction: (instance?: any) => {
         instance.btnclick.subscribe((data: any) => {
           console.log(data.dDocName);
-          this.getImage(data.dDocNam);
+          this.getImage(data.dDocName);
         });
       },
     };
@@ -67,22 +67,22 @@ export class OpenPhotosComponent extends BasePage implements OnInit {
 
   getImage(docName: string) {
     this.wContentService.getObtainFile(docName).subscribe(data => {
-      console.log('data', data);
-      let blob = this.dataURItoBlob(data);
-      let file = new Blob([blob], { type: 'image/jpg' });
+      const type = this.detectMimeType(data);
+      let blob = this.dataURItoBlob(data, type);
+      let file = new Blob([blob], { type });
       const fileURL = URL.createObjectURL(file);
       this.openPrevImg(fileURL);
     });
   }
 
-  dataURItoBlob(dataURI: any) {
+  dataURItoBlob(dataURI: any, type: string) {
     const byteString = window.atob(dataURI);
     const arrayBuffer = new ArrayBuffer(byteString.length);
     const int8Array = new Uint8Array(arrayBuffer);
     for (let i = 0; i < byteString.length; i++) {
       int8Array[i] = byteString.charCodeAt(i);
     }
-    const blob = new Blob([int8Array], { type: 'image/jpg' });
+    const blob = new Blob([int8Array], { type });
     return blob;
   }
 
@@ -103,5 +103,50 @@ export class OpenPhotosComponent extends BasePage implements OnInit {
 
   close(): void {
     this.bsModalRef.hide();
+  }
+
+  detectMimeType(base64String: string, fileName = 'unamedfile') {
+    let ext = fileName.substring(fileName.lastIndexOf('.') + 1);
+    if (ext === undefined || ext === null || ext === '') ext = 'bin';
+    ext = ext.toLowerCase();
+    const signatures: any = {
+      JVBERi0: 'application/pdf',
+      R0lGODdh: 'image/gif',
+      R0lGODlh: 'image/gif',
+      iVBORw0KGgo: 'image/png',
+      TU0AK: 'image/tiff',
+      '/9j/': 'image/jpg',
+      UEs: 'application/vnd.openxmlformats-officedocument.',
+      PK: 'application/zip',
+    };
+    for (const s in signatures) {
+      if (base64String.indexOf(s) === 0) {
+        let x = signatures[s];
+        if (ext.length > 3 && ext.substring(0, 3) === 'ppt') {
+          x += 'presentationml.presentation';
+        } else if (ext.length > 3 && ext.substring(0, 3) === 'xls') {
+          x += 'spreadsheetml.sheet';
+        } else if (ext.length > 3 && ext.substring(0, 3) === 'doc') {
+          x += 'wordprocessingml.document';
+        }
+        return x;
+      }
+    }
+    const extensions: any = {
+      xls: 'application/vnd.ms-excel',
+      ppt: 'application/vnd.ms-powerpoint',
+      doc: 'application/msword',
+      xml: 'text/xml',
+      mpeg: 'audio/mpeg',
+      mpg: 'audio/mpeg',
+      txt: 'text/plain',
+    };
+    for (const e in extensions) {
+      if (ext.indexOf(e) === 0) {
+        const xx = extensions[e];
+        return xx;
+      }
+    }
+    return 'unknown';
   }
 }
