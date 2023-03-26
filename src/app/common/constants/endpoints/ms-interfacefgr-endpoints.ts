edeae35fa813ae113e8dbfca7ed87eeb8e620edc
JRicardoHernandez <riiiicho@gmail.com>
2023-03-26 04:55:31 +0200

    Ajustes en carga masiva PGR

--- src/app/common/constants/endpoints/ms-interfacefgr-endpoints.ts
+++ src/app/common/constants/endpoints/ms-interfacefgr-endpoints.ts
@@ -5,4 +5,5 @@ export enum InterfacefgrEndPoints {
   cityByAsuntoSat = 'pgr-transfer/get-city-number',
   PgrTransferFiltered = `pgr-transfer`,
   PgrFaValAtrib1 = 'pgr-transfer/get-info-by-fa-val-attrb-1',
+  OTkeyByAsunto = `pgr-transfer/get-otkey`,
 }
