fc092a6f7bb0698205a144ef540f74182ff0c0a3
JRicardoHernandez <riiiicho@gmail.com>
2023-03-26 03:09:02 +0200

    Ajustes en carga masiva PGR

--- src/app/core/services/ms-interfacefgr/ms-interfacefgr.service.ts
+++ src/app/core/services/ms-interfacefgr/ms-interfacefgr.service.ts
@@ -4,7 +4,10 @@ import { InterfacefgrEndPoints } from 'src/app/common/constants/endpoints/ms-int
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
 import { HttpService } from 'src/app/common/services/http.service';
 import { IListResponse } from '../../interfaces/list-response.interface';
-import { IPgrTransfer } from '../../models/ms-interfacefgr/ms-interfacefgr.interface';
+import {
+  IFaValAtributo1,
+  IPgrTransfer,
+} from '../../models/ms-interfacefgr/ms-interfacefgr.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -62,5 +65,12 @@ export class InterfacefgrService extends HttpService {
 
   getCityByAsuntoSat(body: { pgrOffice: string }) {
     return this.post(InterfacefgrEndPoints.cityByAsuntoSat, body);
+  }
+
+  getFaValAtributo1(body: IFaValAtributo1) {
+    return this.post<IListResponse<IFaValAtributo1>>(
+      InterfacefgrEndPoints.PgrFaValAtrib1,
+      body
+    );
   }
 }
