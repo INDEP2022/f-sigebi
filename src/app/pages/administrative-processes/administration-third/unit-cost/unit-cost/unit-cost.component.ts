85c4c74638892a6c6db2ce0371f7c8489017fba4
edsonrosales6 <edsonrosales123@gmail.com>
2023-03-28 17:52:12 -0600

    se agregan servicios y modelos para unit cost y unit cost det

--- src/app/pages/administrative-processes/administration-third/unit-cost/unit-cost/unit-cost.component.ts
+++ src/app/pages/administrative-processes/administration-third/unit-cost/unit-cost/unit-cost.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { LocalDataSource } from 'ng2-smart-table';
 import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { BehaviorSubject, takeUntil } from 'rxjs';
@@ -6,7 +7,9 @@ import {
   ListParams,
   SearchFilter,
 } from 'src/app/common/repository/interfaces/list-params';
+import { IUnitCostDet } from 'src/app/core/models/administrative-processes/unit-cost-det.model';
 import { IUnitCost } from 'src/app/core/models/administrative-processes/unit-cost.model';
+import { UnitCostDetService } from 'src/app/core/services/unit-cost/unit-cost-det.service';
 import { UnitCostService } from 'src/app/core/services/unit-cost/unit-cost.service';
 import { BasePage } from 'src/app/core/shared/base-page';
 import Swal from 'sweetalert2';
@@ -19,18 +22,24 @@ import { COSTKEY_COLUMNS, VALIDITYCOST_COLUMNS } from './unit-cost-columns';
   styles: [],
 })
 export class UnitCostComponent extends BasePage implements OnInit {
+  form: FormGroup = new FormGroup({});
   data1: LocalDataSource = new LocalDataSource();
-  params = new BehaviorSubject<ListParams>(new ListParams());
-  totalItems: number = 0;
-  settings1 = { ...this.settings, actions: false };
-  data2: any[] = [];
-  columns: IUnitCost[] = [];
+  data2: LocalDataSource = new LocalDataSource();
   params1 = new BehaviorSubject<ListParams>(new ListParams());
+  params2 = new BehaviorSubject<ListParams>(new ListParams());
+  settings1 = { ...this.settings, actions: false };
+  settings2 = { ...this.settings, actions: {} };
+  columns1: IUnitCost[] = [];
+  columns2: IUnitCostDet[] = [];
   totalItems1: number = 0;
-  columnFilters: any = [];
+  totalItems2: number = 0;
+
+  columnFilters1: any = [];
+  columnFilters2: any = [];
 
   constructor(
     private unitCostService: UnitCostService,
+    private unitCostDetService: UnitCostDetService,
     private modalService: BsModalService
   ) {
     super();
@@ -45,7 +54,17 @@ export class UnitCostComponent extends BasePage implements OnInit {
       },
       columns: COSTKEY_COLUMNS,
     };
-    this.settings1.columns = VALIDITYCOST_COLUMNS;
+    this.settings2 = {
+      ...this.settings,
+      actions: {
+        columnTitle: 'Acciones',
+        edit: false,
+        add: false,
+        delete: true,
+        position: 'right',
+      },
+      columns: VALIDITYCOST_COLUMNS,
+    };
   }
 
   ngOnInit(): void {
@@ -64,16 +83,31 @@ export class UnitCostComponent extends BasePage implements OnInit {
               case 'processNumber':
                 searchFilter = SearchFilter.EQ;
                 break;
+              case 'process':
+                searchFilter = SearchFilter.EQ;
+                break;
               case 'serviceNumber':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'service':
                 searchFilter = SearchFilter.EQ;
                 break;
               case 'serviceTypeNumber':
                 searchFilter = SearchFilter.EQ;
                 break;
+              case 'serviceType':
+                searchFilter = SearchFilter.EQ;
+                break;
               case 'shiftNumber':
                 searchFilter = SearchFilter.EQ;
                 break;
+              case 'shift':
+                searchFilter = SearchFilter.EQ;
+                break;
               case 'varCostNumber':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'varCost':
                 searchFilter = SearchFilter.EQ;
                 break;
               default:
@@ -82,31 +116,106 @@ export class UnitCostComponent extends BasePage implements OnInit {
             }
 
             if (filter.search !== '') {
-              this.columnFilters[field] = `${searchFilter}:${filter.search}`;
+              this.columnFilters1[field] = `${searchFilter}:${filter.search}`;
             } else {
-              delete this.columnFilters[field];
+              delete this.columnFilters1[field];
             }
           });
           this.getUnitCostAll();
         }
       });
-    this.params
+    this.params1
       .pipe(takeUntil(this.$unSubscribe))
       .subscribe(() => this.getUnitCostAll());
+
+    this.data2
+      .onChanged()
+      .pipe(takeUntil(this.$unSubscribe))
+      .subscribe(change => {
+        if (change.action === 'filter') {
+          let filters = change.filter.filters;
+          filters.map((filter: any) => {
+            let field = ``;
+            let searchFilter = SearchFilter.ILIKE;
+            field = `filter.${filter.field}`;
+            /*SPECIFIC CASES*/
+            switch (filters.field) {
+              case 'costId':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'cveZoneContract':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'startDate':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'finalDate':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'costUnitarian':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'porceInflation':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'amount':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'vig':
+                searchFilter = SearchFilter.EQ;
+                break;
+              default:
+                searchFilter = SearchFilter.ILIKE;
+                break;
+            }
+
+            if (filter.search !== '') {
+              this.columnFilters2[field] = `${searchFilter}:${filter.search}`;
+            } else {
+              delete this.columnFilters2[field];
+            }
+          });
+          this.getUnitCostDetAll();
+        }
+      });
+    this.params2
+      .pipe(takeUntil(this.$unSubscribe))
+      .subscribe(() => this.getUnitCostDetAll());
+  }
+
+  getUnitCostDetAll() {
+    this.loading = true;
+    let params = {
+      ...this.params2.getValue(),
+      ...this.columnFilters2,
+    };
+
+    this.unitCostDetService.getAll(params).subscribe({
+      next: response => {
+        this.columns2 = response.data;
+        this.data2.load(this.columns2);
+        this.totalItems2 = response.count || 0;
+        this.data2.refresh();
+        this.loading = false;
+      },
+      error: error => {
+        this.loading = false;
+      },
+    });
   }
 
   getUnitCostAll() {
     this.loading = true;
     let params = {
-      ...this.params.getValue(),
-      ...this.columnFilters,
+      ...this.params1.getValue(),
+      ...this.columnFilters1,
     };
 
     this.unitCostService.getAll(params).subscribe({
       next: response => {
-        this.columns = response.data;
-        this.data1.load(this.columns);
-        this.totalItems = response.count || 0;
+        this.columns1 = response.data;
+        this.data1.load(this.columns1);
+        this.totalItems1 = response.count || 0;
         this.data1.refresh();
         this.loading = false;
       },
@@ -130,14 +239,27 @@ export class UnitCostComponent extends BasePage implements OnInit {
     this.modalService.show(UnitCostFormComponent, config);
   }
 
-  showDeleteAlert(contract?: IUnitCost) {
+  showDeleteAlert(unitCost?: IUnitCost) {
     this.alertQuestion(
       'warning',
       'Eliminar',
       '¿Desea borrar este registro?'
     ).then(question => {
       if (question.isConfirmed) {
-        this.delete(contract.costId);
+        this.delete(unitCost.costId);
+        Swal.fire('Borrado', '', 'success');
+      }
+    });
+  }
+
+  showDeleteAlert2(unitCostDet?: IUnitCostDet) {
+    this.alertQuestion(
+      'warning',
+      'Eliminar',
+      '¿Desea borrar este registro?'
+    ).then(question => {
+      if (question.isConfirmed) {
+        this.delete(unitCostDet.costId);
         Swal.fire('Borrado', '', 'success');
       }
     });
@@ -147,5 +269,31 @@ export class UnitCostComponent extends BasePage implements OnInit {
     this.unitCostService.remove(id).subscribe({
       next: () => this.getUnitCostAll(),
     });
+  }
+
+  delete2(id: number) {
+    this.unitCostDetService.remove(id).subscribe({
+      next: () => this.getUnitCostDetAll(),
+    });
+  }
+
+  confirm(): void {
+    this.loading = true;
+    const pdfurl = `http://reportsqa.indep.gob.mx/jasperserver/rest_v2/reports/SIGEBI/Reportes/blank.pdf`; //window.URL.createObjectURL(blob);
+
+    const downloadLink = document.createElement('a');
+    //console.log(linkSource);
+    downloadLink.href = pdfurl;
+    downloadLink.target = '_blank';
+    downloadLink.click();
+
+    // console.log(this.flyersForm.value);
+    let params = { ...this.form.value };
+    for (const key in params) {
+      if (params[key] === null) delete params[key];
+    }
+    //let newWin = window.open(pdfurl, 'test.pdf');
+    this.onLoadToast('success', '', 'Reporte generado');
+    this.loading = false;
   }
 }
