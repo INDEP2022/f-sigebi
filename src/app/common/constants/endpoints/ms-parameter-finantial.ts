5701e5ba20dcbc378ddded39527e9dd46545d90e
Jessica Orozco <jessicaorozco@gmail.com>
2023-04-03 08:39:15 -0400

    add integration indicator-peer-good- services, endpoint,model

--- src/app/common/constants/endpoints/ms-parameter-finantial.ts
+++ src/app/common/constants/endpoints/ms-parameter-finantial.ts
@@ -2,4 +2,5 @@ export enum ParameterFinantialEndpoints {
   BasePath = 'parameterfinantial',
   FinancialInfo = 'financial-information',
   FindFinantial = '?search=&filter.idGoodNumber=',
+  IndicatorGood = 'indicators-for-good',
 }
--- src/app/pages/administrative-processes/indicators-per-good/indicators-per-good/indicators-per-good.component.ts
+++ src/app/pages/administrative-processes/indicators-per-good/indicators-per-good/indicators-per-good.component.ts
@@ -1,7 +1,12 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import { IFinancialIndicatorsW } from 'src/app/core/models/catalogs/financial-indicators-model';
+import { IGood } from 'src/app/core/models/good/good.model';
+import { GoodService } from 'src/app/core/services/good/good.service';
+import { IndicatorPeerGoodService } from 'src/app/core/services/ms-parameter-finantial/indicator-peer-good.service';
 import { BasePage } from 'src/app/core/shared/base-page';
 import {
   INDICATORS_COLUMNS2,
@@ -16,14 +21,26 @@ import {
 export class IndicatorsPerGoodComponent extends BasePage implements OnInit {
   form: FormGroup;
   data1: any[] = [];
+  good: IGood;
+  indicatorsTotal: IFinancialIndicatorsW[] = [];
   params1 = new BehaviorSubject<ListParams>(new ListParams());
   totalItems1: number = 0;
   settings1 = { ...this.settings, actions: false };
   data2: any[] = [];
   params2 = new BehaviorSubject<ListParams>(new ListParams());
   totalItems2: number = 0;
-
-  constructor(private fb: FormBuilder) {
+  proficientOpinion: string;
+  valuerOpinion: string;
+  observations: string;
+  quantity: string;
+  description: string;
+  date: string;
+  constructor(
+    private fb: FormBuilder,
+    private goodService: GoodService,
+    private indicatorPeerGoodService: IndicatorPeerGoodService,
+    private datePipe: DatePipe
+  ) {
     super();
     this.settings1 = {
       ...this.settings,
@@ -41,6 +58,58 @@ export class IndicatorsPerGoodComponent extends BasePage implements OnInit {
     this.form = this.fb.group({
       good: [null, Validators.required],
       date: [null, Validators.required],
+    });
+  }
+  onChangeGood() {
+    this.searchGoods(this.form.value.noBien);
+  }
+  searchGoods(idGood: number | string) {
+    this.goodService.getById(idGood).subscribe({
+      next: response => {
+        this.good = response;
+        console.log(this.good);
+        // this.proficientOpinion = response.proficientOpinion;
+        // this.valuerOpinion = response.valuerOpinion;
+        // this.observations = response.observations;
+        // this.quantity = response.quantity;
+        // this.description = response.description;
+
+        // this.form.controls['dictaminatedBy'].setValue(this.proficientOpinion);
+        // this.form.controls['avaluo'].setValue(this.valuerOpinion);
+        // this.form.controls['observations'].setValue(this.observations);
+        this.data1.push(this.good);
+        console.log(this.data1);
+      },
+      error: err => {
+        this.onLoadToast('error', 'ERROR', 'Bien no existe');
+        this.form.reset();
+        console.log(err);
+      },
+    });
+  }
+  loadIndicator(search: any) {
+    this.indicatorPeerGoodService.findGood(search).subscribe({
+      next: response => {
+        this.indicatorsTotal = response;
+        console.log(this.indicatorsTotal);
+        this.indicatorsTotal.forEach(date => {
+          this.date = this.datePipe.transform(
+            date.idIndicatorDate,
+            'dd-MM-yyyy h:mm a'
+          );
+        });
+        this.searchGoods(this.form.value.noBien);
+        this.form.controls['date'].setValue(this.date);
+        this.totalItems2 = response.count;
+      },
+      error: err => {
+        this.onLoadToast(
+          'info',
+          'Opss..',
+          'Este bien no tiene indicadores asociado'
+        );
+        console.log(err);
+      },
     });
   }
 }
