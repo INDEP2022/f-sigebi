64ecddc0518f25916dc445ca4d1941cd5f6662c3
genaroadrian <al221711695@gmail.com>
2023-04-10 15:58:57 -0600

    Preview jasper reports

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,9 @@ export const environment = {
   API_URL: 'http://sigebimsqa.indep.gob.mx/', //'http://localhost:3000/api/v1/',
   API_URL2: 'http://sigebimsqa.indep.gob.mx/',
   URL_PREFIX: 'api/v1/',
+  API_REPORTS_BASE: 'http://reports-qa.indep.gob.mx/jasperserver/',
   API_REPORTS:
-    'http://reportsqa.indep.gob.mx/jasperserver/rest_v2/reports/SIGEBI/Reportes/',
+    'http://reports-qa.indep.gob.mx/jasperserver/rest_v2/reports/SIGEBI/Reportes/',
   API_REPORTS_USR: 'sigebiadmon',
   API_REPORTS_PSW: '87654321',
   api_external_token:
