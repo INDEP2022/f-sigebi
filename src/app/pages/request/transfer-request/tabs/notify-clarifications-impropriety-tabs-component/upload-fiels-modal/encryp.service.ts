6fdad18c76404da81778bcc6bad5ac0a9de57f9b
henrygiovannisanchezsolis <sshenrygiovanni@gmail.com>
2023-04-11 21:51:46 -0600

    firma

---
+++ src/app/pages/request/transfer-request/tabs/notify-clarifications-impropriety-tabs-component/upload-fiels-modal/encryp.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root',
+})
+export class EncrypService {
+  private url = 'http://wssiab.sae.gob.mx/WsFirmaElectronicades/wsFirma.asmx';
+  constructor(private http: HttpClient) {}
+
+  public encryp(parametro: any) {
+    const soapMessage = `<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+    <Body>
+        <encriptar xmlns="http://tempuri.org/">
+            <CadenaEncriptar>${parametro}</CadenaEncriptar>
+        </encriptar>
+    </Body>
+</Envelope>`;
+    const headers = {
+      'Access-Control-Allow-Origin': 'http://sigebi.indep.gob.mx:4200/',
+      'Content-Type': 'text/xml;charset=UTF-8',
+      Accept: '*/*',
+    };
+    return this.http.post(this.url, soapMessage, {
+      headers,
+      responseType: 'text',
+    });
+  }
+}
