b93e7129a6ad676e2e5265a20480618c8601db27
gus2612 <gustavoangelsantosclemente@gmail.com>
2023-04-16 21:03:00 -0600

    create document clarification

---
+++ src/app/core/models/notification-aclaration/notification-aclaration-model.ts
@@ -0,0 +1,14 @@
+export interface Inappropriateness {
+  managedTo: string;
+  positionSender: string;
+  positionAddressee: string;
+  sender: string;
+  clarification: string;
+  consistentIn: string;
+  paragraphInitial: string;
+  paragraphFinal: string;
+  areaUserCapture: string;
+  transmitterId: string;
+  mailNotification: string;
+  applicationId: string;
+}
