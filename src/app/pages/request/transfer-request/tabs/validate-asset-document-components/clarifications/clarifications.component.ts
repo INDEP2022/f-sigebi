b0654f9e382b37b9df66442817c7b87ede0c8451
cxg346 <dgm3036@gmail.com>
2023-04-20 18:04:28 -0500

    Validaciones para pasar bienes a acta

--- src/app/pages/request/transfer-request/tabs/validate-asset-document-components/clarifications/clarifications.component.ts
+++ src/app/pages/request/transfer-request/tabs/validate-asset-document-components/clarifications/clarifications.component.ts
@@ -57,6 +57,7 @@ export class ClarificationsComponent
   totalItems: number = 0;
 
   domicilieObject: any;
+  articleColumns = CLARIFICATION_COLUMNS;
 
   constructor(
     private modalService: BsModalService,
@@ -85,6 +86,16 @@ export class ClarificationsComponent
       selectMode: 'multi',
       columns: CLARIFICATION_COLUMNS,
     };
+    this.settings.columns = CLARIFICATION_COLUMNS;
+
+    this.articleColumns.reason = {
+      ...this.articleColumns.reason,
+      onComponentInitFunction: (instance?: any) => {
+        instance.input.subscribe((data: any) => {
+          console.log('data', data);
+        });
+      },
+    };
 
     this.goodSetting = {
       ...TABLE_SETTINGS,
@@ -92,6 +103,7 @@ export class ClarificationsComponent
       selectMode: 'multi',
       columns: ASSETS_COLUMNS,
     };
+
     this.prepareForm();
   }
   private prepareForm() {
