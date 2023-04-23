9941474f9bb3d8a06f3d24e81452370b4d75e1bc
Edson Percy Lopez Cortez <edson_lopez@hotmail.com.ar>
2023-04-23 19:48:33 -0400

    creacion de notificar

--- src/app/pages/request/transfer-request/registration-of-requests/registration-of-requests.component.ts
+++ src/app/pages/request/transfer-request/registration-of-requests/registration-of-requests.component.ts
@@ -755,11 +755,11 @@ export class RegistrationOfRequestsComponent
       url,
       from,
       to,
-      true,
+      false,
       this.task.id,
       user.username,
       'SOLICITUD_TRANSFERENCIA',
-      'Verificar_Cumplimiento',
+      'Destino_Documental',
       'NOTIFICAR_ACLARACIONES'
     );
     if (taskRes) {
@@ -963,6 +963,7 @@ export class RegistrationOfRequestsComponent
       orderservice['pOrderServiceIn'] = '';
 
       body['orderservice'] = orderservice;
+
       this.taskService.createTaskWitOrderService(body).subscribe({
         next: resp => {
           resolve(true);
