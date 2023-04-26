9c85a8897cdfb293890a2fbb1133cbd3f1c3fa18
Jose Eduardo <joseeduardo.jeg37@gmail.com>
2023-04-25 18:18:32 -0600

    services and validations

---
+++ src/app/core/models/ms-numerary/numerary.model.ts
@@ -0,0 +1,11 @@
+export interface INumerary {
+  tasintId: number;
+  month: number;
+  year: number;
+  lastDayMonth: number;
+  captureDate: Date;
+  user: string;
+  dollars: number;
+  pesos: number;
+  euro: number;
+}
