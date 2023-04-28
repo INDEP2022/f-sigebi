8bfb24797506199cd7035752e8df17342942e541
Edson Percy Lopez Cortez <edson_lopez@hotmail.com.ar>
2023-04-28 15:34:03 -0400

    modificar el enviroment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,6 @@ export const environment = {
   production: false,
   API_URL: 'http://sigebimsqa.indep.gob.mx/', //'http://localhost:3000/api/v1/',
   API_URL2: 'http://sigebimsqa.indep.gob.mx/',
-  //API_URL: 'http://sigebimsqa.indep.gob.mx/', //'http://localhost:3000/api/v1/',
-  //API_URL2: 'http://sigebimsqa.indep.gob.mx/',
   URL_PREFIX: 'api/v1/',
   API_REPORTS_BASE: 'http://reports-qa.indep.gob.mx/jasperserver/',
   API_REPORTS:
