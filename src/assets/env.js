6431031219999c86783c89521ed0dce3cdd2f031
medvedbelyi <46970598+medvedbelyi@users.noreply.github.com>
2023-05-01 22:05:29 -0600

    Update env.js

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,9 +1,9 @@
 (function (window) {
   window["env"] = window["env"] || {};
   window["env"].EXTERNAL_INTEGRATION_URL = 'http://www.indep.gob.mx/firmaelectronica'; // not actualized, for local testing
-  window["env"].API_BACKEND_URL = 'http://sigebimsdev.indep.gob.mx/';//'http://20.119.126.102:3000/';//'http://localhost:3000/api/v1/'; // not actualized, for local testing
+  window["env"].API_BACKEND_URL = 'http://sigebimsqa.indep.gob.mx/';//'http://20.119.126.102:3000/';//'http://localhost:3000/api/v1/'; // not actualized, for local testing
   
-  window["env"].API_BACKEND_URL2 = 'http://sigebimsdev.indep.gob.mx/';
+  window["env"].API_BACKEND_URL2 = 'http://sigebimsqa.indep.gob.mx/';
   window["env"].URL_PREFIX = 'api/v1/';
 
   window["env"].API_REPORTS_BASE = 'http://reports-qa.indep.gob.mx/jasperserver/';
