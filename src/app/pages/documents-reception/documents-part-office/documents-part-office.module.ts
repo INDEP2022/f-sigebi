3dc1f4cb4eb0bb023c9c3b541960f3274dd6d5f9
Alexhgg <hg_5@outlook.com>
2023-05-03 10:58:17 -0600

    Se agrega pantalla Recepcion Documentación Oficialia Partes

---
+++ src/app/pages/documents-reception/documents-part-office/documents-part-office.module.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { DocumentsListComponent } from 'src/app/@standalone/documents-list/documents-list.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
+import { DocumentsPartOfficeRoutingModule } from './documents-part-office-routing.module';
+import { DocumentsPartOfficeModalTableComponent } from './documents-part-office/components/documents-part-office-modal-table/documents-part-office-modal-table.component';
+import { TurnPaperworkComponent } from './documents-part-office/components/turn-paperwork/turn-paperwork.component';
+import { DocumentsPartOfficeComponent } from './documents-part-office/documents-part-office.component';
+import { DocumentPartOfficeModalComponent } from './document-part-office-modal/document-part-office-modal.component';
+
+
+@NgModule({
+  declarations: [
+    DocumentsPartOfficeModalTableComponent,
+    TurnPaperworkComponent,
+    DocumentsPartOfficeComponent,
+    DocumentPartOfficeModalComponent
+  ],
+  imports: [
+    CommonModule,
+    DocumentsPartOfficeRoutingModule,
+    SharedModule,
+    BsDatepickerModule,
+    ModalModule.forChild(),
+    DocumentsListComponent,
+    NgScrollbarModule,
+    TabsModule,
+  ]
+})
+export class DocumentsPartOfficeModule { }
