3517892f4a112f3144bcee28e2fb443d414d234b
Edson Percy Lopez Cortez <edson_lopez@hotmail.com.ar>
2023-05-03 11:16:57 -0400

    solucion de errores de select y guardado, court

--- src/app/pages/request/shared-request/detail-assets-tab-component/read-info-good/read-info-good.component.ts
+++ src/app/pages/request/shared-request/detail-assets-tab-component/read-info-good/read-info-good.component.ts
@@ -36,7 +36,7 @@ export class ReadInfoGoodComponent
   @Input() typeOfRequest: string = '';
   @Output() saveDetailInfo: EventEmitter<any> = new EventEmitter();
   goodData: any = {};
-  relevantTypeName: string = 'buscar';
+  relevantTypeName: string = '';
   goodForm: ModelForm<any>;
   destiniSaeSelected = new DefaultSelect();
   selectPhysicalState = new DefaultSelect();
@@ -87,20 +87,15 @@ export class ReadInfoGoodComponent
       this.getUnitMeasureLigie(new ListParams(), this.goodData.ligieUnit);
       //destino sae
       this.getDestinoSAE(new ListParams(), this.goodData.saeDestiny);
-      if (
-        // this.typeOfRequest == 'PGR_SAE' &&
-        this.process == 'classify-assets'
-      ) {
-        this.getUnitMeasureSae(new ListParams());
+      //destino transferente
+      this.getDestinyTransferent(this.goodData.transferentDestiny);
+      if (this.process == 'classify-assets') {
+        this.getUnitMeasureSae(new ListParams(), this.goodData.saeMeasureUnit);
 
         this.getConcervationState(new ListParams());
       }
 
-      if (
-        // this.typeOfRequest == 'PGR_SAE' &&
-        this.process == 'verify-compliance'
-      ) {
-        this.getDestinyTransferent(this.goodData.transferentDestiny);
+      if (this.process == 'verify-compliance') {
         this.getConcervationState(
           new ListParams(),
           this.goodData.stateConservation
@@ -108,10 +103,7 @@ export class ReadInfoGoodComponent
         this.getDestinoSAE(new ListParams());
       }
 
-      if (
-        // this.typeOfRequest == 'MANUAL' &&
-        this.process == 'verify-compliance'
-      ) {
+      if (this.process == 'verify-compliance') {
         this.getConcervationState(
           new ListParams(),
           this.goodData.stateConservation
@@ -164,14 +156,6 @@ export class ReadInfoGoodComponent
           }
         }
         this.destinySAE = resp.data[0].description;
-        /* if (
-          this.typeOfRequest == 'PGR_SAE' &&
-          this.process == 'verify-compliance'
-        ) {
-          this.destinySAE = resp.data[0].description;
-        } else {
-          this.destiniSaeSelected = new DefaultSelect(resp.data, resp.count);
-        } */
       },
       error: error => {
         console.log('destinoSae ', error);
@@ -181,7 +165,7 @@ export class ReadInfoGoodComponent
 
   getPhysicalState(params: ListParams, id?: string) {
     params['filter.name'] = '$eq:Estado Fisico';
-    if (id) {
+    if (id && this.process != 'classify-assets') {
       params['filter.keyId'] = `$eq:${id}`;
     }
     this.genericService
@@ -189,19 +173,21 @@ export class ReadInfoGoodComponent
       .pipe(takeUntil(this.$unSubscribe))
       .subscribe({
         next: (data: any) => {
-          console.log('estado fisico', data.data);
-          if (
-            (this.typeOfRequest == 'MANUAL' ||
-              this.typeOfRequest == 'PGR_SAE') &&
-            this.process == 'verify-compliance'
-          ) {
-            this.physicalStatus = data.data[0].description;
-          } else {
-            this.selectPhysicalState = new DefaultSelect(data.data, data.count);
-            this.goodForm.controls['physicalStatus'].setValue(
-              this.goodData.physicalStatus
-            );
+          if (this.process == 'classify-assets') {
+            if (id) {
+              this.selectPhysicalState = new DefaultSelect(
+                data.data,
+                data.count
+              );
+              this.goodForm.controls['physicalStatus'].setValue(id);
+            } else {
+              this.selectPhysicalState = new DefaultSelect(
+                data.data,
+                data.count
+              );
+            }
           }
+          this.physicalStatus = data.data[0].description;
         },
       });
   }
@@ -277,7 +263,6 @@ export class ReadInfoGoodComponent
       .pipe(takeUntil(this.$unSubscribe))
       .subscribe({
         next: resp => {
-          console.log(resp);
           if (this.process == 'classify-assets') {
             if (id) {
               this.selectMeasureUnitSae = new DefaultSelect(
@@ -347,8 +332,21 @@ export class ReadInfoGoodComponent
   }
 
   unidMediIndep(event: any) {
-    console.log(event);
-    this.dataToSend.saeMeasureUnit = event.saeMeasureUnit;
+    this.dataToSend.saeMeasureUnit = event.uomCode;
+  }
+
+  destinySae(event: any) {
+    this.dataToSend.saeDestiny = event.keyId;
+  }
+
+  physicalState(event: any) {
+    this.dataToSend.physicalStatus = event.keyId;
+    this.dataToSend.physicstateName = event.description;
+  }
+
+  concervationState(event: any) {
+    this.dataToSend.stateConservation = event.keyId;
+    this.dataToSend.stateConservationName = event.description;
   }
 
   save() {
@@ -378,7 +376,8 @@ export class ReadInfoGoodComponent
             body.stateConservation = resp.stateConservation;
             body.saeMeasureUnit = resp.saeMeasureUnit;
 
-            this.saveDetailInfo.emit(body);
+            this.dataToSend.id = resp.id;
+            this.saveDetailInfo.emit(this.dataToSend);
             this.onLoadToast(
               'success',
               'Actualizado',
--- src/app/pages/request/transfer-request/tabs/classify-assets-components/good-classification-tab/classification-assets-tab.component.ts
+++ src/app/pages/request/transfer-request/tabs/classify-assets-components/good-classification-tab/classification-assets-tab.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { LocalDataSource } from 'ng2-smart-table';
 import { BehaviorSubject, takeUntil } from 'rxjs';
 import { TABLE_SETTINGS } from 'src/app/common/constants/table-settings';
 import {
@@ -57,7 +58,7 @@ export class ClassificationAssetsTabComponent
   paramsLvl2 = new BehaviorSubject<ListParams>(new ListParams());
   paramsLvl3 = new BehaviorSubject<ListParams>(new ListParams());
   paramsLvl4 = new BehaviorSubject<ListParams>(new ListParams());
-  paragraphs: any[] = [];
+  paragraphs = new LocalDataSource();
   assetsId: string | number;
   detailArray: IFormGroup<IGood>;
   goodObject: any;
@@ -172,7 +173,7 @@ export class ClassificationAssetsTabComponent
         });
 
         Promise.all(result).then(data => {
-          this.paragraphs = resp.data;
+          this.paragraphs.load(resp.data);
           this.totalItems = resp.count;
           this.loading = false;
         });
@@ -234,45 +235,6 @@ export class ClassificationAssetsTabComponent
         });
     });
   }
-
-  /*getData() {
-    if (this.idRequest) {
-      this.loading = true;
-      this.params.getValue()['filter.requestId'] = this.idRequest;
-      this.goodService.getAll(this.params.getValue()).subscribe({
-        next: data => {
-          const info = data.data.map(items => {
-            const fraction: any = items.fractionId;
-            this.idFraction = fraction.code;
-            items.fractionId = fraction.description;
-            return items;
-          });
-
-          const filtergoodType = info.map(async item => {
-            const goodType: any = await this.getGoodType(item.goodTypeId);
-            item['goodTypeId'] = goodType;
-            if (item['physicalStatus'] == 1) item['physicalStatus'] = 'BUENO';
-            if (item['physicalStatus'] == 2) item['physicalStatus'] = 'MALO';
-            if (item['stateConservation'] == 1)
-              item['stateConservation'] = 'BUENO';
-            if (item['stateConservation'] == 2)
-              item['stateConservation'] = 'MALO';
-            if (item['destiny'] == 1) item['destiny'] = 'VENTA';
-            return item;
-          });
-
-          Promise.all(filtergoodType).then(data => {
-            this.paragraphs = data;
-            this.totalItems = this.paragraphs.length;
-            this.loading = false;
-          });
-        },
-        error: error => {
-          this.loading = false;
-        },
-      });
-    }
-  } */
 
   getGoodType(goodTypeId: string | number) {
     this.showHideErrorInterceptorService.showHideError(false);
@@ -600,5 +562,19 @@ export class ClassificationAssetsTabComponent
 
   updateTableInfo(event: any) {
     console.log('clasifications assets ', event);
+
+    this.paragraphs.getElements().then(data => {
+      data.map((item: any) => {
+        if (item.id === event.id) {
+          for (const key in event) {
+            console.log(key);
+            if (key != 'id') {
+              item[key] = event[key];
+            }
+          }
+        }
+        this.paragraphs.load(data);
+      });
+    });
   }
 }
