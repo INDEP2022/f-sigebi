689d6546a1bb559c17032c5cd9819ad4d5309fa9
Raul Legaspi <legaspi081213@gmail.com>
2023-05-03 23:25:30 -0600

    copy-923e0d9e2bf11fe7fa6deca375e10a734c5817c8

--- src/app/common/constants/endpoints/ms-proceduremanagement-endpoints.ts
+++ src/app/common/constants/endpoints/ms-proceduremanagement-endpoints.ts
@@ -8,7 +8,7 @@ export enum ProcedureManagementEndPoints {
   ReportTranferenciaViews = `views/file-transference-sat`,
   ReportViewsPgr = `views/file-procedure-mng`,
   ReportTranferenciaPgrViews = `views/file-transference`,
-  FolioMax = `FolioMax`,
+  FolioMax = `proceduremanagement/FolioMax`,
 }
 
 export enum MassiveChargeGoods {
