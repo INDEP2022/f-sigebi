7ef67ee0ab8a5d33b77605e2f59fe165f8f241d9
Angelo <angelorp93@gmail.com>
2023-05-05 17:13:39 -0500

    cambios

--- src/app/pages/judicial-physical-reception/partializes-general-goods-1/partializes-general-goods.module.ts
+++ src/app/pages/judicial-physical-reception/partializes-general-goods-1/partializes-general-goods.module.ts
@@ -6,13 +6,20 @@ import { GoodsClasificationSharedComponent } from 'src/app/@standalone/shared-fo
 import { GoodsStatusSharedComponent } from 'src/app/@standalone/shared-forms/goods-status-shared/goods-status-shared.component';
 import { SelectFormComponent } from 'src/app/@standalone/shared-forms/select-form/select-form.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { GoodFormComponent } from './good-form/good-form.component';
+import { ApplyButtonComponent } from './components/apply-button/apply-button.component';
+import { GoodFormComponent } from './components/good-form/good-form.component';
+import { PartializeButtonComponent } from './components/partialize-button/partialize-button.component';
 import { PartializesGeneralGoodsRoutingModule } from './partializes-general-goods-routing.module';
 import { PartializesGeneralGoodsComponent } from './partializes-general-goods.component';
 import { PartializeGeneralGoodService } from './services/partialize-general-good.service';
 
 @NgModule({
-  declarations: [PartializesGeneralGoodsComponent, GoodFormComponent],
+  declarations: [
+    PartializesGeneralGoodsComponent,
+    GoodFormComponent,
+    PartializeButtonComponent,
+    ApplyButtonComponent,
+  ],
   imports: [
     CommonModule,
     PartializesGeneralGoodsRoutingModule,
