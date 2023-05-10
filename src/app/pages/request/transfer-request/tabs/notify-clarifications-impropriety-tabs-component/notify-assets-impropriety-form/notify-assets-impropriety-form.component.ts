323555d621afd7875f66d169c21b197226840f05
Henry Giovanni Sánchez Solís <111904384+HenryGiovanniSanchezSolis@users.noreply.github.com>
2023-05-09 18:58:55 -0600

    Update status notifications (#1453)
    * evitando duplicidad en firmantes
    
    * evitando duplicidad
    
    * reportes con firma
    
    * fix reports
    
    * fix reports

--- src/app/pages/request/transfer-request/tabs/notify-clarifications-impropriety-tabs-component/notify-assets-impropriety-form/notify-assets-impropriety-form.component.ts
+++ src/app/pages/request/transfer-request/tabs/notify-clarifications-impropriety-tabs-component/notify-assets-impropriety-form/notify-assets-impropriety-form.component.ts
@@ -92,13 +92,20 @@ export class NotifyAssetsImproprietyFormComponent
     });
 
     this.clarificationForm = this.fb.group({
-      addresseeName: ['', [Validators.required, Validators.maxLength(50)]],
+      addresseeName: [
+        '',
+        [
+          //Validators.required,
+          Validators.pattern(STRING_PATTERN),
+          Validators.maxLength(50),
+        ],
+      ],
 
       positionAddressee: [
         '',
         [
           Validators.pattern(STRING_PATTERN),
-          Validators.required,
+          //Validators.required,
           Validators.maxLength(50),
         ],
       ],
@@ -209,7 +216,11 @@ export class NotifyAssetsImproprietyFormComponent
     }
 
     if (typeTransference != 'SAT_SAE' || generaXML) {
-      if (this.typeClarifications == 2 && typeTransference != 'SAT_SAE') {
+      if (
+        this.typeClarifications == 2 &&
+        typeTransference != 'SAT_SAE' &&
+        typeTransference != 'PGR_SAE'
+      ) {
         this.improcedenciaTransferentesVoluntarias(); //Aclaración Manual tipo 2
       }
       const obtainTypeDocument = await this.obtainTypeDocument(
@@ -248,6 +259,7 @@ export class NotifyAssetsImproprietyFormComponent
   }
 
   improcedenciaTransferentesVoluntarias() {
+    console.log('improcedenciaTransferentesVoluntarias DESDE EL');
     //Recupera información del usuario logeando para luego registrarlo como firmante
     let token = this.authService.decodeToken();
 
@@ -286,7 +298,7 @@ export class NotifyAssetsImproprietyFormComponent
     this.loading = true;
     this.documentService.createClarDocImp(modelReport).subscribe({
       next: data => {
-        //this.changeStatusAnswered();
+        this.changeStatusAnswered();
         this.openReport(data);
         this.loading = false;
         this.close();
@@ -338,7 +350,7 @@ export class NotifyAssetsImproprietyFormComponent
     this.loading = true;
     this.documentService.createClarDocImp(modelReport).subscribe({
       next: data => {
-        //this.changeStatusAnswered();
+        this.changeStatusAnswered();
         this.openReport(data);
         this.loading = false;
         this.close();
@@ -369,10 +381,10 @@ export class NotifyAssetsImproprietyFormComponent
       positionSender: this.clarificationForm.controls['senderCharge'].value,
       paragraphFinal: this.clarificationForm.controls['paragraphFinal'].value,
       consistentIn: this.clarificationForm.controls['consistentIn'].value,
-      managedTo: 'DIRIGIDO A EJEMPLO',
+      managedTo: this.infoRequest.nameOfOwner,
       invoiceLearned: this.folioReporte,
       //invoiceNumber: 1,
-      positionAddressee: 'CARGO DE EJEMPLO',
+      positionAddressee: this.infoRequest.holderCharge,
       modificationDate: new Date(),
       creationUser: token.name,
       documentTypeId: '212',
@@ -390,7 +402,7 @@ export class NotifyAssetsImproprietyFormComponent
     this.loading = true;
     this.documentService.createClarDocImp(modelReport).subscribe({
       next: data => {
-        //this.changeStatusAnswered();
+        this.changeStatusAnswered();
         this.openReport(data);
         this.loading = false;
         this.close();
@@ -443,7 +455,7 @@ export class NotifyAssetsImproprietyFormComponent
     this.loading = true;
     this.documentService.createClarDocImp(modelReport).subscribe({
       next: data => {
-        //this.changeStatusAnswered();
+        this.changeStatusAnswered();
         this.openReport(data);
         this.loading = false;
         this.close();
@@ -549,7 +561,7 @@ export class NotifyAssetsImproprietyFormComponent
     this.loading = true;
     this.documentService.createClarDocImp(modelReport).subscribe({
       next: data => {
-        //this.changeStatusAnswered();
+        this.changeStatusAnswered();
         this.openReport(data);
         this.loading = false;
         this.close();
@@ -784,6 +796,7 @@ export class NotifyAssetsImproprietyFormComponent
   generateClave(noDictamen?: string) {
     //Trae información del usuario logeado
     let token = this.authService.decodeToken();
+    console.log('Informació del token', token);
     //Trae el año actuar
     const year = this.today.getFullYear();
     //Cadena final (Al final las siglas ya venian en el token xd)
