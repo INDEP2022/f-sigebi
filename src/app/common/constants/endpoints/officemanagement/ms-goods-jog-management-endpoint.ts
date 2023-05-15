a22974e35c60d9c681732edc0c29509d74109399
Osc4r1b4rra <128086640+Osc4r1b4rra@users.noreply.github.com>
2023-05-15 14:31:57 -0600

    Oscar dev4 (#1509)
    * Se suben los cambios para revición
    
    * datos de pruebas
    
    ---------
    
    Co-authored-by: Oscar_Ibarra <oscaribarra406@gmail.com>

--- src/app/common/constants/endpoints/officemanagement/ms-goods-jog-management-endpoint.ts
+++ src/app/common/constants/endpoints/officemanagement/ms-goods-jog-management-endpoint.ts
@@ -2,6 +2,5 @@ export enum GoodsJobManagementEndpoints {
   OfficeManagement = 'officemanagement',
   GoodsJobManagement = 'goods-job-management',
   FindByIds = 'goods-job-management/find-by-ids',
-
   mJobManagement = 'm-job-management',
 }
