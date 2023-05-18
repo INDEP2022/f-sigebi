e720abcc9b0a076d0fffb564c6b243b836f3a1d8
Wilmer Solorzano <wilmersolorzano.dev@gmail.com>
2023-05-18 16:01:18 -0400

    avances FACTJURABANDONOS

--- src/app/common/constants/endpoints/ms-goodprocess-endpoint.ts
+++ src/app/common/constants/endpoints/ms-goodprocess-endpoint.ts
@@ -2,4 +2,6 @@ export enum GoodprocessEndpoints {
   BasePath = 'goodprocess',
   AplicationValidStatus = 'application/validStatus',
   ApplicationAllFill = 'application/all-fill',
+  CountBienEstatusXPantalla = 'application/countBienEstatusXPantalla',
+  ExpedientePostQuery = 'application/expedientePostQuery',
 }
