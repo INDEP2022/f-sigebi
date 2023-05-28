861bc1a3f9e6da39ac0ed631bf22dd394bf4d918
Edson Percy Lopez Cortez <edson_lopez@hotmail.com.ar>
2023-05-28 09:19:11 -0400

    filtro del solicitudes

--- src/app/pages/request/request-in-turn/request-in-turn-selected/request-in-turn-selected.component.ts
+++ src/app/pages/request/request-in-turn/request-in-turn-selected/request-in-turn-selected.component.ts
@@ -213,6 +213,7 @@ export class RequestInTurnSelectedComponent extends BasePage implements OnInit {
       task['programmingId'] = 0;
       task['requestId'] = request.id;
       task['expedientId'] = 0;
+      task['assignedDate'] = new Date().toISOString();
       task['urlNb'] = 'pages/request/transfer-request/registration-request';
       task['processName'] = 'SolicitudTransferencia';
 
