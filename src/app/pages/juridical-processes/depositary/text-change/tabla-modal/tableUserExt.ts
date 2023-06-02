e8c99ca302ef8aaf017161eb72ce74237ebd1a1c
Osc4r1b4rra <128086640+Osc4r1b4rra@users.noreply.github.com>
2023-06-02 11:21:51 -0600

    Ajustando la pantalla de inicio (#1832)
    Co-authored-by: Oscar_Ibarra <oscaribarra406@gmail.com>

--- src/app/pages/juridical-processes/depositary/text-change/tabla-modal/tableUserExt.ts
+++ src/app/pages/juridical-processes/depositary/text-change/tabla-modal/tableUserExt.ts
@@ -1,4 +1,31 @@
 export const EXTERNOS_COLUMS = {
+  personExtInt: {
+    title: 'Tipo persona',
+    type: 'string',
+    sort: false,
+  },
+  namePersonExt: {
+    title: 'Nombre persona',
+    type: 'string',
+    sort: false,
+  },
+};
+
+export const EXTERNOS_COLUMS_OFICIO = {
+  personExtInt: {
+    title: 'Tipo persona',
+    type: 'string',
+    sort: false,
+  },
+  nomPersonExt: {
+    title: 'Nombre persona',
+    type: 'string',
+    sort: false,
+  },
+};
+/*
+
+export const EXTERNOS_COLUMS = {
   id: {
     title: '#',
     type: 'number',
@@ -21,4 +48,3 @@ export const EXTERNOS_COLUMS = {
     sort: false,
   },
   */
-};
