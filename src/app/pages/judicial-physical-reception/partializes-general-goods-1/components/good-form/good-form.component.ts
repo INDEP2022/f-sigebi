6f5be490c0dbf54ad01e4082040b7d738bd6a3ab
Angelo <angelorp93@gmail.com>
2023-06-03 17:27:04 -0500

    fixees

--- src/app/pages/judicial-physical-reception/partializes-general-goods-1/components/good-form/good-form.component.ts
+++ src/app/pages/judicial-physical-reception/partializes-general-goods-1/components/good-form/good-form.component.ts
@@ -290,10 +290,6 @@ export class GoodFormComponent extends AlertButton implements OnInit {
     this.resetForm();
   }
 
-  ngOnDestroy() {
-    this.resetForm();
-  }
-
   private validateStatusXPantalla(good: IGood) {
     const filterParams = new FilterParams();
     filterParams.addFilter('screenKey', 'FACTGENPARCBIEN');
