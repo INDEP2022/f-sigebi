43cbc6a87ed2a3bc78a3935effa22c212796ad1f
henrygiovannisanchezsolis <sshenrygiovanni@gmail.com>
2023-06-06 00:11:00 -0600

    fix-form

--- src/app/pages/request/transfer-request/tabs/notify-clarifications-impropriety-tabs-component/inappropriateness-form/inappropriateness-form.component.ts
+++ src/app/pages/request/transfer-request/tabs/notify-clarifications-impropriety-tabs-component/inappropriateness-form/inappropriateness-form.component.ts
@@ -9,6 +9,7 @@ import { IClarificationDocumentsImpro } from 'src/app/core/models/ms-documents/c
 import { IDictamenSeq } from 'src/app/core/models/ms-goods-query/opinionDelRegSeq-model';
 import { IRequest } from 'src/app/core/models/requests/request.model';
 import { AuthService } from 'src/app/core/services/authentication/auth.service';
+import { RegionalDelegationService } from 'src/app/core/services/catalogs/regional-delegation.service';
 import { ChatClarificationsService } from 'src/app/core/services/ms-chat-clarifications/chat-clarifications.service';
 import { DocumentsService } from 'src/app/core/services/ms-documents/documents.service';
 import { ApplicationGoodsQueryService } from 'src/app/core/services/ms-goodsquery/application.service';
@@ -36,6 +37,8 @@ export class InappropriatenessFormComponent extends BasePage implements OnInit {
   paramsRequest = new BehaviorSubject<ListParams>(new ListParams());
   dataChatClarifications: IChatClarifications[];
   idSolicitud: any;
+  delegationUser: any;
+
   constructor(
     private modalRef: BsModalRef,
     private modalService: BsModalService,
@@ -44,21 +47,38 @@ export class InappropriatenessFormComponent extends BasePage implements OnInit {
     private documentService: DocumentsService,
     private chatService: ChatClarificationsService,
     private applicationGoodsQueryService: ApplicationGoodsQueryService,
-    private rejectedGoodService: RejectedGoodService
+    private rejectedGoodService: RejectedGoodService,
+    private regionalDelegationService: RegionalDelegationService
   ) {
     super();
     this.today = new Date();
   }
 
   ngOnInit(): void {
+    console.log('Delegación de la solicitud', this.delegationUser);
     this.dictamenSeq();
     this.prepareForm();
+
+    /*this.regionalDelegationService.getById(this.delegationUser).subscribe({
+      next: resp => {
+        console.log('Respuesta de la delegación', resp.regionalDelegate, 'Cargo', resp.officeAddress);
+      },
+      error: error => {
+        console.log('Respuesta de la delegación', error.regionalDelegate);
+      },
+    })*/
   }
 
   prepareForm() {
     this.form = this.fb.group({
-      addresseeName: [null, [Validators.maxLength(50)]],
-      positionAddressee: [null, [Validators.maxLength(50)]],
+      addresseeName: [
+        this.request?.nameOfOwner || null,
+        [Validators.maxLength(50)],
+      ],
+      positionAddressee: [
+        this.request?.holderCharge || null,
+        [Validators.maxLength(50)],
+      ],
       senderName: [null, [Validators.maxLength(50)]],
       senderCharge: [null, [Validators.maxLength(50)]],
       clarification: [null, [Validators.maxLength(800)]],
