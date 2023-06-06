e4add31dbe85635e82da1ce24be1ed0809cde597
Fernando-Zhunio <fzhunio91@hotmail.com>
2023-06-06 18:08:53 -0500

    latest

--- src/app/pages/documents-reception/flyers/related-documents/related-documents-response.ts
+++ src/app/pages/documents-reception/flyers/related-documents/related-documents-response.ts
@@ -1,50 +1,42 @@
 import { type FormGroup } from '@angular/forms';
+import { catchError, firstValueFrom, map, Observable, of } from 'rxjs';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
 import { IGood } from 'src/app/core/models/good/good.model';
+import { type INotification } from 'src/app/core/models/ms-notification/notification.model';
+import { IMJobManagement } from 'src/app/core/models/ms-officemanagement/m-job-management.model';
 import { GoodService } from 'src/app/core/services/ms-good/good.service';
+import { NotificationService } from 'src/app/core/services/ms-notification/notification.service';
 import { GoodsJobManagementService } from 'src/app/core/services/ms-office-management/goods-job-management.service';
+import { MJobManagementService } from 'src/app/core/services/ms-office-management/m-job-management.service';
 import { BasePage } from 'src/app/core/shared/base-page';
 
 export abstract class RelateDocumentsResponse extends BasePage {
-  protected abstract goodService: GoodService;
-  protected abstract goodsJobManagementService: GoodsJobManagementService;
+  protected abstract goodServices: GoodService;
+  protected abstract serviceOficces: GoodsJobManagementService;
+  protected abstract notificationService: NotificationService;
+  protected abstract mJobManagementService: MJobManagementService;
+
   abstract data1: IGood[];
   abstract managementForm: FormGroup;
   isLoadingGood: boolean;
-  getGoods(params: ListParams) {
-    const expedientNumber = this.managementForm?.get('noVolante').value;
-    const queryString = `page=${params.page}&limit=${params.limit}&filter.fileNumber=${expedientNumber}`;
-
+  abstract totalItems: number;
+  abstract formJobManagement: FormGroup;
+  getGoods1(params: ListParams) {
     this.isLoadingGood = true;
-    this.goodService.getAllFilter(queryString).subscribe({
+    this.goodServices.getAll(params).subscribe({
       next: async data => {
-        const goods = data.data.map(async (item: any) => {
-          // const isNotAvailable = await this.getGoodsOfficeManagementsExpAvailable();
+        const goods = await data.data.map(async (item: any) => {
+          const isAvailable = await this.getFactaDbOficioGestrel(
+            item.id,
+            this.formJobManagement.get('managementNumber').value
+          );
+          return {
+            ...item,
+            available: isAvailable,
+          };
         });
-        //  const r = await data.data.map(async (item: any) => {
-        //    let isNotAvailable = Boolean(
-        //      this.dataTableBienesOficio.find(x => x.id == item.id)
-        //    );
-        //    console.log('isNotAvailable', isNotAvailable);
-        //    if (item.status == 'ADM' && !isNotAvailable) {
-        //      try {
-        //        const result = await this.getDetailGoodThirdParty(
-        //          item.id,
-        //          true
-        //        );
-        //        isNotAvailable = true;
-        //      } catch (e) {
-        //        isNotAvailable = false;
-        //      }
-        //    }
-        //    return {
-        //      ...item,
-        //      available:
-        //        item.status != 'ADM' || isNotAvailable ? false : true,
-        //    };
-        //  });
-        //  this.dataTableBienes = await Promise.all(r);
-        //  this.totalItemsGood = data.count;
+        this.data1 = await Promise.all(goods);
+        this.totalItems = data.count;
         this.isLoadingGood = false;
       },
       error: () => {
@@ -53,16 +45,50 @@ export abstract class RelateDocumentsResponse extends BasePage {
     });
   }
 
-  //   getGoodsOfficeManagementsExpAvailable(expGoodNumber: string | number, managementNumber: string | number, goodId: string | number) {
-  //     const params = new ListParams();
-  //     params.page = 1;
-  //     params.limit = 1;
-  //     params['expGoodNumber'] = expGoodNumber;
-  //     params['managementNumber'] = managementNumber;
-  //     params['goodId'] = goodId;
-  //     return firstValueFrom(
-  //         this.goodsJobManagementService.getGoodsOfficeManagementsExpAvailable(params)
-  //         .pipe(map(x => x.data))
-  //         );
-  //   }
+  getFactaDbOficioGestrel(
+    no_of_gestion: string | number,
+    no_bien: string | number
+  ): Promise<boolean> {
+    // const params = new ListParams();
+    // params.page = 1;
+    // params.limit = 1;
+    // params['expGoodNumber'] = expGoodNumber;
+    // params['managementNumber'] = managementNumber;
+    // params['goodId'] = goodId;
+    return firstValueFrom(
+      this.goodServices
+        .getFactaDbOficioGestrel({
+          no_bien,
+          no_of_gestion,
+        })
+        .pipe(
+          map(x => true),
+          catchError(() => {
+            return of(false);
+          })
+        )
+    );
+  }
+
+  getNotification(
+    wheelNumber: string | number,
+    expendient: string | number
+  ): Observable<INotification> {
+    const params = new ListParams();
+    params.page = 1;
+    params.limit = 1;
+    params['filter.wheelNumber'] = wheelNumber;
+    if (expendient) {
+      params['filter.expendient'] = expendient;
+    }
+    return this.notificationService.getAll(params).pipe(map(x => x.data[0]));
+  }
+
+  getMJobManagement(wheelNumber: string | number): Observable<IMJobManagement> {
+    const params = new ListParams();
+    params.page = 1;
+    params.limit = 1;
+    params['filter.flyerNumber'] = wheelNumber;
+    return this.mJobManagementService.getAll(params).pipe(map(x => x.data[0]));
+  }
 }
