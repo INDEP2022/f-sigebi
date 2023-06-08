37e92a6571ebb6ffbe0d2475af648a85960202f5
ansoni01 <40308661+ansoni01@users.noreply.github.com>
2023-06-08 11:13:35 -0500

    Feature/ansoni dev (#2053)
    * deletes
    
    * delete

--- src/app/core/services/catalogs/label-okey.service.ts
+++ src/app/core/services/catalogs/label-okey.service.ts
@@ -6,12 +6,21 @@ import { ListParams } from '../../../common/repository/interfaces/list-params';
 import { Repository } from '../../../common/repository/repository';
 import { IListResponse } from '../../interfaces/list-response.interface';
 import { ILabelOKey } from '../../models/catalogs/label-okey.model';
+import { LabelGoodEndPoints } from 'src/app/common/constants/endpoints/label-good-endpoint';
+import { HttpService } from 'src/app/common/services/http.service';
 @Injectable({
   providedIn: 'root',
 })
-export class LabelOkeyService implements ICrudMethods<ILabelOKey> {
+export class LabelOkeyService
+  extends HttpService
+  implements ICrudMethods<ILabelOKey>
+{
   private readonly route: string = ENDPOINT_LINKS.LabelOkey;
-  constructor(private labelOkeyRepository: Repository<ILabelOKey>) {}
+
+  constructor(private labelOkeyRepository: Repository<ILabelOKey>) {
+    super();
+    this.microservice = LabelGoodEndPoints.BasePage;
+  }
 
   getAll(params?: ListParams): Observable<IListResponse<ILabelOKey>> {
     return this.labelOkeyRepository.getAllPaginated(this.route, params);
@@ -31,5 +40,10 @@ export class LabelOkeyService implements ICrudMethods<ILabelOKey> {
 
   remove(id: string | number): Observable<Object> {
     return this.labelOkeyRepository.remove(this.route, id);
+  }
+
+  remove2(id: string | number) {
+    const route = `${LabelGoodEndPoints.labelGood}/id/${id}`;
+    return this.delete(route);
   }
 }
