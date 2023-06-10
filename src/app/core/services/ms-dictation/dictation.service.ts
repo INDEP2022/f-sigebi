dc9df2ae44547eb7319734afbe8c9cbad3a315b9
Jose Eduardo <joseeduardo.jeg37@gmail.com>
2023-06-10 12:14:25 -0600

    adecuaciones boton aprobar dictamen

--- src/app/core/services/ms-dictation/dictation.service.ts
+++ src/app/core/services/ms-dictation/dictation.service.ts
@@ -261,4 +261,8 @@ export class DictationService extends HttpService {
     const route = `${DictationEndpoints.FactJur}`;
     return this.get(route + `/${typeDict}`);
   }
+
+  getNoGoodClass(data: Object) {
+    return this.post(DictationEndpoints.GetGoodClass, data);
+  }
 }
