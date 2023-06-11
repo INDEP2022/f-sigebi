2df1e6e4385fde82d99e19306af4218974b63556
joselalo97 <119750919+joselalo97@users.noreply.github.com>
2023-06-11 12:25:52 -0600

    Correcionesdictamas (#2124)
    * bud sol de transferencia
    
    * bug solicitud de transferencia
    
    * Dictaminaciones
    
    * adecuaciones fechas, carga bienes y documentos
    
    * msj alert
    
    * cambios FACTADBOFICIOGEST
    
    * adecuaciones carga de dictamenes
    
    * adecuacion mas dictamenes
    
    ---------
    
    Co-authored-by: henrygiovannisanchezsolis <sshenrygiovanni@gmail.com>

--- src/app/pages/judicial-physical-reception/confiscated-records/options-history-good-delegation/options-history-good-delegation.components.ts
+++ src/app/pages/judicial-physical-reception/confiscated-records/options-history-good-delegation/options-history-good-delegation.components.ts
@@ -1,25 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { LocalDataSource } from 'ng2-smart-table';
-import { BsModalRef } from 'ngx-bootstrap/modal';
-import { TABLE_SETTINGS } from 'src/app/common/constants/table-settings';
-import { FilterParams } from 'src/app/common/repository/interfaces/list-params';
-import { IDetailWithIndEdo } from 'src/app/core/models/ms-proceedings/detail-proceedings-delivery-reception.model';
-import { GoodsQueryService } from 'src/app/core/services/goodsquery/goods-query.service';
-import { ClassifyGoodService } from 'src/app/core/services/ms-classifygood/ms-classifygood.service';
-import { GoodService } from 'src/app/core/services/ms-good/good.service';
-import { DetailProceeDelRecService } from 'src/app/core/services/ms-proceedings/detail-proceedings-delivery-reception.service';
 import { BasePage } from 'src/app/core/shared/base-page';
-import { SelectElementComponent } from 'src/app/shared/components/select-element-smarttable/select-element';
 
 @Component({
-    selector: 'app-options-history-good-delegation',
-    templateUrl: './option-history-good-delegation.component.html',
-    styleUrls: []
+  selector: 'app-options-history-good-delegation',
+  templateUrl: './option-history-good-delegation.component.html',
+  styleUrls: [],
 })
-
 export class OptionsHistoryGoodDelegation extends BasePage implements OnInit {
-    ngOnInit(): void {
-        throw new Error("Method not implemented.");
-    }
-    
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }
