10ce6be8c55cf45c9ed8dfec7cfc5429355b7a20
ElBarto <genaro.adrian@slide.com.mx>
2023-06-13 22:32:15 -0600

    Correcciones

--- src/app/pages/judicial-physical-reception/judicial-physical-reception-routing.module.ts
+++ src/app/pages/judicial-physical-reception/judicial-physical-reception-routing.module.ts
@@ -2,69 +2,69 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'articles-complement',
+    loadChildren: () =>
+      import('./complement-article/complement-article.module').then(
+        m => m.ComplementArticleModule
+      ),
+  },
+  {
+    path: 'confiscated-reception',
+    loadChildren: () =>
+      import('./confiscated-reception/confiscated-reception.module').then(
+        m => m.ConfiscatedReceptionModule
+      ),
+  },
+  {
+    path: 'records-report',
+    loadChildren: () =>
+      import('./records-report/records-report.module').then(
+        m => m.RecordsReportModule
+      ),
+  },
+  {
+    path: 'confiscated-records',
+    loadChildren: () =>
+      import('./confiscated-records/confiscated-records.module').then(
+        m => m.ConfiscatedRecordsModule
+      ),
+  },
+  {
+    path: 'partializes-general-goods',
+    loadChildren: () =>
+      import(
+        './partializes-general-goods-1/partializes-general-goods.module'
+      ).then(m => m.PartializesGeneralGoodsModule),
+  },
   // {
-  //   path: 'articles-complement',
-  //   loadChildren: () =>
-  //     import('./complement-article/complement-article.module').then(
-  //       m => m.ComplementArticleModule
-  //     ),
-  // },
-  // {
-  //   path: 'confiscated-reception',
-  //   loadChildren: () =>
-  //     import('./confiscated-reception/confiscated-reception.module').then(
-  //       m => m.ConfiscatedReceptionModule
-  //     ),
-  // },
-  // {
-  //   path: 'records-report',
-  //   loadChildren: () =>
-  //     import('./records-report/records-report.module').then(
-  //       m => m.RecordsReportModule
-  //     ),
-  // },
-  // {
-  //   path: 'confiscated-records',
-  //   loadChildren: () =>
-  //     import('./confiscated-records/confiscated-records.module').then(
-  //       m => m.ConfiscatedRecordsModule
-  //     ),
-  // },
-  // {
-  //   path: 'partializes-general-goods',
+  //   path: 'partializes-general-goods-2',
   //   loadChildren: () =>
   //     import(
-  //       './partializes-general-goods-1/partializes-general-goods.module'
+  //       './partializes-general-goods-2/partializes-general-goods.module'
   //     ).then(m => m.PartializesGeneralGoodsModule),
   // },
-  // // {
-  // //   path: 'partializes-general-goods-2',
-  // //   loadChildren: () =>
-  // //     import(
-  // //       './partializes-general-goods-2/partializes-general-goods.module'
-  // //     ).then(m => m.PartializesGeneralGoodsModule),
-  // // },
-  // {
-  //   path: 'partializes-goods',
-  //   loadChildren: () =>
-  //     import('./partializes-goods/partializes-goods.module').then(
-  //       m => m.PartializesGoodsModule
-  //     ),
-  // },
-  // {
-  //   path: 'cancellation-recepcion',
-  //   loadChildren: () =>
-  //     import('./cancellation-recepcion/cancellation-recepcion.module').then(
-  //       m => m.CancellationRecepcionModule
-  //     ),
-  // },
-  // {
-  //   path: 'cancellation-sale',
-  //   loadChildren: () =>
-  //     import('./sale-cancellation/sale-cancellation.module').then(
-  //       m => m.SaleCancellationModule
-  //     ),
-  // },
+  {
+    path: 'partializes-goods',
+    loadChildren: () =>
+      import('./partializes-goods/partializes-goods.module').then(
+        m => m.PartializesGoodsModule
+      ),
+  },
+  {
+    path: 'cancellation-recepcion',
+    loadChildren: () =>
+      import('./cancellation-recepcion/cancellation-recepcion.module').then(
+        m => m.CancellationRecepcionModule
+      ),
+  },
+  {
+    path: 'cancellation-sale',
+    loadChildren: () =>
+      import('./sale-cancellation/sale-cancellation.module').then(
+        m => m.SaleCancellationModule
+      ),
+  },
   {
     path: 'scheduled-maintenance',
     loadChildren: () =>
@@ -79,13 +79,13 @@ const routes: Routes = [
         m => m.ScheduledMaintenanceModule
       ),
   },
-  // {
-  //   path: 'maintenance-records',
-  //   loadChildren: () =>
-  //     import('./maintenance-records/maintenance-records.module').then(
-  //       m => m.MaintenanceRecordsModule
-  //     ),
-  // },
+  {
+    path: 'maintenance-records',
+    loadChildren: () =>
+      import('./maintenance-records/maintenance-records.module').then(
+        m => m.MaintenanceRecordsModule
+      ),
+  },
 ];
 
 @NgModule({
