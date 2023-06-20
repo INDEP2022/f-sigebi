11abeafbdc7084ae09671aad828c715818a5e1a2
malavidaxz <110206463+malavidaxz@users.noreply.github.com>
2023-06-20 10:47:56 -0500

    Avan_2.13

---
+++ src/app/pages/request/programming-request-components/formalize-programming/columns/transportable-goods-columns.ts
@@ -0,0 +1,156 @@
+export const TRANSPORTABLE_GOODS = {
+  id: {
+    title: 'No gestión',
+    type: 'string',
+    sort: false,
+  },
+
+  uniqueKey: {
+    title: 'Clave única',
+    type: 'string',
+    sort: false,
+  },
+
+  fileNumber: {
+    title: 'Expediente',
+    type: 'string',
+    sort: false,
+  },
+
+  goodDescription: {
+    title: 'Descripción transferente',
+    sort: false,
+  },
+
+  descriptionSae: {
+    title: 'Descripción INDEP',
+    type: 'string',
+    sort: false,
+  },
+
+  transferenceQuantity: {
+    title: 'Cantidad transferente',
+    type: 'string',
+    sort: false,
+  },
+
+  saeAmmount: {
+    title: 'Cantidad INDEP',
+    type: 'string',
+    sort: false,
+  },
+
+  transerUnit: {
+    title: 'Unidad transferente',
+    type: 'string',
+    sort: false,
+  },
+
+  unitMeasure: {
+    title: 'Unidad medida INDEP',
+    type: 'string',
+    sort: false,
+  },
+
+  physicalState: {
+    title: 'Estado Físico transferente',
+    type: 'string',
+    sort: false,
+  },
+
+  physicalStateSae: {
+    title: 'Estado Físico INDEP',
+    type: 'string',
+    sort: false,
+  },
+
+  transferConStatus: {
+    title: 'Estado conversación transferente',
+    type: 'string',
+    sort: false,
+  },
+
+  convrsationConStatus: {
+    title: 'Estado conversación INDEP',
+    type: 'string',
+    sort: false,
+  },
+};
+
+export const TRANSPORTABLE_GOODS_FORMALIZE = {
+  goodId: {
+    title: 'Nº gestión',
+    type: 'string',
+    sort: false,
+  },
+
+  uniqueKey: {
+    title: 'Clave única',
+    type: 'string',
+    sort: false,
+  },
+
+  fileNumber: {
+    title: 'Expediente',
+    type: 'string',
+    sort: false,
+  },
+
+  goodDescription: {
+    title: 'Descripción transferente',
+    sort: false,
+  },
+
+  descriptionGoodSae: {
+    title: 'Descripción INDEP',
+    type: 'string',
+    sort: false,
+  },
+
+  quantity: {
+    title: 'Cantidad transferente',
+    type: 'string',
+    sort: false,
+  },
+
+  quantitySae: {
+    title: 'Cantidad INDEP',
+    type: 'string',
+    sort: false,
+  },
+
+  unitMeasure: {
+    title: 'Unidad Medida transferente',
+    type: 'string',
+    sort: false,
+  },
+  saeMeasureUnit: {
+    title: 'Unidad medida INDEP',
+    type: 'string',
+    sort: false,
+  },
+
+  physicalStatusName: {
+    title: 'Estado Físico transferente',
+    type: 'string',
+    sort: false,
+  },
+
+  saePhysicalState: {
+    title: 'Estado Físico INDEP',
+    type: 'string',
+    sort: false,
+  },
+
+  stateConservationName: {
+    title: 'Conservación transferente',
+    type: 'string',
+    sort: false,
+  },
+
+  stateConservationSae: {
+    title: 'Estado conservación INDEP',
+    type: 'string',
+    sort: false,
+  },
+};
