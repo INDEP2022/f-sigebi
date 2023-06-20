9f1d6df65598ae3e30ad15b1690ee0f7617d9ddb
Jessica Orozco <jessicaorozco@gmail.com>
2023-06-20 10:49:58 -0400

    changes acta

--- src/app/pages/administrative-processes/proceedings-conversion/proceedings-conversion.module.ts
+++ src/app/pages/administrative-processes/proceedings-conversion/proceedings-conversion.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
 import { ReactiveFormsModule } from '@angular/forms';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { FormLoaderComponent } from 'src/app/@standalone/form-loader/form-loader.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { FormSearchHandlerModule } from '../../juridical-processes/shared/form-search-handler/form-search-handler.module';
 import { ProceedingsConversionDetailComponent } from './proceedings-conversion-detail/proceedings-conversion-detail.component';
 import { ProceedingsConversionRoutingModule } from './proceedings-conversion-routing.module';
 import { ProceedingsConversionComponent } from './proceedings-conversion/proceedings-conversion.component';
@@ -23,6 +25,9 @@ import { ProceedingsConversionComponent } from './proceedings-conversion/proceed
     SharedModule,
     ReactiveFormsModule,
     ModalModule.forChild(),
+    AccordionModule,
+    FormLoaderComponent,
+    FormSearchHandlerModule,
   ],
 })
 export class ProceedingsConversionModule {}
