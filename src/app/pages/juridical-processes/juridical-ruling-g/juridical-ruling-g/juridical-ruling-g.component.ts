9e180742f2f877d91db99491cc0c26b7beb07d74
henrygiovannisanchezsolis <sshenrygiovanni@gmail.com>
2023-06-20 22:14:11 -0600

    update noexpediente

--- src/app/pages/juridical-processes/juridical-ruling-g/juridical-ruling-g/juridical-ruling-g.component.ts
+++ src/app/pages/juridical-processes/juridical-ruling-g/juridical-ruling-g/juridical-ruling-g.component.ts
@@ -3522,7 +3522,7 @@ export class JuridicalRulingGComponent
     this.goodServices
       .getByExpedient(
         this.expedientesForm.get('noExpediente').value,
-        //this.params.getValue()
+        //this.params.getValue() //Se estaba enviado dos veces el expediente y marcaba error.
       )
       .subscribe({
         next: response => {
