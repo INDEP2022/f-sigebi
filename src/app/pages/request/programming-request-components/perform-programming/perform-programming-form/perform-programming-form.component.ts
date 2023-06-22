68c693f6722e5283a35c993beab733e3c746ee8d
malavidaxz <110206463+malavidaxz@users.noreply.github.com>
2023-06-22 16:02:59 -0500

    cambio

--- src/app/pages/request/programming-request-components/perform-programming/perform-programming-form/perform-programming-form.component.ts
+++ src/app/pages/request/programming-request-components/perform-programming/perform-programming-form/perform-programming-form.component.ts
@@ -1005,8 +1005,8 @@ export class PerformProgrammingFormComponent
     this.loadingGoods = true;
     const filterColumns: Object = {
       regionalDelegation: Number(this.regionalDelegationUser.id),
-      //transferent: Number(this.transferentId),
-      transferent: Number(760),
+      transferent: Number(this.transferentId),
+      // transferent: Number(760),
       relevantType: Number(this.idTypeRelevant),
       statusGood: 'APROBADO',
     };
