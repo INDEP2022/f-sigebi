50d9c256ba996c2b38102229b50d1b676a16f3d2
ansoni01 <40308661+ansoni01@users.noreply.github.com>
2023-06-22 23:56:52 -0500

    Feature/ansoni dev (#2429)
    * correcciones proceso de reclamacion de pago
    
    * correcciones proceso de reclamacion de pago 2
    
    * Corregir validacion para boton regresar
    
    * avances proceso de pago
    
    * Se hacen modificaciones según observaciones en Bug Tracker de pantalla historical-good-situation (#2413)
    
    * Agregar validacion al imprimir oficio
    
    * avances proceso de pago 2
    
    * change into appraisal-goods
    
    * update effective-numerary-devolution
    
    * cambio (#2416)
    
    * loading aprobar dictamen (#2417)
    
    * Programming module (#2418)
    
    * solve errors component print report modal
    
    * changes date and validation button acept programming
    
    * Se concluye la forma (#2420)
    
    * Fix row user extern (#2421)
    
    * cambios msj
    
    * fix oficio de desahogo relacioado
    
    * Caracteristicas bienes1.7 (#2422)
    
    * cambios
    
    * cambios
    
    * cambios
    
    * cambios
    
    * massive-change-status (#2424)
    
    * Se crean filtros en columnas de pantall property-registration (#2425)
    
    * avances proceso de pago 3 (#2426)
    
    * Proceso reclamacion pago (#2427)
    
    * avances proceso de pago 3
    
    * avances proceso de pago 4
    
    * bugs 22- 06-2023
    
    * enviroment
    
    ---------
    
    Co-authored-by: Wilmer Solorzano <wilmersolorzano.dev@gmail.com>
    Co-authored-by: IndustriasBeflo <riiiicho@gmail.com>
    Co-authored-by: Carlos Mario Reyes <104024599+CarlosMario-Pro@users.noreply.github.com>
    Co-authored-by: Henry Giovanni Sánchez Solís <111904384+HenryGiovanniSanchezSolis@users.noreply.github.com>
    Co-authored-by: Jessica Orozco <jessicaorozco@gmail.com>
    Co-authored-by: Jean <110206463+malavidaxz@users.noreply.github.com>
    Co-authored-by: joselalo97 <119750919+joselalo97@users.noreply.github.com>
    Co-authored-by: gus2612 <114940445+gus2612@users.noreply.github.com>
    Co-authored-by: federico <72366423+saustras@users.noreply.github.com>
    Co-authored-by: aalvarezg1998 <107310910+aalvarezg1998@users.noreply.github.com>
    Co-authored-by: RodasPacco <angelorp93@gmail.com>
    Co-authored-by: CxG346 <107780267+CxG346@users.noreply.github.com>
    Co-authored-by: WilmerJSB04 <131001742+WilmerJSB04@users.noreply.github.com>

--- src/app/pages/parameterization/parameter-maintenance/columns.ts
+++ src/app/pages/parameterization/parameter-maintenance/columns.ts
@@ -1,3 +1,5 @@
+import { CustomDateFilterComponent } from '../../../@standalone/shared-forms/filter-date-custom/custom-date-filter';
+
 export const COLUMNSPARAMETER = {
   id: {
     title: 'Clave parámetro',
@@ -11,20 +13,30 @@ export const COLUMNSPARAMETER = {
     title: 'Fecha inical',
     sort: false,
     type: 'html',
+    width: '13%',
     valuePrepareFunction: (text: string) => {
       return `
         ${text ? text.split('T')[0].split('-').reverse().join('-') : ''}  
       `;
     },
+    filter: {
+      type: 'custom',
+      component: CustomDateFilterComponent,
+    },
   },
   endDate: {
     title: 'Fecha final',
     sort: false,
+    width: '13%',
     type: 'html',
     valuePrepareFunction: (text: string) => {
       return `
         ${text ? text.split('T')[0].split('-').reverse().join('-') : ''}  
       `;
+    },
+    filter: {
+      type: 'custom',
+      component: CustomDateFilterComponent,
     },
   },
   initialValue: {
