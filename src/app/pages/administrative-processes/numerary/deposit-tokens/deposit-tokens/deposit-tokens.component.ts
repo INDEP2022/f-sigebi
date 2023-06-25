3234849b11173e72c13301b9299da732a09220a3
WilmerJSB04 <131001742+WilmerJSB04@users.noreply.github.com>
2023-06-25 19:46:25 -0400

    Deposit tokens (#2476)
    * avances proceso de pago 3
    
    * avances proceso de pago 4
    
    * avances deposit-tokens
    
    * avances deposit-tokens 24-06-2023
    
    * avances proceso de reclamacion de pago
    
    * test 2
    
    * test 4
    
    * finalizacion reclamacion de pago y avances de deposit tokens

--- src/app/pages/administrative-processes/numerary/deposit-tokens/deposit-tokens/deposit-tokens.component.ts
+++ src/app/pages/administrative-processes/numerary/deposit-tokens/deposit-tokens/deposit-tokens.component.ts
@@ -10,6 +10,7 @@ import {
   ListParams,
   SearchFilter,
 } from 'src/app/common/repository/interfaces/list-params';
+import { ExcelService } from 'src/app/common/services/excel.service';
 import { DynamicCatalogsService } from 'src/app/core/services/dynamic-catalogs/dynamiccatalog.service';
 import { AccountMovementService } from 'src/app/core/services/ms-account-movements/account-movement.service';
 import { GoodService } from 'src/app/core/services/ms-good/good.service';
@@ -32,13 +33,15 @@ export class DepositTokensComponent extends BasePage implements OnInit {
   dataMovements: any = null;
   columnFilters: any = [];
   paramsList = new BehaviorSubject<ListParams>(new ListParams());
+  jsonToCsv: any[] = [];
   constructor(
     private fb: FormBuilder,
     private modalService: BsModalService,
     private accountMovementService: AccountMovementService,
     private datePipe: DatePipe,
     private readonly goodServices: GoodService,
-    private dynamicCatalogsService: DynamicCatalogsService
+    private dynamicCatalogsService: DynamicCatalogsService,
+    private excelService: ExcelService
   ) {
     super();
     this.settings = {
@@ -273,6 +276,18 @@ export class DepositTokensComponent extends BasePage implements OnInit {
     this.form.get('balanceAt').setValue('');
   }
 
+  async cleanDataBank() {
+    this.form.get('bank').setValue('');
+    this.form.get('account').setValue('');
+    this.form.get('accountType').setValue('');
+    this.form.get('currency').setValue('');
+    this.form.get('square').setValue('');
+    this.form.get('branch').setValue('');
+    this.form.get('balanceOf').setValue('');
+    this.form.get('description').setValue('');
+    this.form.get('balanceAt').setValue('');
+  }
+
   // BUTTONS FUNCTIONS //
 
   async desconciliarFunc() {
@@ -287,6 +302,7 @@ export class DepositTokensComponent extends BasePage implements OnInit {
       } else {
         let obj: any = {
           numberMotion: this.dataMovements.no_movimiento,
+          numberAccount: this.dataMovements.no_cuenta,
           numberGood: null,
           numberProceedings: null,
         };
@@ -298,10 +314,9 @@ export class DepositTokensComponent extends BasePage implements OnInit {
               `El bien ${this.dataMovements.no_bien} ha sido desconciliado`,
               ''
             );
-            this.loading = false;
           },
           error: err => {
-            this.alert('error', `Error al desconciliar`, '');
+            this.alert('error', `Error al desconciliar`, err.error.message);
             this.loading = false;
           },
         });
@@ -309,6 +324,44 @@ export class DepositTokensComponent extends BasePage implements OnInit {
     }
   }
 
+  async actualizarFunc() {
+    this.getAccount();
+    if (this.dataMovements.bank) {
+      this.cleanDataBank();
+    }
+  }
+
+  async importar() {}
+
+  onFileChange(event: Event) {
+    const files = (event.target as HTMLInputElement).files;
+    if (files.length != 1) throw 'No files selected, or more than of allowed';
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(files[0]);
+    fileReader.onload = () => this.readExcel(fileReader.result);
+  }
+
+  readExcel(binaryExcel: string | ArrayBuffer) {
+    try {
+      const excelImport = this.excelService.getData<any>(binaryExcel);
+      this.data1.load(excelImport);
+      this.totalItems = this.data1.count();
+    } catch (error) {
+      this.onLoadToast('error', 'Ocurrio un error al leer el archivo', 'Error');
+    }
+  }
+
+  async exportar() {
+    const filename: string = 'Deposit Tokens';
+    const jsonToCsv = await this.returnJsonToCsv();
+    console.log('jsonToCsv', jsonToCsv);
+    this.jsonToCsv = jsonToCsv;
+    this.excelService.export(this.jsonToCsv, { type: 'csv', filename });
+  }
+
+  async returnJsonToCsv() {
+    return this.data1.getAll();
+  }
   // GET DETAILS CURRENCY //
   async getTvalTable5(currency: any) {
     const params = new ListParams();
@@ -353,6 +406,7 @@ export class DepositTokensComponent extends BasePage implements OnInit {
       },
     });
   }
+
   openForm(data?: any) {
     const modalConfig = MODAL_CONFIG;
     let noCuenta = null;
