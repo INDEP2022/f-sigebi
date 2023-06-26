ed6982744cdaa6c61d2d45287d153a42fc5d9b75
aalvarezg1998 <107310910+aalvarezg1998@users.noreply.github.com>
2023-06-26 18:20:52 -0500

    Ajuste de bugs

--- src/app/pages/administrative-processes/legal-regularization/legal-regularization/legal-regularization.component.ts
+++ src/app/pages/administrative-processes/legal-regularization/legal-regularization/legal-regularization.component.ts
@@ -115,8 +115,8 @@ export class LegalRegularizationComponent extends BasePage implements OnInit {
         } else {
           if (!this.redicrectScan) {
             this.alert(
-              'error',
-              'ERROR',
+              'info',
+              'Información',
               `El estatus del bien ${this.numberGood.value} es incorrecto. Los estatus validos son  ADM o REJ.'`
             );
           }
@@ -124,7 +124,7 @@ export class LegalRegularizationComponent extends BasePage implements OnInit {
       },
       error: err => {
         console.log(err);
-        this.onLoadToast('error', 'ERROR', err.error.message);
+        this.alert('error', 'Ha ocurrido un error', err.error.message);
       },
     });
   }
