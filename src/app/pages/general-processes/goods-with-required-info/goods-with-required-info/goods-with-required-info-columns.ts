a7dec9d2a4f9fcb29badc6c6dc08bccaa2d4ca99
Jessica Orozco <jessicaorozco@gmail.com>
2023-06-26 20:09:58 -0400

    changes into required

--- src/app/pages/general-processes/goods-with-required-info/goods-with-required-info/goods-with-required-info-columns.ts
+++ src/app/pages/general-processes/goods-with-required-info/goods-with-required-info/goods-with-required-info-columns.ts
@@ -2,11 +2,9 @@ export const GOODS_WITH_REQUIRED_INFO_COLUMNS = {
   id: {
     title: 'No. Bien',
     sort: false,
-    filter: true,
   },
   motive: {
     title: 'Motivo',
     sort: false,
-    filter: true,
   },
 };
