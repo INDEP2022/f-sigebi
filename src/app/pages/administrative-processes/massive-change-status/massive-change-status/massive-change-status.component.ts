07fc2afd082f097e65415b07b33d0f46b8f4df28
gus2612 <114940445+gus2612@users.noreply.github.com>
2023-06-26 22:02:04 -0600

    Programming module (#2505)
    * solve error reject programming
    
    * proceeding SAT complete
    
    * correction table with input

--- src/app/pages/administrative-processes/massive-change-status/massive-change-status/massive-change-status.component.ts
+++ src/app/pages/administrative-processes/massive-change-status/massive-change-status/massive-change-status.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { LocalDataSource } from 'ng2-smart-table';
@@ -37,10 +42,10 @@ export class MassiveChangeStatusComponent extends BasePage implements OnInit {
   ids: IDs[];
   form: FormGroup;
   goods: any[] = [];
-  availableToUpdate: any[] = []
+  availableToUpdate: any[] = [];
   idsNotExist: NotData[] = [];
-  idsNotUpdated: any[] = []
-  idsUpdated: any[] = []
+  idsNotUpdated: any[] = [];
+  idsUpdated: any[] = [];
   showError: boolean = false;
   showStatus: boolean = false;
   availableToAssing: boolean = false;
@@ -71,8 +76,11 @@ export class MassiveChangeStatusComponent extends BasePage implements OnInit {
     this.settings.columns = COLUMNS;
     this.settings.actions = false;
     this.settings.rowClassFunction = (row: { data: { avalaible: any } }) =>
-    row.data.avalaible != null ?
-    (row.data.avalaible ? 'bg-success text-white' : 'bg-dark text-white') : '';
+      row.data.avalaible != null
+        ? row.data.avalaible
+          ? 'bg-success text-white'
+          : 'bg-dark text-white'
+        : '';
   }
 
   ngOnInit(): void {
@@ -89,10 +97,9 @@ export class MassiveChangeStatusComponent extends BasePage implements OnInit {
       },
     });
 
-    this.params.pipe(takeUntil(this.$unSubscribe))
-    .subscribe(params => {
+    this.params.pipe(takeUntil(this.$unSubscribe)).subscribe(params => {
       console.log(params);
-      this.paginator(params.page, params.limit)
+      this.paginator(params.page, params.limit);
     });
   }
 
@@ -121,8 +128,8 @@ export class MassiveChangeStatusComponent extends BasePage implements OnInit {
       this.goods = [];
       this.availableToUpdate = [];
       this.idsNotExist = [];
-      this.goodStatus.reset()
-      this.observation.reset()
+      this.goodStatus.reset();
+      this.observation.reset();
       this.showError = false;
       this.showStatus = false;
 
@@ -154,7 +161,10 @@ export class MassiveChangeStatusComponent extends BasePage implements OnInit {
       count = count + 1;
       this.goodServices.getById(good.No_bien).subscribe({
         next: response => {
-          this.goods.push({...JSON.parse(JSON.stringify(response)).data[0], avalaible: null});
+          this.goods.push({
+            ...JSON.parse(JSON.stringify(response)).data[0],
+            avalaible: null,
+          });
           console.log(this.goods);
           this.addStatus();
           /* this.validGood(JSON.parse(JSON.stringify(response)).data[0]); */ //!SE TIENE QUE REVISAR
@@ -177,93 +187,90 @@ export class MassiveChangeStatusComponent extends BasePage implements OnInit {
 
   addStatus() {
     /* this.data.load(this.goods); */
-    this.paginator()
+    this.paginator();
     this.data.refresh();
   }
 
   paginator(noPage: number = 1, elementPerPage: number = 10) {
     const indiceInicial = (noPage - 1) * elementPerPage;
     const indiceFinal = indiceInicial + elementPerPage;
-  
+
     let paginateData = this.goods.slice(indiceInicial, indiceFinal);
-    this.data.load(paginateData)
+    this.data.load(paginateData);
   }
 
   //Asigna estatus
-  assignsStatus(){
-    console.log(this.goodStatus.value)
-    if(this.goodStatus.value != null){
-      if(this.observation.value != null){
-        for(let good of this.goods){
-          console.log(good)  
+  assignsStatus() {
+    console.log(this.goodStatus.value);
+    if (this.goodStatus.value != null) {
+      if (this.observation.value != null) {
+        for (let good of this.goods) {
+          console.log(good);
           this.massiveGoodService.getBanVal(good.status).subscribe(
-              res => {
-                console.log({msg:'res banval', data:res.data[0].count})
-                const count = res.data[0].count
-                if(count == 0){
-                  good.avalaible = false
-                  this.idsNotExist.push({
-                    id: good.goodId,
-                    reason: `Bien no disponible para actualización: `,
-                  });
-                  //Pintar la fila no_disponible
-                }else if(count > 0){
-                  good.avalaible = true
-                  good.status = this.goodStatus.value.status
-                  this.availableToUpdate.push({
-                    goodId: good.goodId,
-                    message: 'disponible para actualizar'
-                  })
-                  if(this.goodStatus.value.status == 'CAN'){
-                    good.observations = `${this.observation.value}. ${good.observations}`
-                  }
+            res => {
+              console.log({ msg: 'res banval', data: res.data[0].count });
+              const count = res.data[0].count;
+              if (count == 0) {
+                good.avalaible = false;
+                this.idsNotExist.push({
+                  id: good.goodId,
+                  reason: `Bien no disponible para actualización: `,
+                });
+                //Pintar la fila no_disponible
+              } else if (count > 0) {
+                good.avalaible = true;
+                good.status = this.goodStatus.value.status;
+                this.availableToUpdate.push({
+                  goodId: good.goodId,
+                  message: 'disponible para actualizar',
+                });
+                if (this.goodStatus.value.status == 'CAN') {
+                  good.observations = `${this.observation.value}. ${good.observations}`;
                 }
-              },
-              err =>{
-                console.log({msg:'err banval', data:err})
               }
-            )
+            },
+            err => {
+              console.log({ msg: 'err banval', data: err });
+            }
+          );
         }
-        this.alert('success','Se asignó estatus a los Bienes','')
-        this.paginator()
-      }else{
-        this.alert('warning','Debe especificar el motivo del cambio.','')
+        this.alert('success', 'Se asignó estatus a los Bienes', '');
+        this.paginator();
+      } else {
+        this.alert('warning', 'Debe especificar el motivo del cambio.', '');
       }
-    }else{
-      this.alert('warning','Debe especificar el Estatus','')
+    } else {
+      this.alert('warning', 'Debe especificar el Estatus', '');
     }
   }
 
-  applyStatus(){
-    for(let good of this.data['data']){
-      if(good.avalaible){
-        const model: IGood ={
+  applyStatus() {
+    for (let good of this.data['data']) {
+      if (good.avalaible) {
+        const model: IGood = {
           id: good.id,
           goodId: good.goodId,
           status: good.status,
-          observations: good.observations
-        }
-  
+          observations: good.observations,
+        };
+
         this.goodServices.update(model).subscribe(
           res => {
             const modelHistory: IHistoryGood = {
               propertyNum: good.goodId,
               status: this.goodStatus.value.status,
               changeDate: new Date().toISOString(),
-              userChange: 
-              localStorage.getItem('username') ==
-              'sigebiadmon'
-                ? localStorage.getItem('username')
-                : localStorage
-                    .getItem('username')
-                    .toLocaleUpperCase(),
+              userChange:
+                localStorage.getItem('username') == 'sigebiadmon'
+                  ? localStorage.getItem('username')
+                  : localStorage.getItem('username').toLocaleUpperCase(),
               statusChangeProgram: 'FACTADBCAMBIOESTAT',
-              reasonForChange: this.observation.value
-            }
-            
+              reasonForChange: this.observation.value,
+            };
+
             this.historyStatusGoodService.create(modelHistory).subscribe(
               res => {
-                this.idsUpdated.push(good)
+                this.idsUpdated.push(good);
                 this.data.refresh();
               },
               err => {
@@ -276,19 +283,18 @@ export class MassiveChangeStatusComponent extends BasePage implements OnInit {
             );
           },
           err => {
-            this.idsNotUpdated.push(good)
+            this.idsNotUpdated.push(good);
           }
-        )
-      }else{
-        this.idsNotUpdated.push(good)
+        );
+      } else {
+        this.idsNotUpdated.push(good);
       }
-      
     }
-    this.alert('success','Se aplicó el cambio de estatus en los Bienes','')
-    this.availableToAssing = false
+    this.alert('success', 'Se aplicó el cambio de estatus en los Bienes', '');
+    this.availableToAssing = false;
   }
 
- /*  applyStatus() {
+  /*  applyStatus() {
     for (let good of this.data['data']) {
       if (good.avalaible) {
         const model: IGood = {
@@ -373,16 +379,16 @@ export class MassiveChangeStatusComponent extends BasePage implements OnInit {
     });
   }
 
-  clearAll(){
-    this.data.load([])
-    this.goodStatus.reset()
-    this.observation.reset()
-    this.idsNotExist = []
-    this.idsNotUpdated = []
-    this.idsUpdated = []
-    this.goods = []
-    this.availableToAssing = false
-    this.showError = false
+  clearAll() {
+    this.data.load([]);
+    this.goodStatus.reset();
+    this.observation.reset();
+    this.idsNotExist = [];
+    this.idsNotUpdated = [];
+    this.idsUpdated = [];
+    this.goods = [];
+    this.availableToAssing = false;
+    this.showError = false;
   }
 
   /* validGood(good: IGood) {
