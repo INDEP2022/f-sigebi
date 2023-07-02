82fde210bfd292efee4c66a3c2c6b24d521ed447
RodasPacco <angelorp93@gmail.com>
2023-07-02 18:51:03 -0500

    cambios (#2681)

--- src/app/pages/administrative-processes/goods-management/services/goods-management.service.ts
+++ src/app/pages/administrative-processes/goods-management/services/goods-management.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { ITrackedGood } from 'src/app/core/models/ms-good-tracker/tracked-good.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GoodsManagementService {
   selectedGoodSubject = new Subject<number>();
+  refreshData = new Subject<boolean>();
+  refreshTable = new Subject<boolean>();
+  clear = new Subject<boolean>();
+  data: ITrackedGood[];
+  loading: boolean = false;
   constructor() {}
 }
