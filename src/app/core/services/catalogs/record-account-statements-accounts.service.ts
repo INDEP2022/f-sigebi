3168d6f01505e216d8c041642361555386267a8c
MarioReyes <cmario.reyesp@gmail.com>
2023-07-05 12:03:47 -0500

    Se crea check para movimientos con nomberGood diferente a null

--- src/app/core/services/catalogs/record-account-statements-accounts.service.ts
+++ src/app/core/services/catalogs/record-account-statements-accounts.service.ts
@@ -14,7 +14,9 @@ export class RecordAccountStatementsAccountsService
   extends HttpService
   implements ICrudMethods<IRecordAccountStatements>
 {
-  private readonly route2: string = ENDPOINT_LINKS.BankAccount;
+  private readonly route: string = ENDPOINT_LINKS.BankAccount;
+  private readonly route2: string = ENDPOINT_LINKS.AccountMovements;
+
   constructor(
     private recordAccountStatementsServiceRepository: Repository<IRecordAccountStatements>
   ) {
@@ -23,15 +25,21 @@ export class RecordAccountStatementsAccountsService
   }
 
   getById(
-    id: string | number,
+    bankCode: string | number,
     params?: ListParams
   ): Observable<IRecordAccountStatements> {
-    const route = `${this.route2}?filter.cveBank=$ilike:${id}`;
-    console.log(this.route2);
-    console.log(route);
+    const route = `${this.route}?filter.cveBank=$eq:${bankCode}`;
     return this.get(route, params);
     // return this.recordAccountStatementsServiceRepository.getById02(route, params); //Usando este repositorio, me encadena para usar el microservicio de 'catalog'
   }
-}
 
+  getDataAccount(
+    accountNumber: string | number,
+    params?: ListParams
+  ): Observable<IRecordAccountStatements> {
+    const route = `${this.route2}?filter.numberAccount=$eq:${accountNumber}`;
+    return this.get(route, params);
+  }
+}
 //http://sigebimsqa.indep.gob.mx/accountmvmnt/api/v1/bank-account?filter.cveBank=$ilike:SANTAND
+//http://sigebimsqa.indep.gob.mx/accountmvmnt/api/v1/account-movements?filter.numberAccount=$ilike:17448178
--- src/app/pages/administrative-processes/numerary/record-account-statements/record-account-statements/record-account-statements.component.ts
+++ src/app/pages/administrative-processes/numerary/record-account-statements/record-account-statements/record-account-statements.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LocalDataSource } from 'ng2-smart-table';
 import { BsModalService } from 'ngx-bootstrap/modal';
-import { BehaviorSubject } from 'rxjs';
-import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import { BehaviorSubject, takeUntil } from 'rxjs';
+import {
+  ListParams,
+  SearchFilter,
+} from 'src/app/common/repository/interfaces/list-params';
 import { BasePage } from 'src/app/core/shared/base-page';
 import { RECORDS_ACCOUNT_STATEMENTS_COLUMNS } from './record-account-statements-columns';
 
@@ -32,11 +35,14 @@ export class RecordAccountStatementsComponent
   banks = new DefaultSelect();
   bankAccountSelect = new DefaultSelect();
 
+  dataAccount: LocalDataSource = new LocalDataSource();
+  dataAccountPaginated: number;
+  current: string;
+
   constructor(
     private fb: FormBuilder,
     private modalService: BsModalService,
     private recordAccountStatementsService: RecordAccountStatementsService,
-
     private recordAccountStatementsAccountsService: RecordAccountStatementsAccountsService
   ) {
     super();
@@ -50,6 +56,54 @@ export class RecordAccountStatementsComponent
   ngOnInit(): void {
     this.prepareForm();
     this.searchBanks();
+    this.dataAccount
+      .onChanged()
+      .pipe(takeUntil(this.$unSubscribe))
+      .subscribe(change => {
+        if (change.action === 'filter') {
+          let filters = change.filter.filters;
+          filters.map((filter: any) => {
+            let field = ``;
+            let searchFilter = SearchFilter.ILIKE;
+            field = `filter.${filter.field}`;
+            /*SPECIFIC CASES*/
+            switch (filter.field) {
+              case 'dateMotion':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'deposit':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'withdrawal':
+                searchFilter = SearchFilter.EQ;
+                break;
+              case 'cveConcept':
+                searchFilter = SearchFilter.ILIKE;
+                break;
+              default:
+                searchFilter = SearchFilter.ILIKE;
+                break;
+            }
+            if (filter.search !== '') {
+              console.log(
+                (this.columnFilters[field] = `${searchFilter}:${filter.search}`)
+              );
+              this.columnFilters[field] = `${searchFilter}:${filter.search}`;
+            } else {
+              delete this.columnFilters[field];
+            }
+          });
+          this.params.value.page = 1;
+          if (this.dataAccountPaginated) {
+            this.searchDataAccount(this.dataAccountPaginated);
+          }
+        }
+      });
+    this.params.pipe(takeUntil(this.$unSubscribe)).subscribe(() => {
+      if (this.dataAccountPaginated) {
+        this.searchDataAccount(this.dataAccountPaginated);
+      }
+    });
   }
 
   private prepareForm() {
@@ -66,14 +120,13 @@ export class RecordAccountStatementsComponent
     });
   }
 
-  //Trae la lista de bancos
+  // Trae la lista de bancos
   searchBanks() {
     this.recordAccountStatementsService
       .getAll(this.params.getValue())
       .subscribe({
         next: (response: { data: any[]; count: number }) => {
           this.banks = new DefaultSelect(response.data, response.count);
-          console.log(this.banks);
           this.loading = false;
         },
         error: (err: any) => {
@@ -83,7 +136,7 @@ export class RecordAccountStatementsComponent
       });
   }
 
-  //Asigna el valor del banco seleccionado a la función "searchBankAccount"
+  // Asigna el valor del banco seleccionado a la función "searchBankAccount"
   onBankSelectChange(value: any) {
     if (value && value.bankCode) {
       const bankCode = value.bankCode;
@@ -93,9 +146,8 @@ export class RecordAccountStatementsComponent
     }
   }
 
-  //Toma el banco seleccionado y busca todas las cuentas pertenecientes a ese banco
+  // Toma el banco seleccionado y busca todas las cuentas pertenecientes a ese banco
   searchBankAccount(bankCode: string) {
-    console.log(bankCode);
     this.recordAccountStatementsAccountsService
       .getById(bankCode, this.params.getValue())
       .subscribe({
@@ -104,7 +156,6 @@ export class RecordAccountStatementsComponent
             response.data,
             response.count
           );
-          console.log(this.bankAccountSelect);
           this.loading = false;
         },
         error: (err: any) => {
@@ -114,20 +165,57 @@ export class RecordAccountStatementsComponent
       });
   }
 
+  // Establece los valores en los input de datos de la cuenta
   onBankAccountSelectChange(value: any) {
-    console.log(value);
+    const accountNumber = value.accountNumber;
+    this.searchDataAccount(accountNumber);
 
     // Obtener los valores correspondientes de la cuenta seleccionada
-    const square = value?.square;
-    const branch = value?.branch;
-    const accountType = value?.accountType;
-    const currency = value?.cveCurrency;
+    const square = value?.square ?? 'Sin datos';
+    const branch = value?.branch ?? 'Sin datos';
+    const accountType = value?.accountType ?? 'Sin datos';
+    let currency = value?.cveCurrency ?? 'Sin datos';
+
+    // Quitar las comillas simples del valor de currency, si existen
+    currency = currency.replace(/'/g, '');
 
     // Asignar los valores al formulario
     this.form.get('square').setValue(square);
     this.form.get('branch').setValue(branch);
     this.form.get('accountType').setValue(accountType);
     this.form.get('currency').setValue(currency);
+    const current = this.form.get('currency').value;
+    this.current = current;
+  }
+
+  // Establece los valores de movimientos de la cuenta seleccionada a la tabla
+  searchDataAccount(accountNumber: number) {
+    this.dataAccountPaginated = accountNumber;
+    this.recordAccountStatementsAccountsService
+      .getDataAccount(accountNumber, this.params.getValue())
+      .subscribe({
+        next: response => {
+          this.loading = true;
+          const dataSource = new LocalDataSource(response.data); // Crear una nueva instancia de LocalDataSource con los datos
+          this.dataAccount = dataSource; // Asignar la instancia de LocalDataSource a dataAccount
+          this.totalItems = response.count;
+          this.loading = false;
+
+          // Imprimir los valores de numberGood en el console.log
+          const numberGoodValues = response.data.map(
+            (item: any) => item.numberGood
+          );
+          console.log('Valores de numberGood:', numberGoodValues);
+        },
+        error: (err: any) => {
+          this.loading = false;
+          this.alert(
+            'warning',
+            'No existen datos de la cuenta seleccionada',
+            ``
+          );
+        },
+      });
   }
 
   cleandInfoGoods() {
