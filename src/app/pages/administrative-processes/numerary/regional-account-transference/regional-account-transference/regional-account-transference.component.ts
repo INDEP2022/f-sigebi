4b35743af4ab37ed39b42e1a632e66c6002619ce
dchavarror <89142501+dchavarror@users.noreply.github.com>
2023-07-08 10:12:58 -0500

    Se suben cambios de regional transfer (#2826)

--- src/app/pages/administrative-processes/numerary/regional-account-transference/regional-account-transference/regional-account-transference.component.ts
+++ src/app/pages/administrative-processes/numerary/regional-account-transference/regional-account-transference/regional-account-transference.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LocalDataSource } from 'ng2-smart-table';
 import { BehaviorSubject } from 'rxjs';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import { SurvillanceService } from 'src/app/core/services/ms-survillance/survillance.service';
+import { TranfergoodService } from 'src/app/core/services/ms-transfergood/transfergood.service';
 import { BasePage } from 'src/app/core/shared/base-page';
 import {
   KEYGENERATION_PATTERN,
   STRING_PATTERN,
 } from 'src/app/core/shared/patterns';
+import { ExcelService } from '../../../../../common/services/excel.service';
 import { REGIONAL_ACCOUNT_COLUMNS } from './regional-account-columns';
 
+interface IExcelToJson {
+  expediente: number;
+  bien: number;
+}
 @Component({
   selector: 'app-regional-account-transference',
   templateUrl: './regional-account-transference.component.html',
@@ -21,9 +29,16 @@ export class RegionalAccountTransferenceComponent
   form: FormGroup;
   total = '22,891.26';
   data1: any[] = [];
+  dataTable = new LocalDataSource();
   params = new BehaviorSubject<ListParams>(new ListParams());
   totalItems: number = 0;
-  constructor(private fb: FormBuilder) {
+  data: IExcelToJson[] = [];
+  constructor(
+    private fb: FormBuilder,
+    private survillanceService: SurvillanceService,
+    private ExcelService: ExcelService,
+    private tranfergoodService: TranfergoodService
+  ) {
     super();
     this.settings = {
       ...this.settings,
@@ -67,6 +82,91 @@ export class RegionalAccountTransferenceComponent
         null,
         [Validators.required, Validators.pattern(KEYGENERATION_PATTERN)],
       ],
+    });
+  }
+
+  getTransNumerario() {
+    this.data1 = [];
+    //this.dataTable.load([]);
+    console.log(
+      "this.form.get('idRequest').value ",
+      this.form.get('idRequest').value
+    );
+    this.survillanceService
+      .getTransNumerarioReg(this.form.get('idRequest').value)
+      .subscribe({
+        next: resp => {
+          console.log('resp ', resp);
+          for (let i = 0; resp.data.length; i++) {
+            console.log('data JCH: ', resp.data[i]);
+            if (resp.data[i] != undefined) {
+              let item = {
+                file: resp.data[i].no_expediente,
+                good: resp.data[i].no_bien,
+                description: resp.data[i].descripcion,
+                status: resp.data[i].estatus,
+                interests: resp.data[i].tot_interes,
+                total: resp.data[i].tot_numerario,
+              };
+              this.data1.push(item);
+              this.dataTable.load(this.data1);
+            } else {
+              this.dataTable.refresh();
+              break;
+            }
+          }
+        },
+        error: err => {
+          let error = '';
+          if (err.status === 0) {
+            error = 'Revise su conexión de Internet.';
+          } else {
+            error = err.error.message;
+          }
+          this.onLoadToast('error', error, '');
+        },
+      });
+  }
+
+  onFileChange(event: Event) {
+    console.log('Datos Ecxel');
+    const file = (event.target as HTMLInputElement).files[0];
+    let formData = new FormData();
+    formData.append('file', file);
+    this.getDataFile(formData);
+  }
+
+  getDataFile(formData: FormData) {
+    this.data1 = [];
+    this.tranfergoodService.getFileCSV(formData).subscribe({
+      next: resp => {
+        console.log('resp excel ', resp); //resp excel
+        for (let i = 0; resp.data.length; i++) {
+          console.log('data JCH: ', resp.data[i]);
+          if (resp.data[i] != undefined) {
+            let item = {
+              file: resp.data[i].NO_EXPEDIENTE,
+              good: resp.data[i].NO_BIEN,
+              description: resp.data[i].DESCRIPCION,
+              status: resp.data[i].ESTATUS,
+            };
+            this.data1.push(item);
+            this.dataTable.load(this.data1);
+          } else {
+            this.dataTable.refresh();
+            break;
+          }
+        }
+      },
+      error: err => {
+        let error = '';
+        if (err.status === 0) {
+          error = 'Revise su conexión de Internet.';
+        } else {
+          error = err.error.message;
+        }
+        this.onLoadToast('error', error, '');
+      },
     });
   }
 }
