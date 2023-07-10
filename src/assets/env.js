5b0e50a7ce29b4d7a014820123ebd8d875877ffe
Juan Pablo S. Vargas <44420245+pablo-vargas@users.noreply.github.com>
2023-07-10 19:12:49 -0300

    Update env.js

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,10 +1,10 @@
 (function (window) {
   window["env"] = window["env"] || {};
   window["env"].EXTERNAL_INTEGRATION_URL = 'http://www.indep.gob.mx/firmaelectronica'; // not actualized, for local testing
-  window["env"].API_BACKEND_URL = 'http://sigebimsdev.indep.gob.mx/';//'http://20.119.126.102:3000/';//'http://localhost:3000/api/v1/'; // not actualized, for local testing
+  window["env"].API_BACKEND_URL = 'http://sigebimstest.indep.gob.mx/';//'http://20.119.126.102:3000/';//'http://localhost:3000/api/v1/'; // not actualized, for local testing
   
-  window["env"].API_BACKEND_URL2 = 'http://sigebimsdev.indep.gob.mx/';
-  window["env"].API_CONTENT = 'http://sigebimsdev.indep.gob.mx/';
+  window["env"].API_BACKEND_URL2 = 'http://sigebimstest.indep.gob.mx/';
+  window["env"].API_CONTENT = 'http://sigebimstest.indep.gob.mx/';
   
   window["env"].URL_PREFIX = 'api/v1/';
 
@@ -17,6 +17,7 @@
   window["env"].api_external_userInfo ='http://52.142.23.169/realms/indep/protocol/openid-connect/userinfo';
   window["env"].api_external_typeUser = 'http://168.61.50.112:3003/api/v1/user/userType/';
   window["env"].api_external_rolesUser = 'http://168.61.50.112:3003/api/v1/user/roles';
+  window["env"].SOCKET_URL = 'http://socket.indep.gob.mx';
 
   
   
