6c9d9d20dbb5fdba662476d398d5bbe5b5da89c5
IndustriasBeflo <riiiicho@gmail.com>
2023-07-11 21:04:05 +0200

    Ajustes en pantalla masiva

--- src/app/pages/juridical-processes/depositary/bulk-loading-depository-cargo/jp-d-bldc-c-bulk-loading-depository-cargo/jp-d-bldc-c-bulk-loading-depository-cargo.service.ts
+++ src/app/pages/juridical-processes/depositary/bulk-loading-depository-cargo/jp-d-bldc-c-bulk-loading-depository-cargo/jp-d-bldc-c-bulk-loading-depository-cargo.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import {
+  IVChecaPost,
+  IVChecaPostReport,
+} from 'src/app/core/models/ms-depositary/ms-depositary.interface';
+import { MsDepositaryService } from 'src/app/core/services/ms-depositary/ms-depositary.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class JpDBldcCBulkLoadingDepositoryCargoService {
-  constructor() {}
+  constructor(private msMsDepositaryService: MsDepositaryService) {}
+  getAppointmentNumber_PBAplica(goodNumber: number) {
+    return this.msMsDepositaryService.getAppointmentNumber_PBAplica(goodNumber);
+  }
+  getVCheca(cvePayConcept: number) {
+    return this.msMsDepositaryService.getVCheca(cvePayConcept);
+  }
+  getVChecaPost(body: IVChecaPost) {
+    return this.msMsDepositaryService.getVChecaPost(body);
+  }
+  getVChecaPostReport(body: IVChecaPostReport) {
+    return this.msMsDepositaryService.getVChecaPostReport(body);
+  }
 }
