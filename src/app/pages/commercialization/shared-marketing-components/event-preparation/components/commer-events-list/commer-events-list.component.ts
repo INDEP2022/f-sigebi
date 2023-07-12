6972ab38017cad9fcce2b0310142cbbcc2b71e51
unknown <genaroadrian>
2023-07-12 16:35:10 -0600

    Captura de evento

--- src/app/pages/commercialization/shared-marketing-components/event-preparation/components/commer-events-list/commer-events-list.component.ts
+++ src/app/pages/commercialization/shared-marketing-components/event-preparation/components/commer-events-list/commer-events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { LocalDataSource } from 'ng2-smart-table';
+import { BehaviorSubject, takeUntil, tap } from 'rxjs';
 import { FilterParams } from 'src/app/common/repository/interfaces/list-params';
 import { BasePage } from 'src/app/core/shared';
 import { PREPARE_EVENT_EVENTS_LIST_COLUMNS } from '../../utils/table-columns/events-list-columns';
@@ -12,7 +13,7 @@ import { PREPARE_EVENT_EVENTS_LIST_COLUMNS } from '../../utils/table-columns/eve
 export class CommerEventsListComponent extends BasePage implements OnInit {
   params = new BehaviorSubject(new FilterParams());
   totalItems = 0;
-  events: any[] = [];
+  events = new LocalDataSource();
   constructor() {
     super();
     this.settings = {
@@ -24,4 +25,13 @@ export class CommerEventsListComponent extends BasePage implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  columnsFilter() {
+    this.events.onChanged().pipe(
+      takeUntil(this.$unSubscribe),
+      tap(dataSource => {
+        // if(datas)
+      })
+    );
+  }
 }
