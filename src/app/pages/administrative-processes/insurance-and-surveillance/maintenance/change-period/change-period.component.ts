9e502618c844975f5b9c25691f9b8d0b1eeaa11e
WilmerJSB04 <131001742+WilmerJSB04@users.noreply.github.com>
2023-07-13 11:14:11 -0400

    Avances Pantallas Servicio de vigilancia - Manteniminiento - Fichas de Deposito - Actas de Conversion 2 (#2917)
    * avances 12 de julio 2023 - no2
    
    * Avances Pantallas Servicio de vigilancia - Manteniminiento - Fichas de Deposito - Actas de Conversion
    
    * Avances Pantallas Servicio de vigilancia - Manteniminiento - Fichas de Deposito - Actas de Conversion 2

--- src/app/pages/administrative-processes/insurance-and-surveillance/maintenance/change-period/change-period.component.ts
+++ src/app/pages/administrative-processes/insurance-and-surveillance/maintenance/change-period/change-period.component.ts
@@ -1,5 +1,12 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FilterParams,
+  ListParams,
+  SearchFilter,
+} from 'src/app/common/repository/interfaces/list-params';
+import { SurvillanceService } from 'src/app/core/services/ms-survillance/survillance.service';
+import { BasePage } from 'src/app/core/shared/base-page';
 import { DefaultSelect } from 'src/app/shared/components/select/default-select';
 
 @Component({
@@ -7,18 +14,20 @@ import { DefaultSelect } from 'src/app/shared/components/select/default-select';
   templateUrl: './change-period.component.html',
   styles: [],
 })
-export class ChangePeriodComponent {
+export class ChangePeriodComponent extends BasePage {
   form = new FormGroup({
     year: new FormControl(null, Validators.required),
     period: new FormControl(null, Validators.required),
-    delegation: new FormControl(null, Validators.required),
+    delegation: new FormControl(null),
     process: new FormControl(null, Validators.required),
     yearDestiny: new FormControl(null, Validators.required),
     periodDestiny: new FormControl(null, Validators.required),
     delegationDestiny: new FormControl(null, Validators.required),
     processDestiny: new FormControl(null, Validators.required),
   });
-
+  delegationDefault: any = null;
+  processDefault: any = null;
+  public delegations = new DefaultSelect();
   processes = [
     { value: 1, label: 'Supervisión' },
     { value: 2, label: 'Validación' },
@@ -30,7 +39,9 @@ export class ChangePeriodComponent {
   isLoading = false;
   @Output() eventChangePeriod = new EventEmitter();
 
-  constructor() {}
+  constructor(private survillanceService: SurvillanceService) {
+    super();
+  }
 
   // ngOnInit(): void {
   //   this.prepareForm();
@@ -55,7 +66,66 @@ export class ChangePeriodComponent {
 
   changePeriod() {
     console.log(this.form.value);
+    this.form.value.delegation = this.delegationDefault.delegationNumber;
+    this.form.value.delegationDestiny = this.delegationDefault.delegationNumber;
+    this.form.value.processDestiny = this.processDefault.value;
+    console.log(this.form.value);
     this.eventChangePeriod.emit(this.form.value);
+  }
+
+  // DELEGACIONES //
+  async getDelegation(lparams: ListParams) {
+    const params = new FilterParams();
+
+    params.page = lparams.page;
+    params.limit = lparams.limit;
+
+    params.addFilter('description', lparams.text, SearchFilter.ILIKE);
+
+    return new Promise((resolve, reject) => {
+      this.survillanceService
+        .getViewVigDelegations(params.getParams())
+        .subscribe({
+          next: async (response: any) => {
+            console.log('resss', response);
+            let result = response.data.map(async (item: any) => {
+              item['numberAndDescrip'] =
+                item.delegationNumber + ' - ' + item.description;
+            });
+
+            Promise.all(result).then(async (resp: any) => {
+              this.delegations = new DefaultSelect(
+                response.data,
+                response.count
+              );
+              this.loading = false;
+            });
+          },
+          error: error => {
+            this.delegations = new DefaultSelect();
+            this.loading = false;
+            resolve(null);
+          },
+        });
+    });
+  }
+
+  changeDelegations(event: any) {
+    if (event) {
+      this.form.get('delegationDestiny').setValue(event.numberAndDescrip);
+    } else {
+      this.form.get('delegationDestiny').setValue('');
+    }
+    this.delegationDefault = event;
+  }
+
+  changeProcess(event: any) {
+    if (event) {
+      this.form.get('processDestiny').setValue(event.label);
+    } else {
+      this.form.get('processDestiny').setValue('');
+    }
+    this.processDefault = event;
   }
 
   public getDelegations(event: any) {
@@ -68,5 +138,11 @@ export class ChangePeriodComponent {
     // this.bankService.getAll(params).subscribe(data => {
     //   this.banks = new DefaultSelect(data.data, data.count);
     // });
+  }
+
+  cleanForm() {
+    this.delegationDefault = null;
+    this.processDefault = null;
+    this.form.reset();
   }
 }
