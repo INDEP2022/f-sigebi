39f3de527d12bb1775bb1754d71bc43f3da16283
SergioRTurizoM <tsergior@gmail.com>
2023-07-18 16:20:56 -0500

    Inicio tarea findica_008

---
+++ src/app/pages/general-processes/indicators/components/capture-filter-dictamina/capture-filter-dictamina.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DefaultSelect } from 'src/app/shared/components/select/default-select';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@Component({
+  selector: 'capture-filter-dictamina',
+  templateUrl: './capture-filter-dictamina.component.html',
+  standalone: true,
+  imports: [SharedModule],
+  styles: [],
+})
+export class CaptureFilterDictaminaComponent implements OnInit {
+  @Input() isReceptionAndDelivery: boolean = false;
+  @Input() isReceptionStrategies: boolean = false;
+  @Input() isConsolidated: boolean = false;
+
+  form = this.fb.group({
+    de: [null, [Validators.required]],
+    a: [null, [Validators.required]],
+    fecha: [null, [Validators.required]],
+    cordinador: [null, [Validators.required]],
+    usuario: [null, [Validators.required]],
+    transferente: [null, [Validators.required]],
+    emisora: [null, [Validators.required]],
+    autoridad: [null, [Validators.required]],
+    clave: [null, [Validators.required]],
+    tipoVolante: [null, [Validators.required]],
+    tipoEvento: [null],
+  });
+  flyerTypes = ['A', 'AP', 'AS', 'AT', 'OF', 'P', 'PJ', 'T  '];
+  eventTypes = [
+    'Entrega-Comercialización',
+    'Entrega-Donación',
+    'Entrega-Destrucción',
+    'Entrega-Devolución',
+    'Recepción Física',
+    'Entrega',
+  ];
+  select = new DefaultSelect();
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  onConsultar(): void {
+    console.log('Click en Consultar Formulario Dictamina');
+    console.log(this.form.value);
+  }
+}
