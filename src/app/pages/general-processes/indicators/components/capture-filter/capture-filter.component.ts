a3372ebeb02df0aa16c3fe3d0071a9e575632b45
rzuleta <ruben.zuleta@complemento360.com>
2023-07-17 19:48:56 -0500

    cambios

--- src/app/pages/general-processes/indicators/components/capture-filter/capture-filter.component.ts
+++ src/app/pages/general-processes/indicators/components/capture-filter/capture-filter.component.ts
@@ -1,5 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { catchError, tap, throwError } from 'rxjs';
+import {
+  FilterParams,
+  ListParams,
+} from 'src/app/common/repository/interfaces/list-params';
+import { ISegUsers } from 'src/app/core/models/ms-users/seg-users-model';
+import { AffairService } from 'src/app/core/services/catalogs/affair.service';
+import { AuthorityService } from 'src/app/core/services/catalogs/authority.service';
+import { DelegationService } from 'src/app/core/services/catalogs/delegation.service';
+import { IssuingInstitutionService } from 'src/app/core/services/catalogs/issuing-institution.service';
+import { StationService } from 'src/app/core/services/catalogs/station.service';
+import { UsersService } from 'src/app/core/services/ms-users/users.service';
 import { DefaultSelect } from 'src/app/shared/components/select/default-select';
 import { SharedModule } from 'src/app/shared/shared.module';
 
@@ -14,6 +26,13 @@ export class CaptureFilterComponent implements OnInit {
   @Input() isReceptionAndDelivery: boolean = false;
   @Input() isReceptionStrategies: boolean = false;
   @Input() isConsolidated: boolean = false;
+  delegations = new DefaultSelect();
+  affairName = new DefaultSelect();
+  station = new DefaultSelect();
+  authority = new DefaultSelect();
+  transference = new DefaultSelect();
+  users$ = new DefaultSelect<ISegUsers>();
+  dictNumber: string | number = undefined;
   maxDate = new Date();
 
   form = this.fb.group({
@@ -22,9 +41,9 @@ export class CaptureFilterComponent implements OnInit {
     fecha: [null, [Validators.required]],
     cordinador: [null, [Validators.required]],
     usuario: [null, [Validators.required]],
-    transferente: [null, [Validators.required]],
-    emisora: [null, [Validators.required]],
-    autoridad: [null, [Validators.required]],
+    transference: [null, Validators.required],
+    station: [null, Validators.required],
+    authority: [null, Validators.required],
     clave: [null, [Validators.required]],
     tipoVolante: [null, [Validators.required]],
     tipoEvento: [null],
@@ -40,11 +59,98 @@ export class CaptureFilterComponent implements OnInit {
   ];
 
   select = new DefaultSelect();
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private delegationService: DelegationService,
+    private affairService: AffairService,
+    private stationService: StationService,
+    private issuingInstitutionService: IssuingInstitutionService,
+    private authorityService: AuthorityService,
+    private usersService: UsersService
+  ) {}
 
   ngOnInit(): void {}
 
   cleanForm() {
     this.form.reset();
+  }
+
+  getDelegations(params: ListParams) {
+    this.delegationService.getAll(params).subscribe({
+      next: res => (this.delegations = new DefaultSelect(res.data, res.count)),
+      error: () => {
+        this.delegations = new DefaultSelect([], 0);
+      },
+    });
+  }
+
+  getSubjects(params: ListParams) {
+    this.affairService.getAll(params).subscribe({
+      next: data => {
+        this.affairName = new DefaultSelect(data.data, data.count);
+      },
+      error: () => {
+        this.affairName = new DefaultSelect();
+      },
+    });
+  }
+
+  getTransference(params: ListParams) {
+    this.issuingInstitutionService.getTransfers(params).subscribe({
+      next: data => {
+        this.transference = new DefaultSelect(data.data, data.count);
+      },
+      error: () => {
+        this.transference = new DefaultSelect();
+      },
+    });
+  }
+
+  getStation(params: ListParams) {
+    this.stationService.getAll(params).subscribe({
+      next: data => {
+        this.station = new DefaultSelect(data.data, data.count);
+      },
+      error: () => {
+        this.station = new DefaultSelect();
+      },
+    });
+  }
+  getAuthority(params: ListParams) {
+    this.authorityService.getAll(params).subscribe({
+      next: data => {
+        this.authority = new DefaultSelect(data.data, data.count);
+      },
+      error: () => {
+        this.authority = new DefaultSelect();
+      },
+    });
+  }
+
+  getUsers($params: ListParams) {
+    let params = new FilterParams();
+    params.page = $params.page;
+    params.limit = $params.limit;
+    const area = this.form.controls['usuario'].value;
+    params.search = $params.text;
+    this.getAllUsers(params).subscribe();
+  }
+
+  getAllUsers(params: FilterParams) {
+    return this.usersService.getAllSegUsers(params.getParams()).pipe(
+      catchError(error => {
+        this.users$ = new DefaultSelect([], 0, true);
+        return throwError(() => error);
+      }),
+      tap(response => {
+        if (response.count > 0) {
+          const name = this.form.get('usuario').value;
+          const data = response.data.filter(m => m.id == name);
+          console.log(data);
+          this.form.get('usuario').patchValue(data[0]);
+        }
+        this.users$ = new DefaultSelect(response.data, response.count);
+      })
+    );
   }
 }
