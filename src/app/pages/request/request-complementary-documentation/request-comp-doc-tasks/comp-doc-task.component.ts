3300cd7ad6cf2dadd4aa03bd47e87a2d351ab61f
gus2612 <gustavoangelsantosclemente@gmail.com>
2023-07-20 20:45:14 -0600

    changes view

--- src/app/pages/request/request-complementary-documentation/request-comp-doc-tasks/comp-doc-task.component.ts
+++ src/app/pages/request/request-complementary-documentation/request-comp-doc-tasks/comp-doc-task.component.ts
@@ -1,3 +1,4 @@
+import { IRequest } from 'src/app/core/models/requests/request.model';
 import { BasePage } from 'src/app/core/shared';
 
 export abstract class CompDocTasksComponent extends BasePage {
@@ -15,13 +16,52 @@ export abstract class CompDocTasksComponent extends BasePage {
   protected abstract turnReq: boolean;
   protected abstract createReport: boolean;
   protected abstract rejectReq: boolean;
+  protected abstract title: string;
+  protected abstract complementaryDoc: boolean;
+  protected abstract requestInfo: IRequest;
 
   constructor() {
     super();
   }
 
+  titleView(affair: number) {
+    if (affair == 13) {
+      this.title = `DOCUMENTACIÓN COMPLEMENTARIA: Registro de Documentación Complementaria, No. Solicitud ${this.requestInfo.id}`;
+      this.complementaryDoc = true;
+    } else if (affair == 10) {
+      this.title = `Devolución: Registro de documentación complementaria, No. Solicitud ${this.requestInfo.id}`;
+      this.complementaryDoc = true;
+    }
+  }
+
   mapTask(process: string, affair?: number) {
+    console.log('affair', affair);
     switch (process) {
+      case 'register-request':
+        if (affair == 13) {
+          this.regDocForm = true;
+          this.regDocView = false;
+          this.searchRequestSimGoods = true;
+          this.selectGoods = false;
+          this.viewSelectedGoods = false;
+          this.guidelines = false;
+          this.docRequest = false;
+          this.expRequest = true;
+          this.saveRequest = true;
+          this.dictumValidate = false;
+          this.notifyReport = false;
+
+          this.turnReq = true;
+          this.createReport = false;
+          this.rejectReq = false;
+        } else if (affair == 10) {
+          this.searchRequestSimGoods = true;
+          this.regDocForm = true;
+          this.selectGoods = true;
+          this.expRequest = true;
+        }
+
+        break;
       case 'similar-good-register-documentation':
         this.regDocForm = true;
         this.regDocView = false;
@@ -34,6 +74,95 @@ export abstract class CompDocTasksComponent extends BasePage {
         this.saveRequest = true;
         this.dictumValidate = false;
         this.notifyReport = false;
+
+        this.turnReq = true;
+        this.createReport = false;
+        this.rejectReq = false;
+        break;
+
+      case 'guidelines-review':
+        this.regDocForm = false;
+        this.regDocView = true;
+        this.searchRequestSimGoods = false;
+        this.selectGoods = false;
+        this.viewSelectedGoods = true;
+        this.guidelines = true;
+        this.docRequest = false;
+        this.expRequest = true;
+        this.saveRequest = true;
+        this.dictumValidate = false;
+        this.notifyReport = false;
+
+        this.turnReq = true;
+        this.createReport = true;
+        this.rejectReq = false;
+
+        break;
+      case 'analysis-result':
+        this.regDocForm = false;
+        this.regDocView = true;
+        this.searchRequestSimGoods = false;
+        this.selectGoods = true;
+        this.viewSelectedGoods = false;
+        this.guidelines = true;
+        this.docRequest = false;
+        this.expRequest = true;
+        this.saveRequest = true;
+        this.dictumValidate = false;
+        this.notifyReport = false;
+
+        this.turnReq = true;
+        this.createReport = true;
+        this.rejectReq = true;
+
+        break;
+      case 'dictum-validate':
+        this.regDocForm = false;
+        this.regDocView = true;
+        this.searchRequestSimGoods = false;
+        this.selectGoods = true;
+        this.viewSelectedGoods = false;
+        this.guidelines = true;
+        this.docRequest = false;
+        this.expRequest = true;
+        this.saveRequest = true;
+        this.dictumValidate = true;
+        this.notifyReport = false;
+
+        this.turnReq = true;
+        this.createReport = true;
+        this.rejectReq = true;
+
+        break;
+      case 'similar-good-register-documentation':
+        this.regDocForm = true;
+        this.regDocView = false;
+        this.searchRequestSimGoods = true;
+        this.selectGoods = true;
+        this.viewSelectedGoods = false;
+        this.guidelines = false;
+        this.docRequest = false;
+        this.expRequest = true;
+        this.saveRequest = true;
+        this.dictumValidate = false;
+        this.notifyReport = false;
+
+        this.turnReq = true;
+        this.createReport = false;
+        this.rejectReq = false;
+        break;
+      case 'similar-good-notify-transferor':
+        this.regDocForm = false;
+        this.regDocView = true;
+        this.searchRequestSimGoods = false;
+        this.selectGoods = false;
+        this.viewSelectedGoods = true;
+        this.guidelines = false;
+        this.docRequest = true;
+        this.expRequest = true;
+        this.saveRequest = true;
+        this.dictumValidate = false;
+        this.notifyReport = true;
 
         this.turnReq = true;
         this.createReport = false;
