8924dfc90f49682c1d21c5864f286faa9fce5c60
Jessica Orozco <jessicaorozco@gmail.com>
2023-07-24 10:22:40 -0400

    change capture -digital

--- src/app/pages/general-processes/indicators/capture-digitalization/capture-digitalization/capture-digitalization-columns.ts
+++ src/app/pages/general-processes/indicators/capture-digitalization/capture-digitalization/capture-digitalization-columns.ts
@@ -44,7 +44,7 @@ export const GENERAL_PROCESSES_CAPTURE_DIGITALIZATION_COLUNNS = {
     renderComponent: CheckboxElementComponent,
     onComponentInitFunction(instance: any) {
       instance.toggle.subscribe((data: any) => {
-        data.row.cumplio == 1;
+        data.row.cumplio = data.toggle ? '1' : '0';
       });
     },
   },
