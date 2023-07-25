2a8249a492fa91a8a2d632e663d684028256a12d
feliMC12 <felixmamanicala@gmail.com>
2023-07-25 00:42:59 -0500

    FCOMER076-Estatus Disponibles para Comercializar
    FCOMER076-Estatus Disponibles para Comercializar

--- src/app/pages/commercialization/catalogs/goods-available-sale/status/goods-available-sale-form/goods-available-sale-form.component.ts
+++ src/app/pages/commercialization/catalogs/goods-available-sale/status/goods-available-sale-form/goods-available-sale-form.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { ListParams } from 'src/app/common/repository/interfaces/list-params';
 import { ModelForm } from 'src/app/core/interfaces/model-form';
+import { IComerTpEvent } from 'src/app/core/models/ms-event/event-type.model';
+import { ComerTpEventosService } from 'src/app/core/services/ms-event/comer-tpeventos.service';
+import { StatusDispService } from 'src/app/core/services/ms-status-disp/status-disp.service';
 //import { IGoodsAvailableSale } from 'src/app/core/models/commercialization/goodsAvailableSale';
 //import { GoodsAvailableSaleService } from 'src/app/core/services/ms-goods-available-sale/ms-goods-available-sale.service';
 import { BasePage } from 'src/app/core/shared/base-page';
-import { NUMBERS_PATTERN, STRING_PATTERN } from 'src/app/core/shared/patterns';
+import { STRING_PATTERN } from 'src/app/core/shared/patterns';
+import { DefaultSelect } from 'src/app/shared/components/select/default-select';
 
 @Component({
   selector: 'goods-available-sale-form',
@@ -20,33 +25,54 @@ export class GoodsAvailableSaleFormComponent
   title: string = 'Estatus de bienes disponibles para comercializar';
   edit: boolean = false;
   goodsAvailable: any;
-  constructor(private modalRef: BsModalRef, private fb: FormBuilder) {
+  addressList = new DefaultSelect<any>();
+  typeEvents = new DefaultSelect<IComerTpEvent>();
+  constructor(
+    private modalRef: BsModalRef,
+    private fb: FormBuilder,
+    private comerTpEventosService: ComerTpEventosService,
+    private statusDispService: StatusDispService
+  ) {
     super();
   }
 
   ngOnInit(): void {
     this.prepareForm();
+    this.addressList = new DefaultSelect([
+      { value: 'M', description: 'Muebles' },
+      { value: 'I', description: 'Inmuebles' },
+      { value: 'R', description: 'Remesas' },
+      { value: 'D', description: 'Disponibles' },
+      { value: 'V', description: 'Validar SIRSAE' },
+    ]);
   }
-
   //private goodsAvailableSaleService: GoodsAvailableSaleService
   private prepareForm() {
     this.goodsAvailableSaleForm = this.fb.group({
-      id: [null],
-      goodStatus: [null, [Validators.pattern(STRING_PATTERN)]],
+      idStatus: [null, [Validators.required]],
+      idDirection: [null, [Validators.required]],
       description: [null, [Validators.pattern(STRING_PATTERN)]],
-      processStatus: [null, [Validators.pattern(STRING_PATTERN)]],
-      quantity: [null, [Validators.pattern(NUMBERS_PATTERN)]],
-      transferentDestiny: [null, [Validators.pattern(STRING_PATTERN)]],
+      idTypeEvent: [null, [Validators.required]],
     });
     if (this.goodsAvailable != null) {
       this.edit = true;
       this.goodsAvailableSaleForm.patchValue(this.goodsAvailable);
+      this.getTypeEvent(
+        new ListParams(),
+        this.goodsAvailableSaleForm.get('idTypeEvent').value
+      );
+      this.goodsAvailableSaleForm.controls['idStatus'].disable();
+      this.goodsAvailableSaleForm.controls['idDirection'].disable();
+      this.goodsAvailableSaleForm.controls['idTypeEvent'].disable();
     }
+    setTimeout(() => {
+      this.getTypeEvent(new ListParams());
+    }, 1000);
   }
   close() {
     this.modalRef.hide();
   }
-  /*
+
   confirm() {
     console.log('Confirmar');
     this.edit ? this.update() : this.create();
@@ -54,8 +80,8 @@ export class GoodsAvailableSaleFormComponent
 
   create() {
     this.loading = true;
-    this.goodsAvailableSaleService
-      .create(this.goodsAvailableSaleForm.value)
+    this.statusDispService
+      .create(this.goodsAvailableSaleForm.getRawValue())
       .subscribe({
         next: data => this.handleSuccess(),
         error: error => (this.loading = false),
@@ -64,17 +90,30 @@ export class GoodsAvailableSaleFormComponent
 
   update() {
     this.loading = true;
-    this.goodsAvailableSaleService
-      .update(this.goodsAvailableSaleForm.value)
+    this.statusDispService
+      .update(this.goodsAvailableSaleForm.getRawValue())
       .subscribe({
         next: data => this.handleSuccess(),
         error: error => (this.loading = false),
       });
-  }*/
-
+  }
+  getTypeEvent(params: ListParams, id?: string) {
+    if (id) {
+      params['filter.id'] = `$eq:${id}`;
+    }
+    this.comerTpEventosService.getAllComerTpEvent(params).subscribe({
+      next: data => {
+        console.log(data);
+        this.typeEvents = new DefaultSelect(data.data, data.count);
+      },
+      error: error => {
+        this.typeEvents = new DefaultSelect();
+      },
+    });
+  }
   handleSuccess() {
     const message: string = this.edit ? 'Actualizada' : 'Guardada';
-    this.onLoadToast('success', this.title, `${message} Correctamente`);
+    this.onLoadToast('success', `${message} Correctamente`);
     this.loading = false;
     this.modalRef.content.callback(true);
     this.modalRef.hide();
