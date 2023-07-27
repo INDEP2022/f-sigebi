bbf842890678796907df0fc3d4165ff1c87232f3
feliMC12 <felixmamanicala@gmail.com>
2023-07-27 17:03:31 -0400

    GeneracionRecibos.aspx
    GeneracionRecibos.aspx

--- src/app/pages/siab-web/sami/receipt-generation-sami/receipt-table-goods/columns.ts
+++ src/app/pages/siab-web/sami/receipt-generation-sami/receipt-table-goods/columns.ts
@@ -1,10 +1,10 @@
 export const COLUMNS = {
   id_bien: {
-    title: 'ID BIEN',
+    title: 'ID Bien',
     sort: false,
   },
   clave_unica: {
-    title: 'CLAVE ÚNICA',
+    title: 'Clave Única',
     sort: false,
   },
   no_expediente: {
@@ -16,51 +16,51 @@ export const COLUMNS = {
     sort: false,
   },
   cantidad: {
-    title: 'CANTIDAD',
+    title: 'Cantidad',
     sort: false,
   },
   cantidad_sae: {
-    title: 'CANTIDAD_SAE',
+    title: 'Cantidad SAE',
     sort: false,
   },
   unidad_medida_letra: {
-    title: 'UNIDAD_MEDIDA_LETRA',
+    title: 'Unidad Medida Letra',
     sort: false,
   },
   unidad_medida_sae_letra: {
-    title: 'UNIDAD_MEDIDA_SAE_LETRA',
+    title: 'Unidad Medida SAE Letra',
     sort: false,
   },
   estado_fisico_letra: {
-    title: 'ESTADO_FISICO_LETRA',
+    title: 'Estado Fisico Letra',
     sort: false,
   },
   estado_fisico_sae_letra: {
-    title: 'ESTADO_FISICO_SAE_LETRA',
+    title: 'Estado Fisico SAE Letra',
     sort: false,
   },
   estado_conservacion_letra: {
-    title: 'ESTADO_CONSERVACION_LETRA',
+    title: 'Estado Conservación Letra',
     sort: false,
   },
   estado_conservacion_sae_letra: {
-    title: 'ESTADO_CONSERVACION_SAE_LETRA',
+    title: 'Estado Conservación SAE Letra',
     sort: false,
   },
   destino_letra: {
-    title: 'DESTINO_LETRA',
+    title: 'Destino Letra',
     sort: false,
   },
   destino_transferente_letra: {
-    title: 'DESTINO_TRANSFERENTE_LETRA',
+    title: 'Destino Transferente Letra',
     sort: false,
   },
   destino_sae_letra: {
-    title: 'DESTINO_SAE_LETRA',
+    title: 'Destino SAE Letra',
     sort: false,
   },
   id_recorrido: {
-    title: 'ID_RECORRIDO',
+    title: 'ID Recorrido',
     sort: false,
   },
 };
--- src/app/pages/siab-web/sami/receipt-generation-sami/receipt-table-programings/receipt-table-programings.component.ts
+++ src/app/pages/siab-web/sami/receipt-generation-sami/receipt-table-programings/receipt-table-programings.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
-import { takeUntil } from 'rxjs';
+import { LocalDataSource } from 'ng2-smart-table';
+import { BehaviorSubject, takeUntil } from 'rxjs';
+import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import { ReceptionGoodService } from 'src/app/core/services/reception/reception-good.service';
 import { ReceptionTicketsService } from 'src/app/core/services/reception/reception-tickets.service';
 import { BasePageWidhtDinamicFiltersExtra } from 'src/app/core/shared/base-page-dinamic-filters-extra';
 import { EReceiptType } from '../models/eReceiptType';
 import { ReceiptGenerationDataService } from '../services/receipt-generation-data.service';
-import { COLUMNS } from './columns';
+import { COLUMNS, COLUMNS1 } from './columns';
 
 @Component({
   selector: 'app-receipt-table-programings',
@@ -15,12 +18,16 @@ export class ReceiptTableProgramingsComponent extends BasePageWidhtDinamicFilter
   @Input() id_programacion: string;
   @Input() folio: string;
   @Input() type: string;
-
+  settings2: any;
+  params1 = new BehaviorSubject<ListParams>(new ListParams());
+  totalItems1: number = 0;
+  data1: LocalDataSource = new LocalDataSource();
   // pageSizeOptions = [5, 10, 20, 25];
   // limit: FormControl = new FormControl(5);
   constructor(
     private dataService: ReceiptGenerationDataService,
-    private receptionTicketsService: ReceptionTicketsService
+    private receptionTicketsService: ReceptionTicketsService,
+    private receptionGoodService: ReceptionGoodService
   ) {
     super();
     this.service = this.receptionTicketsService;
@@ -35,6 +42,14 @@ export class ReceiptTableProgramingsComponent extends BasePageWidhtDinamicFilter
       // rowClassFunction: (row: any) => {
       //   return row.data.notSelect ? 'notSelect' : '';
       // },
+    };
+    this.settings2 = {
+      ...this.settings,
+      hideSubHeader: false,
+      actions: false,
+      columns: {
+        ...COLUMNS1,
+      },
     };
     this.dataService.refreshTableProgrammings
       .pipe(takeUntil(this.$unSubscribe))
@@ -83,5 +98,30 @@ export class ReceiptTableProgramingsComponent extends BasePageWidhtDinamicFilter
       ...this.params.getValue(),
       ...newColumnFilters,
     };
+  }
+  selectGoodstickets(data: any) {
+    console.log(data);
+    let datos = {
+      programmingId: data.data.id_programacion,
+      actId: data.data.id_acta,
+      ticketId: data.data.id_recibo,
+      typeTicket: data.data.tipo_recibo,
+    };
+    console.log(datos);
+    this.receptionGoodService.createQueryGoodsTickets(datos).subscribe({
+      next: resp => {
+        console.log(resp);
+        this.data1.load(resp.data);
+        this.data1.refresh();
+        this.totalItems1 = resp.count;
+      },
+      error: eror => {
+        this.alert(
+          'warning',
+          'Generación de Recibos',
+          'No se encontraron Bienes'
+        );
+      },
+    });
   }
 }
