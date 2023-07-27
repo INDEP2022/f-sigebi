d2a801440d50f3db54cee40ea614d35f86187d51
MarioReyes <cmario.reyesp@gmail.com>
2023-07-27 10:07:21 -0500

    Se crea componente capture-lines del modulo de Catálogos de Comercialización

---
+++ src/app/core/services/ms-capture-line/captureline.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CapturelineEndpoints } from 'src/app/common/constants/endpoints/ms-captureline';
+import { HttpService } from 'src/app/common/services/http.service';
+import {
+  ICaptureLinesMain,
+  IDetCapturelines,
+} from 'src/app/core/models/catalogs/capture-lines-main.model';
+import { IListResponse } from '../../interfaces/list-response.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CapturelineService extends HttpService {
+  private readonly route: string = CapturelineEndpoints.ComerCaptureLines;
+  private readonly route2: string = CapturelineEndpoints.ComerDetCaptureLines;
+  constructor() {
+    super();
+    this.microservice = CapturelineEndpoints.BasePath;
+  }
+
+  getAll(params?: string): Observable<IListResponse<ICaptureLinesMain>> {
+    return this.get<IListResponse<ICaptureLinesMain>>(this.route, params);
+  }
+
+  getAllDetCaptureLines(
+    captureLinesId: number | string,
+    params?: string
+  ): Observable<IListResponse<IDetCapturelines>> {
+    return this.get<IListResponse<IDetCapturelines>>(
+      `${this.route2}?filter.eventId=$eq:${captureLinesId}`,
+      params
+    );
+  }
+}
--- src/app/pages/commercialization/catalogs/capture-lines/capture-lines-main/capture-lines-columns.ts
+++ src/app/pages/commercialization/catalogs/capture-lines/capture-lines-main/capture-lines-columns.ts
@@ -1,34 +1,48 @@
+import { CustomDateDayFilterComponent } from 'src/app/@standalone/shared-forms/filter-date-mounth-custom/custom-date-day-filter';
+
 export const EVENT_COLUMNS = {
-  event: {
+  eventId: {
     title: 'Evento',
     type: 'number',
     sort: false,
   },
-  cve: {
-    title: 'Cve.',
-    type: 'string',
-    sort: false,
-  },
-  bmxClient: {
+  // cve: {//SALE DE LA TABLA COMER_EVENTOS
+  //   title: 'Clave',
+  //   type: 'string',
+  //   sort: false,
+  // },
+  customerBmx: {
     title: 'Cliente Bmx',
     type: 'number',
     sort: false,
   },
-  userCreate: {
-    title: 'Creo',
+  userCreated: {
+    title: 'Usuario Creo',
     type: 'string',
     sort: false,
   },
-  date: {
+  creationDate: {
     title: 'Fecha',
     type: 'string',
     sort: false,
+    valuePrepareFunction: (cell: any, row: any) => {
+      const parts = cell.split('-');
+      const year = parts[0];
+      const month = parts[1];
+      const day = parts[2];
+      const formattedDate = `${day}/${month}/${year}`;
+      return formattedDate;
+    },
+    filter: {
+      type: 'custom',
+      component: CustomDateDayFilterComponent,
+    },
   },
 };
 
 export const CAPTURE_LINES_COLUMNS = {
-  palette: {
-    title: 'N° Paleta',
+  pallette: {
+    title: 'No. Paleta',
     type: 'number',
     sort: false,
   },
