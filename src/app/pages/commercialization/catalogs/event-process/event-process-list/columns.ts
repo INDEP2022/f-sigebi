86060d97353f421a069ea79cb9bbad7e8a9a1a61
guiodvpr <guiodvpr@gmail.com>
2023-07-31 18:37:07 -0500

    pantalla FCOMEREVENTPOPROCESO arreglo filtros y editar

--- src/app/pages/commercialization/catalogs/event-process/event-process-list/columns.ts
+++ src/app/pages/commercialization/catalogs/event-process/event-process-list/columns.ts
@@ -1,30 +1,50 @@
+import { CustomDateFilterComponent } from 'src/app/@standalone/shared-forms/filter-date-custom/custom-date-filter';
+
 export const COLUMNS = {
   id: {
     title: 'Id Evento',
     sort: false,
     valuePrepareFunction: (cell: any, row: any) => {
-      return row.id.id;
+      return row.comerDetail.id;
+    },
+    filterFunction(cell?: any, search?: string): boolean {
+      console.log('c ' + cell);
+      console.log('s ' + search);
+      const cellValue = cell.toString().toLowerCase();
+      const searchValue = search.toLowerCase();
+      return cellValue.includes(searchValue);
     },
   },
   processKey: {
     title: 'Cve Proceso',
     sort: false,
     valuePrepareFunction: (cell: any, row: any) => {
-      return row.id.processKey;
+      return row.comerDetail.processKey != null
+        ? row.comerDetail.processKey
+        : '';
+    },
+    filterFunction(cell?: any, search?: string): boolean {
+      return true;
     },
   },
   tpeventoId: {
     title: 'Tipo',
     sort: false,
     valuePrepareFunction: (cell: any, row: any) => {
-      return row.id.tpeventoId;
+      return row.comerDetail.tpeventoId;
+    },
+    filterFunction(cell?: any, search?: string): boolean {
+      return true;
     },
   },
-  StatusvtaId: {
+  statusvtaId: {
     title: 'Estatus',
     sort: false,
     valuePrepareFunction: (cell: any, row: any) => {
-      return row.id.statusvtaId;
+      return row.comerDetail.statusvtaId;
+    },
+    filterFunction(cell?: any, search?: string): boolean {
+      return true;
     },
   },
   phase: {
@@ -38,5 +58,28 @@ export const COLUMNS = {
   warrantyDate: {
     title: 'Fecha Garantía',
     sort: false,
+    valuePrepareFunction: (text: string) => {
+      return `${text ? text.split('-').reverse().join('/') : ''}`;
+    },
+    filter: {
+      type: 'custom',
+      component: CustomDateFilterComponent,
+    },
+  },
+  topost: {
+    title: 'Publicar',
+    sort: false,
+    type: 'html',
+    valuePrepareFunction: (text: string) => {
+      return text ? (text == '1' ? '☑️' : '🔲') : '';
+    },
+    filter: {
+      type: 'checkbox',
+      config: {
+        true: '1',
+        false: '0',
+        resetText: 'clear',
+      },
+    },
   },
 };
