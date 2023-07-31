2834dda6bba2258fbb23a88d3c0798d8bca66aad
Carlos Mario Reyes <104024599+CarlosMario-Pro@users.noreply.github.com>
2023-07-31 10:24:51 -0500

    bank-movements-types (#3313)

--- src/app/core/services/ms-bank-movement/bank-movement.service.ts
+++ src/app/core/services/ms-bank-movement/bank-movement.service.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
 import { HttpService } from 'src/app/common/services/http.service';
 import { IListResponse } from 'src/app/core/interfaces/list-response.interface';
+import { IBankMovementsTypes } from 'src/app/core/models/catalogs/bank-movements-types.models';
 import { ParameterComerEndpoints } from '../../../common/constants/endpoints/ms-parametercomer-endpoints';
 
 @Injectable({
@@ -24,6 +25,16 @@ export class BankMovementType extends HttpService {
     );
   }
 
+  getAllFilter(
+    bankCode: string | number,
+    params?: ListParams
+  ): Observable<IListResponse<any>> {
+    return this.get<IListResponse<any>>(
+      `${ParameterComerEndpoints.BankMovements}?filter.bankKey=$eq:${bankCode}`,
+      params
+    );
+  }
+
   getAllWithFilters(params?: string): Observable<IListResponse<any>> {
     return this.get<IListResponse<any>>(
       ParameterComerEndpoints.BankMovements,
@@ -31,13 +42,13 @@ export class BankMovementType extends HttpService {
     );
   }
 
-  create(client: any) {
-    return this.post(ParameterComerEndpoints.BankMovements, client);
+  create(model: IBankMovementsTypes) {
+    return this.post(ParameterComerEndpoints.BankMovements, model);
   }
 
-  update(id: string | number, client: any) {
+  update(id: string | number, model: IBankMovementsTypes) {
     const route = `${ParameterComerEndpoints.BankMovements}/${id}`;
-    return this.put(route, client);
+    return this.put(route, model);
   }
 
   remove(id: string | number) {
