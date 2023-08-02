31a6a27b7d83f94146ddc67693fc32b5dd4f7491
WilmerJSB04 <131001742+WilmerJSB04@users.noreply.github.com>
2023-08-01 21:05:36 -0400

    Wilmer destinos (#3370)
    * Correcciones Pantallas Destino 01-08-2023
    
    * Correcciones Pantallas Destino 01-08-2023 2
    
    * Correcciones Pantallas Destino 01-08-2023 3

--- src/app/pages/commercialization/shared-marketing-components/referenced-payment/referenced-payment.module.ts
+++ src/app/pages/commercialization/shared-marketing-components/referenced-payment/referenced-payment.module.ts
@@ -9,6 +9,7 @@ import { ReferencedPaymentRoutingModule } from './referenced-payment-routing.mod
 //Components
 import { ReferencedPaymentComponent } from './referenced-payment/referenced-payment.component';
 //@Standalone Components
+import { FormLoaderComponent } from 'src/app/@standalone/form-loader/form-loader.component';
 import { BanksSharedComponent } from 'src/app/@standalone/shared-forms/banks-shared/banks-shared.component';
 import { EventsSharedComponent } from 'src/app/@standalone/shared-forms/events-shared/events-shared.component';
 
@@ -22,6 +23,7 @@ import { EventsSharedComponent } from 'src/app/@standalone/shared-forms/events-s
     ReferencedPaymentRoutingModule,
     EventsSharedComponent,
     BanksSharedComponent,
+    FormLoaderComponent,
   ],
 })
 export class ReferencedPaymentModule {}
