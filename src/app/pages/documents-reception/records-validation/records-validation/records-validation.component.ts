2facd2f0d692750437615906d45c5b44fe36e3d7
SergioRTurizoM <tsergior@gmail.com>
2023-08-04 11:38:49 -0500

    Pendiente conteo correctos api

--- src/app/pages/documents-reception/records-validation/records-validation/records-validation.component.ts
+++ src/app/pages/documents-reception/records-validation/records-validation/records-validation.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { LocalDataSource } from 'ng2-smart-table';
 import { BehaviorSubject, takeUntil } from 'rxjs';
-import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import {
+  FilterParams,
+  ListParams,
+  SearchFilter,
+} from 'src/app/common/repository/interfaces/list-params';
 import { BasePage } from 'src/app/core/shared/base-page';
 import { KEYGENERATION_PATTERN } from 'src/app/core/shared/patterns';
 import { ProceedingsValidationsService } from './../../../../core/services/ms-proceedings/proceedings-validations.service';
@@ -18,11 +23,13 @@ export class RecordsValidationComponent extends BasePage implements OnInit {
   fileNumber: number;
   proceedingsNumb: number;
   proceedingsCve: string;
-  dataTable: any[] = [];
+  dataTable: LocalDataSource = new LocalDataSource();
   correctRecords: number = 0;
   recordsCount: number = 0;
   params = new BehaviorSubject<ListParams>(new ListParams());
   totalItems: number = 0;
+  columnFilters: any = [];
+  filterParams = new BehaviorSubject<FilterParams>(new FilterParams());
 
   constructor(
     private fb: FormBuilder,
@@ -46,12 +53,53 @@ export class RecordsValidationComponent extends BasePage implements OnInit {
   }
 
   ngOnInit(): void {
+    const exist = this.filterParams.getValue().getFilterParams();
+    if (exist) {
+      const filters = exist.split('&');
+      filters.map(fil => {
+        const partsFilter = fil.split('=');
+        this.columnFilters[partsFilter[0]] = partsFilter[1];
+      });
+    }
+
+    this.dataTable
+      .onChanged()
+      .pipe(takeUntil(this.$unSubscribe))
+      .subscribe(change => {
+        if (change.action === 'filter') {
+          let filters = change.filter.filters;
+          filters.map((filter: any) => {
+            let field = '';
+            let searchFilter = SearchFilter.ILIKE;
+            field = `filter.${filter.field}`;
+
+            switch (filter.field) {
+              case 'valSec':
+                searchFilter = SearchFilter.EQ;
+                break;
+              default:
+                searchFilter = SearchFilter.ILIKE;
+                break;
+            }
+
+            if (filter.search !== '') {
+              this.columnFilters[field] = `${searchFilter}:${filter.search}`;
+            } else {
+              delete this.columnFilters[field];
+            }
+          });
+          this.params = this.pageFilter(this.params);
+          console.log(this.params);
+          this.getInfo();
+        }
+      });
+
     this.prepareForm();
     this.getParams();
     this.setForm();
-    this.getInfo(this.params.value);
+
     this.params.pipe(takeUntil(this.$unSubscribe)).subscribe(() => {
-      this.getInfo(this.params.value);
+      this.getInfo();
     });
   }
 
@@ -71,33 +119,37 @@ export class RecordsValidationComponent extends BasePage implements OnInit {
     });
   }
 
-  getInfo(params: ListParams) {
+  getInfo() {
+    this.loading = true;
     let data: any[] = [];
-    let temp: any = {};
-    console.log(this.proceedingsNumb);
-    const paramameters = {
-      ...params,
-      'filter.numProceedings': this.proceedingsNumb,
-    };
-    this.proceedingsValidationsService.getAll(paramameters).subscribe({
-      next: resp => {
-        // for (let validation of resp.data) {
-        //   (temp.secVal = validation.secVal),
-        //     (temp.descVal = validation.proceedingsType.descVal),
-        //     (temp.resultValue = validation.resultValue),
-        //     (temp.statusValue = validation.statusValue);
-        // if (validation.statusValue == 1) this.correctRecords++;
-        // this.recordsCount++;
-        //   data.push(temp);
-        // }
 
-        this.dataTable = resp.data.map(item => ({
-          ...item,
-          descVal: item.proceedingsType.descVal,
-          // statusValue: item.statusValue === '1' ? 'Si' : 'No',
-        }));
+    console.log(this.proceedingsNumb);
+    const params = {
+      ...this.params.getValue(),
+      'filter.numProceedings': this.proceedingsNumb,
+      ...this.columnFilters,
+    };
+    this.proceedingsValidationsService.getAll(params).subscribe({
+      next: resp => {
+        console.log(resp);
+        for (let validation of resp.data) {
+          let temp: any = {};
+          (temp.valSec = validation.valSec),
+            (temp.valDescription = validation.valDescription),
+            (temp.valResult = validation.valResult),
+            (temp.statusValue = validation.valStatus);
+          data.push(temp);
+        }
+
+        this.dataTable.load(data);
+        const correctos = resp.data.filter((item: any) => {
+          return item.valStatus === '1';
+        });
+        this.correctRecords = correctos.length;
+        this.recordsCount = resp.count;
 
         this.totalItems = resp.count;
+        this.loading = false;
       },
       error: err => {
         if (err.status <= 404) {
@@ -106,32 +158,10 @@ export class RecordsValidationComponent extends BasePage implements OnInit {
             'Información',
             'No existen validadores para esta acta'
           );
+          this.loading = false;
         }
       },
     });
-    const paramameters2 = {
-      'filter.numProceedings': this.proceedingsNumb,
-    };
-    this.proceedingsValidationsService
-      .getTotalRegisters(paramameters2)
-      .subscribe({
-        next: resp => {
-          const correctos = resp.data.filter((item: any) => {
-            return item.statusValue === '1';
-          });
-          this.correctRecords = correctos.length;
-          this.recordsCount = resp.count;
-        },
-        error: err => {
-          if (err.status <= 404) {
-            this.onLoadToast(
-              'info',
-              'Información',
-              'No existen validadores para esta acta'
-            );
-          }
-        },
-      });
   }
 
   prepareForm() {
