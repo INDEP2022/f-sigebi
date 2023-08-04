1743290a969d99c885f2be045058a178eeb29dd3
gus2612 <gustavoangelsantosclemente@gmail.com>
2023-08-04 01:34:59 -0600

    timeout email

--- src/app/pages/request/programming-request-components/formalize-programming/formalize-programming-form/formalize-programming-form.component.ts
+++ src/app/pages/request/programming-request-components/formalize-programming/formalize-programming-form/formalize-programming-form.component.ts
@@ -1202,7 +1202,11 @@ export class FormalizeProgrammingFormComponent
           this.getProccedings();
           this.proceeding.clear();
           this.totalItemsProceedings = 0;
-          this.sendEmail();
+
+          setTimeout(() => {
+            this.sendEmail();
+            this.formLoading = false;
+          }, 5000);
         }
       },
     };
@@ -1216,29 +1220,31 @@ export class FormalizeProgrammingFormComponent
     params.getValue()['filter.statusProceeedings'] = 'CERRADO';
     this.proceedingService.getProceedings(params.getValue()).subscribe({
       next: async response => {
-        if (response.data.length > 0) {
-          const data = {
-            //recipients: `gustavoangelsantosclemente@gmail.com, al221810743@gmail.com`,
-            recipients: `${response.data[0].emailOic}, ${response.data[0].emailWorker1}, ${response.data[0].emailWorker2}, ${response.data[0].emailWitness1}, ${response.data[0].emailWitness2}, al221810743@gmail.com`,
-            message: `Le informamos que el acta con folio: ${response.data[0].folioProceedings}, terminó satisfactoriamente.`,
-            userCreation: 'dr_sigebi',
-            dateCreation: '2023-07-31',
-            userModification: 'dr_sigebi',
-            dateModification: '2023-07-31',
-            version: '2',
-            subject: 'Notificación de cierre de acta de entrega recepción',
-            nameAtt: response.data[0].folioProceedings,
-            typeAtt: 'application/pdf;',
-            //"urlAtt": "https://seguimiento.agoraparticipa.org/docs/PDF_TEXT-CA4Bn.pdf", //si cuentas con una url usas esto en ves del base64
-            process: 'FORMALIZAR',
-            wcontent: response.data[0].id_content,
-          };
-          this.emailService.createEmailNotify(data).subscribe({
-            next: response => {
-              console.log('correo enviado', response);
-            },
-          });
-        }
+        setTimeout(() => {
+          if (response.data.length > 0) {
+            const data = {
+              //recipients: `gustavoangelsantosclemente@gmail.com, al221810743@gmail.com`,
+              recipients: `${response.data[0].emailOic}, ${response.data[0].emailWorker1}, ${response.data[0].emailWorker2}, ${response.data[0].emailWitness1}, ${response.data[0].emailWitness2}, al221810743@gmail.com`,
+              message: `Le informamos que el acta con folio: ${response.data[0].folioProceedings}, terminó satisfactoriamente.`,
+              userCreation: 'dr_sigebi',
+              dateCreation: '2023-07-31',
+              userModification: 'dr_sigebi',
+              dateModification: '2023-07-31',
+              version: '2',
+              subject: 'Notificación de cierre de acta de entrega recepción',
+              nameAtt: response.data[0].folioProceedings,
+              typeAtt: 'application/pdf;',
+              //"urlAtt": "https://seguimiento.agoraparticipa.org/docs/PDF_TEXT-CA4Bn.pdf", //si cuentas con una url usas esto en ves del base64
+              process: 'FORMALIZAR',
+              wcontent: response.data[0].id_content,
+            };
+            this.emailService.createEmailNotify(data).subscribe({
+              next: response => {
+                console.log('correo enviado', response);
+              },
+            });
+          }
+        }, 3000);
       },
       error: error => {},
     });
