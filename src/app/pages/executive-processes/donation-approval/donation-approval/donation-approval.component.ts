eb33a5c4b4d29d920707b9afca9f6548709b04e8
aalvarezg1998 <107310910+aalvarezg1998@users.noreply.github.com>
2023-08-05 01:53:22 -0500

    Se termina la forma - Alexander (#3457)
    * remittance-exportation
    
    * Se termina la forma
    
    ---------
    
    Co-authored-by: DESKTOP-MCR4TPC\ADMIN <luisgoemon@gmail.com>

--- src/app/pages/executive-processes/donation-approval/donation-approval/donation-approval.component.ts
+++ src/app/pages/executive-processes/donation-approval/donation-approval/donation-approval.component.ts
@@ -14,10 +14,13 @@ import { DONATION_APPROVAL_COLUMNS } from './donation-approval-columns';
 import { IGood } from 'src/app/core/models/ms-good/good';
 //Services
 import { LocalDataSource } from 'ng2-smart-table';
+import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
 import { IExpedient } from 'src/app/core/models/ms-expedient/expedient';
 import { ExpedientService } from 'src/app/core/services/ms-expedient/expedient.service';
+import { GoodProcessService } from 'src/app/core/services/ms-good/good-process.service';
 import { GoodService } from 'src/app/core/services/ms-good/good.service';
 import { DefaultSelect } from 'src/app/shared/components/select/default-select';
+import { UpdateModalComponent } from '../update-modal/update-modal.component';
 
 @Component({
   selector: 'app-donation-approval',
@@ -36,21 +39,34 @@ export class DonationApprovalComponent extends BasePage implements OnInit {
 
   rowSelected: boolean = false;
   selectedRow: any = null;
+  title: string = 'Aprobación de Bienes para Donación';
+  columnFilters: any = [];
+  fileNumber: number;
+  consult: boolean = false;
+  /// http://sigebimstest.indep.gob.mx/goodprocess/api/v1/update-good-status/getOneStatus
 
   constructor(
     private fb: FormBuilder,
     private goodService: GoodService,
     private expedientService: ExpedientService,
-    private datePipe: DatePipe
+    private datePipe: DatePipe,
+    private modalService: BsModalService,
+    private goodprocessService: GoodProcessService
   ) {
     super();
     this.settings = {
       ...this.settings,
-      actions: false,
+      hideSubHeader: false,
+      actions: {
+        columnTitle: 'Acciones',
+        edit: true,
+        delete: false,
+        add: false,
+        position: 'right',
+      },
       columns: { ...DONATION_APPROVAL_COLUMNS },
-      mode: '',
       rowClassFunction: (row: any) => {
-        if (row.data.estatus.active === '1') {
+        if (row.data.di_disponible === 'S') {
           return 'text-success';
         } else {
           return 'text-danger';
@@ -61,7 +77,42 @@ export class DonationApprovalComponent extends BasePage implements OnInit {
 
   ngOnInit(): void {
     this.prepareForm();
-    // this.getPagination();
+    this.data
+      .onChanged()
+      .pipe(takeUntil(this.$unSubscribe))
+      .subscribe(change => {
+        if (change.action === 'filter') {
+          let filters = change.filter.filters;
+          filters.map((filter: any) => {
+            console.log(filter);
+            let field = ``;
+            let searchFilter = SearchFilter.ILIKE;
+            field = `filter.${filter.field}`;
+            /*SPECIFIC CASES*/
+            switch (filter.field) {
+              case 'id':
+                searchFilter = SearchFilter.EQ;
+                break;
+              default:
+                searchFilter = SearchFilter.ILIKE;
+                break;
+            }
+
+            if (filter.search !== '') {
+              console.log(
+                (this.columnFilters[field] = `${searchFilter}:${filter.search}`)
+              );
+              this.columnFilters[field] = `${searchFilter}:${filter.search}`;
+            } else {
+              delete this.columnFilters[field];
+            }
+          });
+          if (this.consult) this.getGoods(this.fileNumber);
+        }
+      });
+    this.params.pipe(takeUntil(this.$unSubscribe)).subscribe(() => {
+      if (this.consult) this.getGoods(this.fileNumber);
+    });
   }
 
   private prepareForm() {
@@ -116,23 +167,23 @@ export class DonationApprovalComponent extends BasePage implements OnInit {
 
   getExpedientById(): void {
     let _id = this.form.controls['idExpedient'].value;
+    this.fileNumber = _id;
+    this.consult = true;
     this.loading = true;
-    this.expedientService.getById(_id).subscribe(
-      response => {
-        //TODO: Validate Response
+    this.expedientService.getById(_id).subscribe({
+      next: response => {
         if (response !== null) {
           this.form.patchValue(response);
           this.form.updateValueAndValidity();
-          this.getGoodsByExpedient(response.id);
+          this.getGoods(response.id);
         } else {
           //TODO: CHECK MESSAGE
-          this.alert('info', 'No se encontraron registros', '');
+          this.alert('warning', this.title, 'No se encontraron registros');
         }
-
         this.loading = false;
       },
-      error => (this.loading = false)
-    );
+      error: error => (this.loading = false),
+    });
   }
 
   getGoodsByExpedient(id: string | number): void {
@@ -140,42 +191,108 @@ export class DonationApprovalComponent extends BasePage implements OnInit {
       .pipe(takeUntil(this.$unSubscribe))
       .subscribe(() => this.getGoods(id));
   }
+
   getGoods(id: string | number): void {
-    this.goodService.getByExpedient(id, this.params.getValue()).subscribe(
-      response => {
-        //console.log(response);
-        let data = response.data.map((item: IGood) => {
-          //console.log(item);
-          item.promoter = item.userPromoterDecoDevo?.id;
-          let dateDecoDev = item.scheduledDateDecoDev;
-          let dateTeso = item.tesofeDate;
-          item.scheduledDateDecoDev = this.datePipe.transform(
-            dateDecoDev,
-            'yyyy-MM-dd'
-          );
-          item.tesofeDate = this.datePipe.transform(dateTeso, 'yyyy-MM-dd');
-          return item;
-        });
+    this.loading = true;
+    let params = {
+      ...this.params.getValue(),
+      ...this.columnFilters,
+    };
+    params['filter.fileNumber'] = `$eq:${id}`;
+    this.goodService.getAll(params).subscribe({
+      next: async response => {
+        let data = await Promise.all(
+          response.data.map(async (item: any) => {
+            console.log(item.ubicationType);
+            switch (item.ubicationType) {
+              case 'A':
+                item.di_cve_ubicacion = 'ALMACEN';
+                item.di_ubicacion1 = `${
+                  item.almacen ? item.almacen.ubication : ''
+                } LOTE ${item.lotNumber ? item.lotNumber.id : ''} RACK ${
+                  item.rackNumber
+                }`;
+                break;
+              case 'B':
+                item.di_cve_ubicacion = 'BOVEDA';
+                item.di_ubicacion1 = `${
+                  item.boveda ? item.boveda.ubication : ''
+                } GAVETA ${item.lotNumber ? item.lotNumber.id : ''}`;
+                break;
+              case 'D':
+                item.di_cve_ubicacion = 'DEPOSITARÍA';
+                break;
+              default:
+                item.di_ubicacion1 = 'UBICACIÓN DESCONOCIDA';
+                break;
+            }
+            const di_disponible = await this.getDisponible(item.id);
+            item.di_disponible = di_disponible;
+            return item;
+          })
+        );
+        console.log('Nueva data: ', data);
         this.data.load(data);
+        this.data.refresh();
         this.totalItems = response.count;
         this.loading = false;
       },
-      error => (this.loading = false)
-    );
-  }
-
-  settingsChange($event: any): void {
-    this.settings = $event;
-  }
-
-  selectRow(row: any) {
-    console.log(row);
-    this.selectedRow = row;
-    this.rowSelected = true;
+      error: error => (this.loading = false),
+    });
   }
 
   resetScreen() {
     window.scrollTo(0, 0);
     this.form.reset();
+  }
+
+  clean() {
+    this.consult = false;
+    window.scrollTo(0, 0);
+    this.form.reset();
+  }
+
+  getDisponible(goodNumber: number) {
+    return new Promise<string>((res, _rej) => {
+      const model = {
+        goodNumber,
+        vcScreen: 'FACTDESAPROBDONAC',
+      };
+      console.log(model);
+      this.goodprocessService.getDisponible2(model).subscribe({
+        next: resp => {
+          res('S');
+        },
+        error: err => {
+          res('N');
+        },
+      });
+    });
+  }
+
+  edit(good: any) {
+    if (good.di_disponible === 'S') {
+      this.openModal(good);
+    } else {
+      this.alert('warning', this.title, 'Este Bien no Está Disponible');
+    }
+  }
+
+  openModal(good?: IGood) {
+    let config: ModalOptions = {
+      initialState: {
+        good,
+        callback: (next: boolean) => {
+          if (next) {
+            this.params
+              .pipe(takeUntil(this.$unSubscribe))
+              .subscribe(() => this.getGoods(this.fileNumber));
+          }
+        },
+      },
+      class: 'modal-lg modal-dialog-centered',
+      ignoreBackdropClick: true,
+    };
+    this.modalService.show(UpdateModalComponent, config);
   }
 }
