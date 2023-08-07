2a18b6e8bf865d6bc28874128d5d8a146a5c1aba
Jessica Orozco <jessicaorozco@gmail.com>
2023-08-07 12:09:16 -0400

    bugs

--- src/app/pages/general-processes/valid-statuses/valid-statuses/valid-statuses.component.ts
+++ src/app/pages/general-processes/valid-statuses/valid-statuses/valid-statuses.component.ts
@@ -1,138 +1,10 @@
-// import { Component, OnInit } from '@angular/core';
-// import { FormControl } from '@angular/forms';
-// import { ActivatedRoute } from '@angular/router';
-// import {
-//   BehaviorSubject,
-//   catchError,
-//   forkJoin,
-//   skip,
-//   switchMap,
-//   takeUntil,
-//   tap,
-//   throwError,
-// } from 'rxjs';
-// import { FilterParams } from 'src/app/common/repository/interfaces/list-params';
-// import { IStatusXScreen } from 'src/app/core/models/ms-screen-status/status.model';
-// import { ScreenHelpService } from 'src/app/core/services/ms-business-rule/screen-help.service';
-// import { ScreenStatusService } from 'src/app/core/services/ms-screen-status/screen-status.service';
-// import { SegAppScreenService } from 'src/app/core/services/ms-screen-status/seg-app-screen.service';
-// import { BasePage } from 'src/app/core/shared/base-page';
-// import { VALID_STATUSES_COLUMNS } from './valid-statuses-columns';
-
-// @Component({
-//   selector: 'app-valid-statuses',
-//   templateUrl: './valid-statuses.component.html',
-//   styles: [],
-// })
-// export class ValidStatusesComponent extends BasePage implements OnInit {
-//   private global = {
-//     // ? FACTREFACTAENTREC usar este parametro como ejemplo
-//     screenStatus: '',
-//   };
-//   helpText = 'No hay ayuda para esta pantalla';
-//   screenCtrl = new FormControl<string>({ value: null, disabled: true });
-//   helpCtrol = new FormControl<string>(null);
-//   params = new BehaviorSubject(new FilterParams());
-//   showStatus: boolean = true;
-//   statuses: IStatusXScreen[] = [];
-//   totalItems = 0;
-//   constructor(
-//     private segAppScreenService: SegAppScreenService,
-//     private activatedRoute: ActivatedRoute,
-//     private screenStatusService: ScreenStatusService,
-//     private screenHelpService: ScreenHelpService
-//   ) {
-//     super();
-//     this.settings.actions = false;
-//     this.settings.columns = VALID_STATUSES_COLUMNS;
-//     this.settings.hideSubHeader = false;
-//     this.activatedRoute.queryParams
-//       .pipe(takeUntil(this.$unSubscribe))
-//       .subscribe(params => {
-//         this.global.screenStatus = params['screen'];
-//       });
-//   }
-
-//   ngOnInit(): void {
-//     this.params
-//       .pipe(
-//         skip(1),
-//         switchMap(params =>
-//           forkJoin([this.getScreenStatuses(params), this.getHelpByScreen()])
-//         )
-//       )
-//       .subscribe();
-//     this.fillFromParams();
-//   }
-
-//   fillFromParams() {
-//     const { screenStatus } = this.global;
-//     if (screenStatus) {
-//       this.getScreenStatusById(screenStatus).subscribe();
-//       const params = this.params.getValue();
-//       params.removeAllFilters();
-//       params.addFilter('screenKey', screenStatus);
-//       this.params.next(params);
-//     }
-//   }
-
-//   getScreenStatusById(id: string) {
-//     this.hideError();
-//     return this.segAppScreenService.getById(id).pipe(
-//       catchError(error => {
-//         this.loading = false;
-//         if (error.status >= 500) {
-//           this.onLoadToast(
-//             'error',
-//             'Error',
-//             'Ocurrio un error al obtener el status'
-//           );
-//         }
-
-//         return throwError(() => error);
-//       }),
-//       tap(screenStatus => {
-//         this.loading = false;
-//         this.screenCtrl.setValue(screenStatus.description);
-//       })
-//     );
-//   }
-
-//   getScreenStatuses(params: FilterParams) {
-//     this.hideError();
-//     this.loading = true;
-//     return this.screenStatusService.getAllFiltered(params.getParams()).pipe(
-//       catchError(error => {
-//         this.loading = false;
-//         if (error.status >= 500) {
-//           this.onLoadToast(
-//             'error',
-//             'Error',
-//             'Ocurrio un error al obtener los estatus'
-//           );
-//         }
-
-//         return throwError(() => error);
-//       }),
-//       tap(response => {
-//         this.loading = false;
-//         this.statuses = response.data;
-//         this.totalItems = response.count;
-//         this.showStatus = this.statuses.length > 0;
-//       })
-//     );
-//   }
-
-//   getHelpByScreen() {
-//     this.hideError();
-//     return this.screenHelpService
-//       .getById(this.global.screenStatus)
-//       .pipe(tap(screenHelp => (this.helpText = screenHelp.businessRoleDesc)));
-//   }
-// }
-
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import {
   BehaviorSubject,
@@ -142,13 +14,18 @@ import {
   tap,
   throwError,
 } from 'rxjs';
-import { FilterParams } from 'src/app/common/repository/interfaces/list-params';
+import {
+  FilterParams,
+  ListParams,
+} from 'src/app/common/repository/interfaces/list-params';
+import { IScreenHelpTwo } from 'src/app/core/models/ms-business-rule/screen-help.model';
 import { IStatusXScreen } from 'src/app/core/models/ms-screen-status/status.model';
 import { ScreenHelpService } from 'src/app/core/services/ms-business-rule/screen-help.service';
 import { ScreenStatusService } from 'src/app/core/services/ms-screen-status/screen-status.service';
 import { SegAppScreenService } from 'src/app/core/services/ms-screen-status/seg-app-screen.service';
 import { BasePage } from 'src/app/core/shared/base-page';
-import { VALID_STATUSES_COLUMNS } from './valid-statuses-columns';
+import { STRING_PATTERN } from 'src/app/core/shared/patterns';
+import { HELP_COLUMNS, VALID_STATUSES_COLUMNS } from './valid-statuses-columns';
 
 @Component({
   selector: 'app-valid-statuses',
@@ -160,14 +37,22 @@ export class ValidStatusesComponent extends BasePage implements OnInit {
     // ? FACTREFACTAENTREC usar este parametro como ejemplo
     screenStatus: '',
   };
+  helpForm: FormGroup;
   helpText = 'No hay ayuda para esta pantalla';
   screenCtrl = new FormControl<string>({ value: null, disabled: true });
   helpCtrol = new FormControl<string>(null);
   params = new BehaviorSubject(new FilterParams());
   showStatus: boolean = true;
+  screenStatus: string = '';
+  help: IScreenHelpTwo[];
+  loading2: boolean = false;
   statuses: IStatusXScreen[] = [];
   totalItems = 0;
+  params2 = new BehaviorSubject(new ListParams());
+  totalItems2 = 0;
+
   constructor(
+    private fb: FormBuilder,
     private segAppScreenService: SegAppScreenService,
     private activatedRoute: ActivatedRoute,
     private screenStatusService: ScreenStatusService,
@@ -177,6 +62,10 @@ export class ValidStatusesComponent extends BasePage implements OnInit {
     this.settings.actions = false;
     this.settings.columns = VALID_STATUSES_COLUMNS;
     this.settings.hideSubHeader = false;
+    settings2 = { ...TABLE_SETTINGS };
+    this.settings2.actions = false;
+    this.settings2.columns = HELP_COLUMNS;
+    this.settings2.hideSubHeader = false;
     this.activatedRoute.queryParams
       .pipe(takeUntil(this.$unSubscribe))
       .subscribe(params => {
@@ -185,9 +74,10 @@ export class ValidStatusesComponent extends BasePage implements OnInit {
   }
 
   ngOnInit(): void {
+    this.prepareHelpForm();
     this.params.pipe(skip(1)).subscribe(params => {
       this.getScreenStatuses(params).subscribe();
-      this.getHelpByScreen().subscribe();
+      this.getHelpByScreen(this.global.screenStatus).subscribe();
     });
     this.fillFromParams();
   }
@@ -198,9 +88,19 @@ export class ValidStatusesComponent extends BasePage implements OnInit {
       this.getScreenStatusById(screenStatus).subscribe();
       const params = this.params.getValue();
       params.removeAllFilters();
+      this.screenStatus = screenStatus;
       params.addFilter('screenKey', screenStatus);
       this.params.next(params);
     }
+  }
+  prepareHelpForm() {
+    this.helpForm = this.fb.group({
+      screenKey: [
+        null,
+        { value: '', disabled: false },
+        [Validators.pattern(STRING_PATTERN), Validators.maxLength(11)],
+      ],
+    });
   }
 
   getScreenStatusById(id: string) {
@@ -243,7 +143,7 @@ export class ValidStatusesComponent extends BasePage implements OnInit {
       }),
       tap(response => {
         console.log(response);
-
+        this.getHelpByScreen(this.screenStatus);
         this.loading = false;
         this.statuses = response.data;
         this.totalItems = response.count;
@@ -252,10 +152,9 @@ export class ValidStatusesComponent extends BasePage implements OnInit {
     );
   }
 
-  getHelpByScreen() {
-    this.hideError();
+  getHelpByScreen(screen: string) {
     return this.screenHelpService
-      .getById(this.global.screenStatus)
-      .pipe(tap(screenHelp => (this.helpText = screenHelp.businessRoleDesc)));
+      .getHelpScreen(screen)
+      .pipe(tap(screenHelp => (this.help = screenHelp.data)));
   }
 }
