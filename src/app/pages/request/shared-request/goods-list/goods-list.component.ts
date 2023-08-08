1ceb24bb9a3043d1ed281ff4270ff491421aa303
Edson Percy Lopez Cortez <edson_lopez@hotmail.com.ar>
2023-08-08 13:48:15 -0400

    llamado a los servicios requeridos para las tablas (#3493)

--- src/app/pages/request/shared-request/goods-list/goods-list.component.ts
+++ src/app/pages/request/shared-request/goods-list/goods-list.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BehaviorSubject, takeUntil } from 'rxjs';
 import { TABLE_SETTINGS } from 'src/app/common/constants/table-settings';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import { TypeRelevantService } from 'src/app/core/services/catalogs/type-relevant.service';
+import { RejectedGoodService } from 'src/app/core/services/ms-rejected-good/rejected-good.service';
 import { BasePage } from 'src/app/core/shared/base-page';
-import { SELECT_GOODS_COLUMNS } from '../select-goods/select-goods-columns';
 import { ViewFileButtonComponent } from '../select-goods/view-file-button/view-file-button.component';
+import { SELECT_GOODS_LIST_COLUMNS } from './select-good-list-columns';
 
 @Component({
   selector: 'app-goods-list',
@@ -40,10 +42,12 @@ export class GoodsListComponent extends BasePage implements OnInit {
   ];
 
   /* INJECTIONS */
+  private rejectedGoodService = inject(RejectedGoodService);
+  private typeRelevantService = inject(TypeRelevantService);
 
   constructor(private modalService: BsModalService) {
     super();
-    this.selectedGoodSettings.columns = SELECT_GOODS_COLUMNS;
+    this.selectedGoodSettings.columns = SELECT_GOODS_LIST_COLUMNS;
   }
 
   ngOnInit(): void {
@@ -67,16 +71,46 @@ export class GoodsListComponent extends BasePage implements OnInit {
       .pipe(takeUntil(this.$unSubscribe))
       .subscribe(data => {
         if (this.requestId) {
-          this.getData();
+          this.getData(data);
         }
       });
   }
 
-  getData() {
+  getData(params: ListParams) {
     //const good: any = Object.assign({ viewFile: '' }, this.goodTestData[0]);
-    const good: any = Object.assign(this.goodTestData[0]);
+    /*const good: any = Object.assign(this.goodTestData[0]);
     this.selectedGoodColumns = [...this.selectedGoodColumns, good];
-    this.selectedGoodTotalItems = this.selectedGoodColumns.length;
+    this.selectedGoodTotalItems = this.selectedGoodColumns.length;*/
+    params['filter.applicationId'] = `$eq:${this.requestId}`;
+    this.rejectedGoodService.getAll(params).subscribe({
+      next: resp => {
+        const result = resp.data.map(async (item: any) => {
+          item['typeRelevantDescrip'] = await this.getTypeRelevant(
+            item.relevantTypeId
+          );
+        });
+
+        Promise.all(result).then(data => {
+          this.selectedGoodColumns = resp.data;
+          this.selectedGoodTotalItems = resp.count;
+        });
+      },
+    });
+  }
+
+  getTypeRelevant(id: number) {
+    return new Promise((resolve, reject) => {
+      const params = new ListParams();
+      params[`filter.id`] = `$eq:${id}`;
+      this.typeRelevantService.getAll(params).subscribe({
+        next: resp => {
+          resolve(resp.data[0].description);
+        },
+        error: error => {
+          resolve('');
+        },
+      });
+    });
   }
 
   viewFile(file: any) {
