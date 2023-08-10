ea61ecc9e8c3128f98fc2106ab37528fd76f8ebe
Sergio Raul Turizo Medina <96961270+SergioRTurizoM@users.noreply.github.com>
2023-08-10 17:42:34 -0500

    Solucion bugstracker (#3562)

--- src/app/core/services/catalogs/good-type.service.ts
+++ src/app/core/services/catalogs/good-type.service.ts
@@ -16,7 +16,7 @@ export class GoodTypeService implements ICrudMethods<IGoodType> {
   getAll(params?: ListParams): Observable<IListResponse<IGoodType>> {
     return this.goodTypeRepository.getAllPaginated(this.route, params);
   }
-  getAllS(params?: string): Observable<IListResponse<IGoodType>> {
+  getAllS(params?: any): Observable<IListResponse<IGoodType>> {
     return this.goodTypeRepository.getAllPaginated(this.route, params);
   }
 
