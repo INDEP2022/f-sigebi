839b4f25e61359617e85ed8fac55677075d76816
pablo vargas <pablo.vargas.js@gmail.com>
2023-08-09 22:00:05 -0300

    workflows

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,11 +1,11 @@
 (function (window) {
   window["env"] = window["env"] || {};
   window["env"].EXTERNAL_INTEGRATION_URL = 'http://www.indep.gob.mx/firmaelectronica'; // not actualized, for local testing
-  window["env"].API_BACKEND_URL = 'http://sigebimstest.indep.gob.mx/';//'http://20.119.126.102:3000/';//'http://localhost:3000/api/v1/'; // not actualized, for local testing
-  
-  window["env"].API_BACKEND_URL2 = 'http://sigebimstest.indep.gob.mx/';
-  window["env"].API_CONTENT = 'http://sigebimstest.indep.gob.mx/';
-  
+  window["env"].API_BACKEND_URL = 'http://sigebimsqa.indep.gob.mx/';//'http://20.119.126.102:3000/';//'http://localhost:3000/api/v1/'; // not actualized, for local testing
+
+  window["env"].API_BACKEND_URL2 = 'http://sigebimsqa.indep.gob.mx/';
+  window["env"].API_CONTENT = 'http://sigebimsqa.indep.gob.mx/';
+
   window["env"].URL_PREFIX = 'api/v1/';
 
   window["env"].API_REPORTS_BASE = 'http://reports-qa.indep.gob.mx/jasperserver/';
@@ -14,11 +14,11 @@
   window["env"].API_REPORTS_PSW = '87654321';
 
   window["env"].api_external_token = 'http://52.142.23.169/realms/indep/protocol/openid-connect/token';
-  window["env"].api_external_userInfo ='http://52.142.23.169/realms/indep/protocol/openid-connect/userinfo';
+  window["env"].api_external_userInfo = 'http://52.142.23.169/realms/indep/protocol/openid-connect/userinfo';
   window["env"].api_external_typeUser = 'http://168.61.50.112:3003/api/v1/user/userType/';
   window["env"].api_external_rolesUser = 'http://168.61.50.112:3003/api/v1/user/roles';
   window["env"].SOCKET_URL = 'http://socket.indep.gob.mx';
 
-  
-  
+
+
 })(this);
