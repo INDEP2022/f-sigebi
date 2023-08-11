18aa62eec95feae4789d0e98261a6df26497b7ea
IndustriasBeflo <riiiicho@gmail.com>
2023-08-11 03:59:33 +0200

    Cambios en pantallas Destino y Reportes de Cartas de Responsabilidad

--- src/app/common/constants/endpoints/ms-customers-endpoints.ts
+++ src/app/common/constants/endpoints/ms-customers-endpoints.ts
@@ -7,4 +7,5 @@ export enum CustomersEndpoints {
   PFmcomr612ClientxEvent1 = 'application/pFmcomr612ClientxEvent1',
   PFmcomr612ClientxEvent2 = 'application/pFmcomr612ClientxEvent2',
   Fcomer612Get1 = 'application/fcomer612Get1',
+  ApplicationDataCustomers = 'application/getDataCustomers',
 }
