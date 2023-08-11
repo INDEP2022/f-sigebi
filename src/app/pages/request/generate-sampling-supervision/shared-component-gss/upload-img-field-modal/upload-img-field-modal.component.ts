b132fb3d152e45f1150186c5fb93c445690160d1
Edson Percy Lopez Cortez <edson_lopez@hotmail.com.ar>
2023-08-11 15:56:17 -0400

    guardo de mi cambio

--- src/app/pages/request/generate-sampling-supervision/shared-component-gss/upload-img-field-modal/upload-img-field-modal.component.ts
+++ src/app/pages/request/generate-sampling-supervision/shared-component-gss/upload-img-field-modal/upload-img-field-modal.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
+import * as moment from 'moment';
 import { BsModalRef } from 'ngx-bootstrap/modal';
+import { AuthService } from 'src/app/core/services/authentication/auth.service';
 import { WContentService } from 'src/app/core/services/ms-wcontent/wcontent.service';
 import { IUploadEvent } from 'src/app/utils/file-upload/components/file-upload.component';
 
@@ -11,52 +13,65 @@ import { IUploadEvent } from 'src/app/utils/file-upload/components/file-upload.c
 export class UploadImgFieldModalComponent implements OnInit {
   data: any = null;
   process: string = '';
+  userLogName: string = '';
 
   constructor() {}
 
   private bsModalService = inject(BsModalRef);
   private wcontentService = inject(WContentService);
+  private authUser = inject(AuthService);
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getUser();
+    console.log(this.data);
+  }
+
+  getUser() {
+    const auth = this.authUser.decodeToken();
+    console.log(auth);
+    this.userLogName = auth.username;
+  }
 
   loadImage(uploadEvent: IUploadEvent) {
-    /*console.log(uploadEvent)
-    console.log(this.data)
-    console.log(this.process)
+    console.log(uploadEvent);
+    console.log(this.data);
+    console.log(this.process);
     const { index, fileEvents } = uploadEvent;
 
-    fileEvents.map((item)=>{
+    fileEvents.map(item => {
+      debugger;
       const formData = {
         xidcProfile: 'NSBDB_Gral',
         dDocAuthor: this.userLogName,
-        xidSolicitud: this.idRequest,
-        xidTransferente: this.idTrans,
-        xidBien: this.idGood,
-        xnombreProceso: 'Clasificar Bien',
+        //xidSolicitud: this.idRequest,
+        xidTransferente: '',
+        xidBien: this.data[0].goodId,
+        xnombreProceso: 'Generar Muestreo',
       };
       const contentType = 'img';
-      const docName = `IMG_${this.date}${contentType}`;
-      this.insertImage()
-    })
-        
+      const docName = `IMG_${this.getDate()}${contentType}`;
+
+      //this.insertImage(docName,contentType,JSON.stringify(formData),contentType)
+    });
   }
 
-  insertImage(docName: string, contentType:string, body:any, file:any){ 
+  insertImage(docName: string, contentType: string, body: any, file: any) {
     this.wcontentService
-            .addImagesToContent(
-              docName,
-              contentType,
-              body,
-              file
-            )
-            .subscribe({
-              next: data => {
-                this.bsModalService.content.callBack(true);
-                this.close();
-              },
-              error: error => {},
-            });*/
+      .addImagesToContent(docName, contentType, body, file)
+      .subscribe({
+        next: data => {
+          this.bsModalService.content.callBack(true);
+          this.close();
+        },
+        error: error => {},
+      });
   }
+
+  getDate() {
+    const date = new Date();
+    return moment(date).format('DD-MM-YYYY');
+  }
+
   close() {
     this.bsModalService.hide();
   }
