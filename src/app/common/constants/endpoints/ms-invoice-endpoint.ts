eb36db28ca8a72e0f88d1be2a1bdced98f619545
NCatariMamani <128624201+NCatariMamani@users.noreply.github.com>
2023-08-15 16:52:56 -0400

    FMCOMDONAC

--- src/app/common/constants/endpoints/ms-invoice-endpoint.ts
+++ src/app/common/constants/endpoints/ms-invoice-endpoint.ts
@@ -18,4 +18,5 @@ export enum ENDPOINT_INVOICE {
   ApplicationFolioDispo = 'application/valida-folios-dispo',
   ValidateUser = 'procedure-sera/validate-user',
   ControlProcedure = 'procedure-sera/ctrl-generate-preinvoices',
+  GetGegraficaFacturas = 'application/get-getGraficaFacturas',
 }
