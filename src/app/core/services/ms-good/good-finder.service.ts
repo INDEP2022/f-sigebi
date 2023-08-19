f6dcec4c19a4f1f43b02d317e219fe81bcbff65c
RodasPacco <angelorp93@gmail.com>
2023-08-18 21:36:43 -0500

    Correcciones y Cambios Fotos y Características (#3724)
    * cambios-fotos
    
    * correcciones

--- src/app/core/services/ms-good/good-finder.service.ts
+++ src/app/core/services/ms-good/good-finder.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpService, _Params } from 'src/app/common/services/http.service';
 
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
-import { IListResponse } from '../../interfaces/list-response.interface';
+import {
+  IListResponse,
+  IListResponseMessage,
+} from '../../interfaces/list-response.interface';
 import { IGood } from '../../models/good/good.model';
 import { GoodFinderEndpoint } from './../../../common/constants/endpoints/ms-good-endpoints';
 
@@ -13,6 +16,13 @@ export class GoodFinderService extends HttpService {
   constructor() {
     super();
     this.microservice = GoodFinderEndpoint.GoodFinderBase;
+  }
+
+  getByGoodNumber(goodId: string | number) {
+    const route = GoodFinderEndpoint.GoodQuery;
+    return this.get<IListResponseMessage<IGood>>(
+      `${route}?filter.goodId=$eq:${goodId}`
+    );
   }
 
   goodFinder(params?: _Params) {
