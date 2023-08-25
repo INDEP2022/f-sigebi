7b374637c9f1ad8d9993f86c329ae410f68f757c
Alvaro Tinoco <62573410+Aetiru@users.noreply.github.com>
2023-08-25 11:31:44 -0500

    Pantalla Muebles

--- src/app/pages/siab-web/tools/tools-routing.module.ts
+++ src/app/pages/siab-web/tools/tools-routing.module.ts
@@ -9,6 +9,12 @@ const routes: Routes = [
     data: { title: 'Consulta de inmuebles' },
   },
   {
+    path: 'propertyInm',
+    loadChildren: async () =>
+      (await import('./propertyInm/propertyInm.module')).PropertyInmModule,
+    data: { title: 'Consulta de Muebles' },
+  },
+  {
     path: 'scanned-documents',
     loadChildren: async () =>
       (await import('./scanned-documents/scanned-documents.module'))
