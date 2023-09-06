9845631e97a5db63a09069ea2cd25a5eb2409955
gus2612 <gustavoangelsantosclemente@gmail.com>
2023-09-06 00:15:35 -0600

    change part detail event in programming delivery

--- src/app/pages/request/shared-request/event-detail-form/event-detail-form.component.ts
+++ src/app/pages/request/shared-request/event-detail-form/event-detail-form.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as moment from 'moment';
 import { LocalDataSource } from 'ng2-smart-table';
 import { BehaviorSubject } from 'rxjs';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
+import { GenericService } from 'src/app/core/services/catalogs/generic.service';
 import { GoodTypeService } from 'src/app/core/services/catalogs/good-type.service';
+import { GoodsQueryService } from 'src/app/core/services/goodsquery/goods-query.service';
 import { ProgrammingRequestService } from 'src/app/core/services/ms-programming-request/programming-request.service';
 import { BasePage } from 'src/app/core/shared/base-page';
 import { STRING_PATTERN } from 'src/app/core/shared/patterns';
+import { IprogrammingDelivery } from 'src/app/pages/siab-web/sami/receipt-generation-sami/receipt-table-goods/ireceipt';
 import { IGoodDelivery } from '../../scheduling-deliveries/scheduling-deliveries-form/good-delivery.interface';
 import { SCHEDULING_DELIVERIES_COLUMNS } from './columns/scheduling-deliveries-columns';
+
 @Component({
   selector: 'app-event-detail-form',
   templateUrl: './event-detail-form.component.html',
@@ -29,11 +34,16 @@ export class EventDetailFormComponent extends BasePage implements OnInit {
   showDetails: boolean = true;
   showSearch: boolean = false;
   totalItems: number = 0;
+  nameTypeEvent: string = '';
+  nameStore: string = '';
+  programmingDeliveryInfo: IprogrammingDelivery;
   params = new BehaviorSubject<ListParams>(new ListParams());
   constructor(
     private fb: FormBuilder,
     private programmingService: ProgrammingRequestService,
-    private goodTypeService: GoodTypeService
+    private goodTypeService: GoodTypeService,
+    private genericService: GenericService,
+    private goodsQueryService: GoodsQueryService
   ) {
     super();
 
@@ -47,6 +57,62 @@ export class EventDetailFormComponent extends BasePage implements OnInit {
   ngOnInit(): void {
     this.prepareForm();
     this.showGoodDeliveryProg();
+    this.getInfoProgrammingDelivery();
+  }
+
+  getInfoProgrammingDelivery() {
+    const params = new BehaviorSubject<ListParams>(new ListParams());
+    params.getValue()['filter.id'] = 16896;
+    this.programmingService
+      .getProgrammingDelivery(params.getValue())
+      .subscribe({
+        next: response => {
+          console.log('Programaciones de entrega', response);
+          if (response.data[0].startDate)
+            response.data[0].startDate = moment(
+              response.data[0].startDate
+            ).format('DD/MM/YYYY HH:mm:ss');
+          if (response.data[0].endDate)
+            response.data[0].endDate = moment(response.data[0].endDate).format(
+              'DD/MM/YYYY HH:mm:ss'
+            );
+
+          if (response.data[0].typeEvent) {
+            this.getNameTypeEvent(response.data[0].typeEvent);
+          }
+
+          if (response.data[0].store) {
+            this.getNameStore(response.data[0].store);
+          }
+          this.programmingDeliveryInfo = response.data[0];
+        },
+        error: error => {},
+      });
+  }
+
+  getNameTypeEvent(typeEvent: number) {
+    const params = new BehaviorSubject<ListParams>(new ListParams());
+    params.getValue()['filter.name'] = 'Evento Entregas';
+    params.getValue()['filter.keyId'] = typeEvent;
+    this.genericService.getAll(params.getValue()).subscribe({
+      next: response => {
+        console.log('tipo evento', response);
+        this.nameTypeEvent = response.data[0].description;
+      },
+      error: error => {},
+    });
+  }
+
+  getNameStore(store: number) {
+    const params = new BehaviorSubject<ListParams>(new ListParams());
+    params.getValue()['filter.organizationCode'] = store;
+    this.goodsQueryService.getCatStoresView(params.getValue()).subscribe({
+      next: response => {
+        console.log('almacenes', response);
+        this.nameStore = response.data[0].name;
+      },
+      error: error => {},
+    });
   }
 
   showGoodDeliveryProg() {
@@ -59,12 +125,6 @@ export class EventDetailFormComponent extends BasePage implements OnInit {
           const showTypeGood: any = await this.showTypeGood(response.data);
           this.paragraphs.load([showTypeGood]);
           this.totalItems = response.count;
-          /*Promise.all(showTypeGood).then((info: any) => {
-          }); */
-          /*
-
-          
-          console.log('response', response); */
         },
         error: error => {},
       });
