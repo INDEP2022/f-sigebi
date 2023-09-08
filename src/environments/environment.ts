baadcc806cde0cc66fdc983457b4ba546c66de89
ELBarto <genaroadrian>
2023-09-07 19:52:05 -0600

    Rastreador

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 /** ********************************************************************
  * ! ME ENCANTARIA DECLARARTE MI AMOR PERO SOLO SE DECLARAR VARIABLES **
+ *
  *!                     ****   ****
  *!                    **************
  *!                    *************
