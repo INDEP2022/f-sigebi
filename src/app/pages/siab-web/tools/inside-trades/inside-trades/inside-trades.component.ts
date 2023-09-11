9fa1195183ce334680dcdec4a31c122e177d383b
johann1997 <johann.montoya@uao.edu.co>
2023-09-10 23:43:43 -0500

    oficios externo e internos

--- src/app/pages/siab-web/tools/inside-trades/inside-trades/inside-trades.component.ts
+++ src/app/pages/siab-web/tools/inside-trades/inside-trades/inside-trades.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { takeUntil } from 'rxjs';
 import { AuthService } from 'src/app/core/services/authentication/auth.service';
+import { OutsideTradesService } from 'src/app/core/services/catalogs/outside-trades.service';
 import { BasePage } from 'src/app/core/shared';
 import { STRING_PATTERN } from 'src/app/core/shared/patterns';
 
@@ -15,6 +16,7 @@ export class InsideTradesComponent extends BasePage implements OnInit {
   constructor(
     private authService: AuthService,
     private activatedRoute: ActivatedRoute,
+    private outsideTradesService: OutsideTradesService,
     private router: Router
   ) {
     super();
@@ -31,6 +33,10 @@ export class InsideTradesComponent extends BasePage implements OnInit {
   ngOnInit(): void {
     this.createForm();
     this.getData();
+
+    console.log(this.authService.decodeToken());
+    //verificar si la autenticacion es correcta
+
     if (this.authService.decodeToken().azp === 'indep-auth') {
       this.activatedRoute.queryParams
         .pipe(takeUntil(this.$unSubscribe))
@@ -42,6 +48,7 @@ export class InsideTradesComponent extends BasePage implements OnInit {
       if (this.val_no_ges == null || this.val_no_ges == '') {
         //
         //
+        ////  this.router.navigate([`/auth/login`]);
         //this.router.navigate([`/pages/home`]);
         //
         //
@@ -50,30 +57,69 @@ export class InsideTradesComponent extends BasePage implements OnInit {
         this.noOFGestion = JSON.parse(this.val_no_ges);
         // implementa el endpoint  filtro.tipo_oficio(noOFGestion);
         let metodo: number;
-        this.principal = new Array();
+        // this.principal = new Array();
+        this.outsideTradesService.getBasicBody(this.noOFGestion).subscribe({
+          next: data => {
+            this.principal = data.data;
+            console.log(this.principal);
+          },
+          error: err => {
+            console.log(err);
+          },
+        });
 
-        this.form.controls['cve_of_gestion'] = this.principal[0];
-        this.form.controls['fecha'] = this.principal[1];
-        this.form.controls['nombre'] = this.principal[3];
+        this.form.controls['cve_of_gestion'] = this.principal.cve_of_gestion;
+        this.form.controls['fecha'] = this.principal.fecha;
+        this.form.controls['nombre'] = this.principal.nombre;
         this.form.value.ciudad = 'ADMINISTRACION';
-        this.form.controls['texto1'] = this.principal[4];
-        this.form.controls['texto2'] = this.principal[5];
-        this.form.controls['NoVolante'] = this.principal[11];
-        this.form.controls['NoExpediente'] = this.principal[12];
+        this.form.controls['texto1'] = this.principal.texto1;
+        this.form.controls['texto2'] = this.principal.texto2;
+        this.form.controls['NoVolante'] = this.principal.no_volante;
+        this.form.controls['NoExpediente'] = this.principal.no_expediente;
         //metodos para obtener la data de los siguientes 2 paramentros
-        this.form.controls['avePre'] = this.principal[metodo];
+        this.form.controls['avePre'].setValue(
+          this.outsideTradesService
+            .getPreliminaryInvestigation(this.noOFGestion)
+            .subscribe({
+              next: data => {
+                return data.data[0].averiguacion_previa;
+              },
+              error: err => {},
+            })
+        );
         //metodos para obtener la data del siguiente paramentro
-        /**/ this.form.controls['notv'] = this.principal[metodo];
+        /**/ this.form.controls['notv'].setValue(
+          this.outsideTradesService.getNameOTValue(this.noOFGestion).subscribe({
+            next: data => {
+              return data.data[0];
+            },
+            error: err => {
+              console.log(err);
+            },
+          })
+        );
         this.form.controls['nombre2'] = this.form.value.notv[0];
         this.form.controls['OTVALOR1'] = this.form.value.notv[1];
         //metodos para obtener la data del siguiente paramentro
-        this.form.controls['cpp'] = this.principal[metodo];
+        this.form.controls['cpp'].setValue(
+          this.outsideTradesService.getCPP(this.noOFGestion).subscribe({
+            next: data => {
+              let message = `tipo:${data.data[0].tipo}, cpp:${data.data[0].cpp}`;
+              return message;
+            },
+            error: err => {
+              console.log(err);
+            },
+          })
+        );
       }
       this.sessionInvalid = false;
     } else {
       this.sessionInvalid = true;
-      this.onLoadToast('success', 'ay caramba', 'Actualizado Correctamente');
+      //this.onLoadToast('success', 'ay caramba', 'Actualizado Correctamente');
       //this.router.navigate([`/pages/home`]);
+      this.router.navigate([`/auth/login`]);
+
       localStorage.clear();
     }
   }
