7946c3085826e5bd2dea13a2d96f551db9b7d1eb
SergioRTurizoM <>
2023-09-14 19:33:02 -0500

    servicio donacion

--- src/app/common/constants/endpoints/ms-donation.ts
+++ src/app/common/constants/endpoints/ms-donation.ts
@@ -7,4 +7,5 @@ export enum DonationEndPoint {
   eventComDonation = 'event-com-donation',
   DetailEventComDon = 'detail-event-com-don',
   eventComDonationExcel = 'event-com-donation/excel-export',
+  fdonacDocumAdm1 = 'fdonacDocumAdm1',
 }
