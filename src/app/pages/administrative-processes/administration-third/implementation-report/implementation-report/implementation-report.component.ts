9ff8f689f4c4835594b7aa7329565a4dac109bd2
Jessica Orozco <jessicaorozco@gmail.com>
2023-09-15 00:43:52 -0400

    cambio en form

--- src/app/pages/administrative-processes/administration-third/implementation-report/implementation-report/implementation-report.component.ts
+++ src/app/pages/administrative-processes/administration-third/implementation-report/implementation-report/implementation-report.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
@@ -7,6 +8,7 @@ import {
   ListParams,
 } from 'src/app/common/repository/interfaces/list-params';
 import {
+  IReportImp,
   IStrategyLovSer,
   IStrategyProcess,
   IStrategyTurn,
@@ -15,9 +17,9 @@ import {
 } from 'src/app/core/models/ms-strategy-service/strategy-service.model';
 import { AuthService } from 'src/app/core/services/authentication/auth.service';
 import { IndicatorsParametersService } from 'src/app/core/services/ms-parametergood/indicators-parameter.service';
+import { StrategyProcessService } from 'src/app/core/services/ms-strategy/strategy-process.service';
 import { StrategyServiceService } from 'src/app/core/services/ms-strategy/strategy-service.service';
 import { BasePage } from 'src/app/core/shared/base-page';
-import { STRING_PATTERN } from 'src/app/core/shared/patterns';
 import { DefaultSelect } from 'src/app/shared/components/select/default-select';
 import { ImplementationReportHistoricComponent } from '../implementation-report-historic/implementation-report-historic.component';
 import {
@@ -35,6 +37,7 @@ export class ImplementationReportComponent extends BasePage implements OnInit {
   filterLovSer: IStrategyLovSer;
   filterTurn: IStrategyTurn;
   filterCost: IStrateyCost;
+  reportImp: IReportImp;
   area: number = 0;
   data1: any[] = [];
   desStrategy: string = '';
@@ -42,6 +45,9 @@ export class ImplementationReportComponent extends BasePage implements OnInit {
   turnos = new DefaultSelect();
   totalItems: number = 0;
   delegationUser: any;
+  dateCapt: string = '';
+  maxDate = new Date();
+  dateClose: string = '';
   delegation = new DefaultSelect();
   settings2 = { ...this.settings, actions: false };
   types = new DefaultSelect();
@@ -50,6 +56,7 @@ export class ImplementationReportComponent extends BasePage implements OnInit {
   public process = new DefaultSelect<IStrategyProcess>();
   public regionalCoordination = new DefaultSelect();
   public reportKey = new DefaultSelect();
+  public noFormat: number = 0;
   public status = new DefaultSelect();
 
   constructor(
@@ -57,7 +64,9 @@ export class ImplementationReportComponent extends BasePage implements OnInit {
     private modalService: BsModalService,
     private strategyServiceService: StrategyServiceService,
     private authService: AuthService,
-    private indicatorsParametersService: IndicatorsParametersService
+    private indicatorsParametersService: IndicatorsParametersService,
+    private datePipe: DatePipe,
+    private strategyProcessService: StrategyProcessService
   ) {
     super();
     this.settings = {
@@ -76,32 +85,19 @@ export class ImplementationReportComponent extends BasePage implements OnInit {
   private prepareForm() {
     this.serviceOrdersForm = this.fb.group({
       serviceOrderKey: [null, Validators.required],
+      noFormat: [null, Validators.required],
       type: [null, Validators.required],
       turno: [null, Validators.required],
       process: [null, Validators.required],
       regionalCoordination: [null, Validators.required],
-      reportKey: [null, Validators.required],
+      reportKey: [null],
       status: [null, Validators.required],
       authorizationDate: [null, Validators.required],
       dateCapture: [null, Validators.required],
-      observations: [
-        null,
-        Validators.required,
-        Validators.pattern(STRING_PATTERN),
-      ],
+      observations: [null, Validators.required],
     });
   }
 
-  public getReportKey(event: any) {
-    // this.bankService.getAll(params).subscribe(data => {
-    //   this.peritos = new DefaultSelect(data.data, data.count);
-    // });
-  }
-  public getStatus(event: any) {
-    // this.bankService.getAll(params).subscribe(data => {
-    //   this.peritos = new DefaultSelect(data.data, data.count);
-    // });
-  }
   openHistoric(data: any) {
     let config: ModalOptions = {
       initialState: {
@@ -131,6 +127,7 @@ export class ImplementationReportComponent extends BasePage implements OnInit {
       },
     });
   }
+
   getServices() {
     this.filterLovSer = {
       pProcessNumber: Number(this.serviceOrdersForm.get('process').value),
@@ -222,7 +219,7 @@ export class ImplementationReportComponent extends BasePage implements OnInit {
       pServiceTypeNumber: Number(this.serviceOrdersForm.get('type').value),
       pTurnNumber: this.serviceOrdersForm.get('turno').value,
     };
-    this.strategyServiceService.getTurn(this.filterTurn).subscribe({
+    this.strategyServiceService.getCosts(this.filterCost).subscribe({
       next: data => {
         // data.data.filter((item: any) => {
         //   item['turnAndName'] = item.no_tiposervicio + '-' + item.descripcion;
@@ -236,16 +233,135 @@ export class ImplementationReportComponent extends BasePage implements OnInit {
       },
     });
   }
-  getPostQuery() {
-    // cursor T_DESPR is
-    // select DESCRIPCION
-    //   from ESTRATEGIA_PROCESO
-    //  where NO_PROCESO = : ESTRATEGIA_FORMATO.NO_PROCESO;
-    //   --Descripción de la Coordinacion Regional(2)
-    // cursor T_DELEG is
-    // select DESCRIPCION
-    //   from CAT_DELEGACIONES
-    //  where NO_DELEGACION = : ESTRATEGIA_FORMATO.NO_DELEGACION_1
-    //  and ETAPA_EDO = FA_ETAPACREDA(SYSDATE);
+  getReportImp() {
+    // if (this.serviceOrdersForm.value.noFormat == null || 0) {
+    //   this.alert('warning', 'Debe ingresar la Clave de Orden de Servicio', '');
+    //   return;
+    // }
+    this.strategyProcessService
+      .getStrategyRepImplementation(this.serviceOrdersForm.value.noFormat)
+      .subscribe({
+        next: data => {
+          data.data.filter((value: any) => {
+            this.reportImp = value;
+            console.log(value);
+            this.dateCapt = this.datePipe.transform(
+              this.serviceOrdersForm.value.dateCapture,
+              'dd/MM/yyyy'
+            );
+            this.dateClose = this.datePipe.transform(
+              this.serviceOrdersForm.value.authorizationDate,
+              'dd/MM/yyyy'
+            );
+            this.serviceOrdersForm.get('reportKey').setValue(value.reportKey);
+            this.serviceOrdersForm.get('status').setValue(value.status);
+            this.serviceOrdersForm
+              .get('authorizationDate')
+              .setValue(this.dateClose);
+            this.serviceOrdersForm.get('dateCapture').setValue(this.dateCapt);
+            this.serviceOrdersForm
+              .get('observations')
+              .setValue(value.observations);
+          });
+        },
+      });
   }
+  pupGenera() {
+    console.log(this.serviceOrdersForm.value.noFormat);
+    try {
+      if (this.serviceOrdersForm.value.noFormat == null) {
+        this.alert(
+          'warning',
+          'Debe ingresar la Clave de Orden de Servicio y seleccionar las Estrategias de Administración',
+          ''
+        );
+        return;
+      }
+      this.alertQuestion(
+        'warning',
+        'Generar',
+        '¿Seguro que desea generar el Reporte de Implementación?'
+      ).then(question => {
+        if (question.isConfirmed) {
+          if (
+            this.reportImp.reportNumber == null &&
+            this.reportImp.reportKey != null
+          ) {
+            this.genClave();
+          } else if (this.reportImp.captureDate == null) {
+            this.alert(
+              'warning',
+              'La Clave de Acta aun no se encuentra Cerrada',
+              ''
+            );
+          } else {
+            this.alert(
+              'warning',
+              'El tiempo para Generar la Clave de Reporte a Expirado',
+              ''
+            );
+          }
+        }
+      });
+      // this.getReportImp();
+      this.dateCapt = this.datePipe.transform(
+        this.serviceOrdersForm.value.dateCapture,
+        'dd/MM/yyyy'
+      );
+      this.dateClose = this.datePipe.transform(
+        this.serviceOrdersForm.value.authorizationDate,
+        'dd/MM/yyyy'
+      );
+      let fechaCapture = this.dateCapt;
+      let fechaCierre = this.dateClose;
+      let vParUser = this.authService.decodeToken().username;
+    } catch {
+      console.log('error reporte');
+    }
+  }
+
+  cargaBienes() {
+    //   if : ESTRATEGIA_REP_IMPLEMENTACION.NO_REPORTE is null then
+    //   LIP_MENSAJE('No se pueden incorporar bienes si no hay Reporte de Implementación', 'C');
+    // else
+    // LIP_COMMIT_SILENCIOSO;
+    // PUP_INCORPORA_BIENES;
+    // end if;
+    if (this.reportImp.reportNumber == null) {
+      this.alert(
+        'warning',
+        'Debe elegir una Estrategia de Administración para Generar la clave de Reporte de Implementación',
+        ''
+      );
+      return;
+    }
+    this.alertQuestion(
+      'warning',
+      'Generar',
+      '¿Seguro que desea generar el Reporte de Implementación?'
+    ).then(question => {
+      if (question.isConfirmed) {
+        // if (this.repImplenta == null) {
+        // }
+        // IF: ESTRATEGIA_REP_IMPLEMENTACION.NO_REPORTE is null then
+        // this.parameterTiieService.remove(tiie.id).subscribe({
+        //   next: data => {
+        //     this.loading = false;
+        //     this.onLoadToast('success', 'Registro Eliminado', '');
+        //     this.getData();
+        //   },
+        //   error: error => {
+        //     this.onLoadToast('error', 'No Se Puede Eliminar Registro', '');
+        //     this.loading = false;
+        //   },
+        // });
+      }
+    });
+  }
+  cleanForm() {
+    this.serviceOrdersForm.reset();
+    this.reportImp = null;
+  }
+
+  genClave() {}
 }
