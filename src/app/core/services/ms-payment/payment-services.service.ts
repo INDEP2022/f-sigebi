69c593287bd22bca09e946c443ff333b674c421e
IndustriasBeflo <riiiicho@gmail.com>
2023-09-19 03:24:58 +0200

    Cambios destino pantalla pagos devolucion

--- src/app/core/services/ms-payment/payment-services.service.ts
+++ src/app/core/services/ms-payment/payment-services.service.ts
@@ -215,4 +215,8 @@ export class PaymentService extends HttpService {
   getComerRelUsuCanc(params?: string) {
     return this.get(`comer-rel-usu-canc`, params);
   }
+
+  getDataFromView(params: any) {
+    return this.post(PaymentEndPoints.GetDataFromView, params);
+  }
 }
