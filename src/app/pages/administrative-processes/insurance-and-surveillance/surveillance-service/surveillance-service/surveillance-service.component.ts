31b05ff96da78628e75a3aeea7e3a0f9ab335580
WilmerJSB04 <wilmersolorzano.dev@gmail.com>
2023-09-21 11:48:07 -0400

    Servicio de vigilancia

--- src/app/pages/administrative-processes/insurance-and-surveillance/surveillance-service/surveillance-service/surveillance-service.component.ts
+++ src/app/pages/administrative-processes/insurance-and-surveillance/surveillance-service/surveillance-service/surveillance-service.component.ts
@@ -199,7 +199,7 @@ export class SurveillanceServiceComponent extends BasePage implements OnInit {
     params.sortBy = `delegationNumber:ASC`;
     return new Promise((resolve, reject) => {
       this.survillanceService
-        .getViewVigDelegations(params.getParams())
+        .getViewVigDelegations_(params.getParams())
         .subscribe({
           next: async (response: any) => {
             console.log('resss', response);
