10ea9ce0ed8e609d1dbb4423705a5ce718269287
Alvaro Tinoco <62573410+Aetiru@users.noreply.github.com>
2023-09-22 14:28:11 -0500

    Alvaro dev3 (#4173)
    * adas
    
    * a
    
    * FESTATUSRGA

--- src/app/pages/executive-processes/destruction-authorization-management/destruction-authorization-management.module.ts
+++ src/app/pages/executive-processes/destruction-authorization-management/destruction-authorization-management.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { PreviewDocumentsComponent } from 'src/app/@standalone/preview-documents/preview-documents.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DestructionAuthorizationManagementRoutingModule } from './destruction-authorization-management-routing.module';
@@ -29,6 +30,7 @@ import { ProceedingsModalComponent } from './proceedings-modal/proceedings-modal
     AlertModule.forRoot(),
     StoreModule.forFeature('destructionAuth', destructionAuthReducer),
     PreviewDocumentsComponent,
+    TooltipModule.forRoot(),
   ],
 })
 export class DestructionAuthorizationManagementModule {}
