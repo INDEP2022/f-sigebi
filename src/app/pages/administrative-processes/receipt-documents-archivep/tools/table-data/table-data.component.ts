a0b686fb81fc26573106607816e938bb161ee533
aalvarezg1998 <107310910+aalvarezg1998@users.noreply.github.com>
2023-09-25 19:20:24 -0500

    Se resuelven incidencias

---
+++ src/app/pages/administrative-processes/receipt-documents-archivep/tools/table-data/table-data.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-data',
+  templateUrl: './table-data.component.html',
+  styles: [],
+})
+export class TableDataComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
