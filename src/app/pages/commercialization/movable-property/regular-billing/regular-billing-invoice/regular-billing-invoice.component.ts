37315b9a0e047094c46cf092d79a02463f230313
Henry Giovanni Sánchez Solís <111904384+HenryGiovanniSanchezSolis@users.noreply.github.com>
2023-10-09 18:58:16 -0600

    Fix henry v017 (#4258)
    * actualizando
    
    * msj
    
    * updates
    
    * updates
    
    * cambios
    
    * mejorando

--- src/app/pages/commercialization/movable-property/regular-billing/regular-billing-invoice/regular-billing-invoice.component.ts
+++ src/app/pages/commercialization/movable-property/regular-billing/regular-billing-invoice/regular-billing-invoice.component.ts
@@ -2029,7 +2029,7 @@ export class RegularBillingInvoiceComponent extends BasePage implements OnInit {
       this.alert(
         'warning',
         'Atención',
-        'No cuenta con los permisos para efectuar esta operación'
+        'No cuenta con los permisos para realizar esta operación'
       );
     } else {
       if (data.length == 0) {
@@ -2059,7 +2059,7 @@ export class RegularBillingInvoiceComponent extends BasePage implements OnInit {
               next: () => {
                 this.alert(
                   'success',
-                  'La Fecha de impresión ha sido actualizada',
+                  'La fecha de impresión ha sido actualizada',
                   ''
                 );
               },
