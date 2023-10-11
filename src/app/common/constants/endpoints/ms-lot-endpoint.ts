e59704563345bb2394ae4cb1b9d86c7c4eb7b4b9
Jerson-P <bolanosy8@gmail.com>
2023-10-11 10:53:07 -0500

    cambios pantalla FOFICIOCOMER-Creación de Oficios Comer

--- src/app/common/constants/endpoints/ms-lot-endpoint.ts
+++ src/app/common/constants/endpoints/ms-lot-endpoint.ts
@@ -19,4 +19,5 @@ export enum LotEndpoints {
   AppsConsultPayLots = 'apps/consult-pay-lots-pag',
   PupCambioMasv = 'apps/pup-cambio-masiv',
   GetCursor = 'apps/get-cursor',
+  pupRemiEntLote = 'apps/pupRemiEntLote',
 }
