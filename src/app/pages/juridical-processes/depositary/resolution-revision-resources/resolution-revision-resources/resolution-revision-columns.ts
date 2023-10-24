3b4fc0a6cfbd7370e5b0410defc8430a471b7b77
Jessica Orozco <52942112+jessicaorozco@users.noreply.github.com>
2023-10-24 17:44:37 -0400

    Bug hiist7 (#4357)
    * udate resolution-resources endpoint timeout solved
    
    * table Localdata source ok
    
    * update service goods
    
    * goodhistorical
    
    * good historical button back

--- src/app/pages/juridical-processes/depositary/resolution-revision-resources/resolution-revision-resources/resolution-revision-columns.ts
+++ src/app/pages/juridical-processes/depositary/resolution-revision-resources/resolution-revision-resources/resolution-revision-columns.ts
@@ -1,5 +1,7 @@
+import { CustomDateFilterComponent } from 'src/app/@standalone/shared-forms/filter-date-custom/custom-date-filter';
+
 export const RESOLUTION_REVISION_COLUMNS = {
-  NoBien: {
+  goodId: {
     title: 'No Bien',
     type: 'string',
     sort: false,
@@ -8,36 +10,92 @@ export const RESOLUTION_REVISION_COLUMNS = {
     title: 'Descripción',
     type: 'string',
     sort: false,
-  },
-  quantity: {
-    title: 'Cantidad',
-    type: 'number',
-    sort: false,
+    valuePrepareFunction: (cell: any, row: any) => {
+      return row?.description;
+    },
   },
   status: {
     title: 'Estatus',
-    type: 'string',
     sort: false,
+    valuePrepareFunction: (cell: any, row: any) => {
+      return row?.status;
+    },
   },
-  motiveReRe: {
+  quantity: {
+    title: 'Cantidad',
+    sort: false,
+    valuePrepareFunction: (amount: string) => {
+      const numericAmount = parseFloat(amount);
+      if (!isNaN(numericAmount)) {
+        return numericAmount.toLocaleString('en-US', {
+          // style: 'currency',
+          // currency: 'USD',
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        });
+      } else {
+        return amount;
+      }
+    },
+  },
+  revRecCause: {
     title: 'Motivo de Recurso de Revisión',
     type: 'string',
     sort: false,
     width: '20%',
+    valuePrepareFunction: (cell: any, row: any) => {
+      return row?.revRecCause;
+    },
   },
-  dateRecep: {
+  physicalReceptionDate: {
     title: 'Fecha de Recepción',
-    type: 'string',
     sort: false,
+    valuePrepareFunction: (value: string) => formatDate(value),
+    filter: {
+      type: 'custom',
+      component: CustomDateFilterComponent,
+    },
+    filterFunction(cell?: any, search?: string): boolean {
+      let column = cell;
+      if (column?.toUpperCase() >= search.toUpperCase() || search === '') {
+        return true;
+      } else {
+        return false;
+      }
+    },
   },
-  dateEmitRes: {
+  resolutionEmissionRecRevDate: {
     title: 'Fecha de Emisión de Resolución',
-    type: 'string',
     sort: false,
+    //  type: 'html',
+    valuePrepareFunction: (value: string) => formatDate(value),
+    filter: {
+      type: 'custom',
+      component: CustomDateFilterComponent,
+    },
+    filterFunction(cell?: any, search?: string): boolean {
+      let column = cell;
+      if (column?.toUpperCase() >= search.toUpperCase() || search === '') {
+        return true;
+      } else {
+        return false;
+      }
+    },
   },
-  obResourceRe: {
+  revRecObservations: {
     title: 'Observaciones del Recurso de Revisión',
     type: 'string',
     sort: false,
+    valuePrepareFunction: (cell: any, row: any) => {
+      return row?.revRecObservations;
+    },
   },
 };
+
+function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${day}/${month}/${year}`;
+}
