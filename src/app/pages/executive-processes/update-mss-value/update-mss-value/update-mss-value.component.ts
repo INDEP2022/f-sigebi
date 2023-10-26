69e5fde2babff48d4c5e63f515f8264d18a16023
Alvaro Tinoco <62573410+Aetiru@users.noreply.github.com>
2023-10-26 16:02:02 -0500

    m

--- src/app/pages/executive-processes/update-mss-value/update-mss-value/update-mss-value.component.ts
+++ src/app/pages/executive-processes/update-mss-value/update-mss-value/update-mss-value.component.ts
@@ -5,10 +5,12 @@ import { BasePage } from 'src/app/core/shared/base-page';
 import { UPDATE_MASS_VALUE_COLUMNS } from './update-mss-value-columns';
 
 //XLSX
+import { DatePipe } from '@angular/common';
 import { LocalDataSource } from 'ng2-smart-table';
 import { BehaviorSubject } from 'rxjs';
 import { ListParams } from 'src/app/common/repository/interfaces/list-params';
 import { ExcelService } from 'src/app/common/services/excel.service';
+import { GoodProcessService } from 'src/app/core/services/ms-good/good-process.service';
 
 @Component({
   selector: 'app-update-mss-value',
@@ -23,9 +25,17 @@ export class UpdateMssValueComponent extends BasePage implements OnInit {
   totalItems: number = 0;
   dataExcel: any = [];
   columns: any[] = [];
+  mappedData: any = [];
   fileReader = new FileReader();
+  tRegProcesados: number = 0;
+  tLeidos: number = 0;
 
-  constructor(private fb: FormBuilder, private excelService: ExcelService) {
+  constructor(
+    private fb: FormBuilder,
+    private excelService: ExcelService,
+    private goodProcessService: GoodProcessService,
+    private datePipe: DatePipe
+  ) {
     super();
     this.settings = {
       ...this.settings,
@@ -36,7 +46,6 @@ export class UpdateMssValueComponent extends BasePage implements OnInit {
 
   ngOnInit(): void {
     this.prepareForm();
-    //this.getPagination();
   }
 
   private prepareForm() {
@@ -63,7 +72,7 @@ export class UpdateMssValueComponent extends BasePage implements OnInit {
       this.fileReader.onload = loadEvent => {
         if (loadEvent.target && loadEvent.target.result) {
           // Llama a la función para procesar el archivo
-          this.readExcel(loadEvent.target.result);
+          this.Excel(loadEvent.target.result);
 
           // Limpia el input de archivo para permitir cargar el mismo archivo nuevamente
           (event.target as HTMLInputElement).value = '';
@@ -75,37 +84,89 @@ export class UpdateMssValueComponent extends BasePage implements OnInit {
     } catch (error) {}
   }
 
-  readExcel(binaryExcel: string | ArrayBuffer) {
+  Excel(binaryExcel: string | ArrayBuffer) {
     try {
       this.dataExcel = this.excelService.getData(binaryExcel);
-      const mappedData: any = [];
+
       for (let i = 0; i < this.dataExcel.length; i++) {
-        mappedData.push({
-          SOLICITANTE: this.dataExcel[i].SOLICITANTE,
-          FECAVALUO: this.dataExcel[i].FECAVALUO,
-          INSTITUCION: this.dataExcel[i].INSTITUCION,
-          PERITO: this.dataExcel[i].PERITO,
-          OBSERV: this.dataExcel[i].OBSERV,
-          NOBIEN: this.dataExcel[i].NOBIEN,
-          VALORAVALUO: this.dataExcel[i].VALORAVALUO,
-          MONEDA: this.dataExcel[i].MONEDA,
+        const fechaOriginal = this.dataExcel[i].FECAVALUO;
+
+        // Convertir el número de fecha en una fecha de JavaScript
+        const fechaExcel = parseFloat(fechaOriginal);
+        const fechaJavaScript = new Date((fechaExcel - 25569) * 86400 * 1000);
+
+        // Formatear la fecha en 'AAAA-MM-DD'
+        const year = fechaJavaScript.getFullYear();
+        const month = (fechaJavaScript.getMonth() + 1)
+          .toString()
+          .padStart(2, '0');
+        const day = fechaJavaScript.getDate().toString().padStart(2, '0');
+        const fechaForm = `${year}-${month}-${day}`;
+
+        this.mappedData.push({
+          proficientOpinion: this.dataExcel[i].SOLICITANTE,
+          appraisalVigDate: fechaForm,
+          valuerOpinion: this.dataExcel[i].PERITO,
+          goodNumber: this.dataExcel[i].NOBIEN,
+          appraisedValue: this.dataExcel[i].VALORAVALUO,
+          appraisalCurrencyKey: this.dataExcel[i].MONEDA,
         });
       }
-      console.log(mappedData);
-      this.source.load(mappedData);
+      console.log(this.dataExcel);
+      this.source.load(this.mappedData);
+      this.tLeidos = this.dataExcel.length;
+      this.totalItems = this.tLeidos;
+      this.columns = this.ExcelData;
     } catch (error) {
-      this.onLoadToast('error', 'Ocurrio un error al leer el archivo', 'Error');
+      this.onLoadToast('error', 'Ocurrió un error al leer el archivo', 'Error');
     }
-  }
-
-  getPagination() {
-    this.columns = this.ExcelData;
-    this.totalItems = this.columns.length;
   }
 
   clean() {
     this.totalItems = 0;
+    this.tLeidos = 0;
+    this.tRegProcesados = 0;
     this.columns = [];
     this.ExcelData = [];
+    this.mappedData = [];
+    this.source.load([]);
+  }
+
+  formatDate(date: Date): string {
+    const day = date.getUTCDate().toString().padStart(2, '0');
+    const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+    const year = date.getUTCFullYear().toString();
+    return `${year}-${month}-${day}`;
+  }
+
+  updateMassive() {
+    console.log(this.mappedData);
+
+    if (this.mappedData.length === 0) {
+      this.alert('warning', 'Debe importar un archivo a excel', '');
+      return;
+    }
+
+    const body = {
+      blkPreview: this.mappedData,
+    };
+
+    console.log(body);
+
+    this.goodProcessService.massiveUpdate(body).subscribe({
+      next: resp => {
+        console.log(resp);
+        this.tRegProcesados = resp.T_REG_PROCESADOS;
+        this.tLeidos = this.dataExcel.length;
+        this.alert(
+          'success',
+          'Registros  procesados exitosamente:',
+          resp.T_REG_PROCESADOS
+        );
+      },
+      error: err => {
+        console.log(err);
+      },
+    });
   }
 }
