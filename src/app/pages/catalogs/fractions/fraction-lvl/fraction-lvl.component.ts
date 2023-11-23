a1a73a61b22c21f33a9b691cd78e240f83533bc2
edson <edson_lopez@hotmail.com.ar>
2023-11-23 19:55:26 -0400

    modificacion de fraciones

--- src/app/pages/catalogs/fractions/fraction-lvl/fraction-lvl.component.ts
+++ src/app/pages/catalogs/fractions/fraction-lvl/fraction-lvl.component.ts
@@ -1,24 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
 import { BsModalService, ModalOptions } from 'ngx-bootstrap/modal';
-import { BehaviorSubject, takeUntil } from 'rxjs';
-
+import { BehaviorSubject, catchError, of, takeUntil } from 'rxjs';
 import {
   ListParams,
   SearchFilter,
 } from 'src/app/common/repository/interfaces/list-params';
 import { IFraction } from 'src/app/core/models/catalogs/fraction.model';
 import { FractionService } from 'src/app/core/services/catalogs/fraction.service';
-import { BasePage } from 'src/app/core/shared/base-page';
+import { BasePage } from 'src/app/core/shared';
 import { FractionsFormComponent } from '../fractions-form/fractions-form.component';
-import { FRACTIONS_COLUMNS } from './fractions-columns';
+import { FRACTIONS_COLUMNS } from '../fractions-list/fractions-columns';
 
 @Component({
-  selector: 'app-fractions-list',
-  templateUrl: './fractions-list.component.html',
+  selector: 'app-fraction-lvl',
+  templateUrl: './fraction-lvl.component.html',
   styles: [],
 })
-export class FractionsListComponent extends BasePage implements OnInit {
+export class FractionLvlComponent extends BasePage implements OnInit {
+  @Input() fractionLvl: any = null;
+  @Output() returnfraction = new EventEmitter<any>();
+  @Output() nofraction = new EventEmitter<any>();
+
   paragraphs: IFraction[] = [];
   data: LocalDataSource = new LocalDataSource();
 
@@ -26,18 +29,7 @@ export class FractionsListComponent extends BasePage implements OnInit {
   params = new BehaviorSubject<ListParams>(new ListParams());
   columnFilters: any = [];
 
-  parentIdlvl1: number = null;
-  parentIdlvl2: number = null;
-  parentIdlvl3: number = null;
-  parentIdlvl4: number = null;
-  parentIdlvl5: number = null;
-  parentIdlvl6: number = null;
-  fractionLvl1: IFraction = null;
-  fractionLvl2: IFraction = null;
-  fractionLvl3: IFraction = null;
-  fractionLvl4: IFraction = null;
-  fractionLvl5: IFraction = null;
-  fractionLvl6: IFraction = null;
+  level: number = 0;
 
   constructor(
     private fractionService: FractionService,
@@ -45,17 +37,17 @@ export class FractionsListComponent extends BasePage implements OnInit {
   ) {
     super();
     this.settings.columns = FRACTIONS_COLUMNS;
-    this.settings.actions = false;
-    //this.settings.actions.delete = true;
-    //this.settings.actions.add = false;
+    this.settings.actions.delete = true;
+    this.settings.actions.add = false;
     this.settings.hideSubHeader = false;
   }
 
   ngOnInit(): void {
+    this.level = Number(this.fractionLvl.level) + 1;
     this.data
       .onChanged()
       .pipe(takeUntil(this.$unSubscribe))
-      .subscribe(change => {
+      .subscribe((change: any) => {
         if (change.action === 'filter') {
           let filters = change.filter.filters;
           filters.map((filter: any) => {
@@ -112,137 +104,48 @@ export class FractionsListComponent extends BasePage implements OnInit {
       ...this.params.getValue(),
       ...this.columnFilters,
     };
-    params['filter.level'] = `$eq:0`;
+    params['filter.parentId'] = `$eq:${this.fractionLvl.id}`;
     params['sortBy'] = `id:ASC`;
-    this.fractionService.getAll(params).subscribe({
-      next: response => {
-        /*this.paragraphs = response.data.map((item: any) => {
+    this.fractionService
+      .getAll(params)
+      .pipe(
+        catchError(e => {
+          if (e.status == 400) return of({ data: [], count: 0 });
+          return e;
+        })
+      )
+      .subscribe({
+        next: (response: any) => {
+          /*this.paragraphs = response.data.map((item: any) => {
           item.clasificationName =
             item.siabClasification?.sssubtypeDescription || '';
           return item;
         });*/
-        this.totalItems = response.count || 0;
-        this.data.load(response.data);
-        this.data.refresh();
-        this.loading = false;
-      },
-      error: error => (this.loading = false),
-    });
+          if (response.data.length == 0) {
+            this.onLoadToast('info', 'No cuenta con mas fracciones');
+            this.nofraction.emit({ level: this.fractionLvl.level });
+            return;
+          }
+          this.totalItems = response.count || 0;
+          this.data.load(response.data);
+          this.data.refresh();
+          this.loading = false;
+        },
+        error: error => (this.loading = false),
+      });
   }
 
   selectRow(event: any) {
-    console.log(event);
-    if (event.isSelected == false) {
-      return;
-    }
-    this.parentIdlvl1 = null;
-    this.fractionLvl1 = null;
-    this.parentIdlvl2 = null;
-    this.parentIdlvl2 = null;
-    this.fractionLvl3 = null;
-    this.fractionLvl3 = null;
-    this.parentIdlvl4 = null;
-    this.fractionLvl4 = null;
-    this.parentIdlvl5 = null;
-    this.fractionLvl5 = null;
-    this.parentIdlvl6 = null;
-    this.fractionLvl6 = null;
-
-    setTimeout(() => {
-      this.parentIdlvl1 = event.data.id;
-      const level = event.data.level;
-      this.fractionLvl1 = event.data;
-    }, 500);
-  }
-
-  getFraction(event: any) {
-    console.log(event);
-    if (event == null) {
-      this.onLoadToast('info', 'No se encontraron registros');
-      return;
-    }
-
-    if (event.level == 1) {
-      this.parentIdlvl2 = event.id;
-      this.fractionLvl2 = event;
-      //clean
-      this.parentIdlvl3 = null;
-      this.fractionLvl3 = null;
-      this.parentIdlvl4 = null;
-      this.fractionLvl4 = null;
-      this.parentIdlvl5 = null;
-      this.fractionLvl5 = null;
-      this.parentIdlvl6 = null;
-      this.fractionLvl6 = null;
-    }
-
-    if (event.level == 2) {
-      this.parentIdlvl3 = event.id;
-      this.fractionLvl3 = event;
-      //clean
-      this.parentIdlvl4 = null;
-      this.fractionLvl4 = null;
-      this.parentIdlvl5 = null;
-      this.fractionLvl5 = null;
-      this.parentIdlvl6 = null;
-      this.fractionLvl6 = null;
-    }
-
-    if (event.level == 3) {
-      this.parentIdlvl4 = event.id;
-      this.fractionLvl4 = event;
-      //clean
-      this.parentIdlvl5 = null;
-      this.fractionLvl5 = null;
-      this.parentIdlvl6 = null;
-      this.fractionLvl6 = null;
-    }
-
-    if (event.level == 4) {
-      this.parentIdlvl5 = event.id;
-      this.fractionLvl5 = event;
-      //clean
-      this.parentIdlvl6 = null;
-      this.fractionLvl6 = null;
-    }
-
-    if (event.level == 5) {
-      this.parentIdlvl6 = event.id;
-      this.fractionLvl6 = event;
-    }
-  }
-
-  hideFraction({ level }: any) {
-    if (level == 1) {
-      this.parentIdlvl2 = null;
-      this.fractionLvl2 = null;
-    }
-
-    if (level == 2) {
-      this.parentIdlvl3 = null;
-      this.fractionLvl3 = null;
-    }
-
-    if (level == 3) {
-      this.parentIdlvl4 = null;
-      this.fractionLvl4 = null;
-    }
-
-    if (level == 4) {
-      this.parentIdlvl5 = null;
-      this.fractionLvl5 = null;
-    }
-
-    if (level == 5) {
-      this.parentIdlvl6 = null;
-      this.fractionLvl6 = null;
-    }
+    this.returnfraction.emit(event.data);
   }
 
   openForm(fraction?: IFraction) {
+    let oldFraction = null;
+    if (fraction == undefined) oldFraction = this.data['data'][0];
     let config: ModalOptions = {
       initialState: {
         fraction,
+        oldFraction,
         callback: (next: boolean) => {
           if (next) this.getFractions();
         },
