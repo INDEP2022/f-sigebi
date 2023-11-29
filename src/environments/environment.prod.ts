eb72d1750d108dbae8787bed3abdd4f6127c920d
alebrhine <medved.belyi@gmail.com>
2023-11-29 14:26:48 -0600

    add donations page update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,26 @@
 export const environment = {
   production: true,
+  API_URL: 'http://sigebimsqa.indep.gob.mx/', //'http://localhost:3000/api/v1/', // ********** SOLO CAMBIAR ESTA PARTE
+  API_URL2: 'http://sigebimsqa.indep.gob.mx/', // ********** SOLO CAMBIAR ESTA PARTE
+  SOCKET_URL: 'http://socket.indep.gob.mx/',
+  URL_PREFIX: 'api/v1/',
+  API_REPORTS_BASE: 'http://reports-qa.indep.gob.mx/jasperserver/',
+  API_REPORTS:
+    'http://reports-qa.indep.gob.mx/jasperserver/rest_v2/reports/SIGEBI/Reportes/',
+  API_REPORTS_USR: 'sigebiadmon',
+  API_REPORTS_PSW: '87654321',
+  API_CONTENT: 'http://sigebimstest.indep.gob.mx/',
+  api_external_token:
+    'http://52.142.23.169/realms/indep/protocol/openid-connect/token',
+  api_external_userInfo:
+    'http://52.142.23.169/realms/indep/protocol/openid-connect/userinfo',
+  api_external_typeUser: 'http://168.61.50.112:3003/api/v1/user/userType/',
+  api_external_rolesUser: 'http://168.61.50.112:3003/api/v1/user/roles',
+};
+
+/*
+export const environment = {
+  production: true,
   API_URL: 'http://sigebimsdev.indep.gob.mx/', //'http://localhost:3000/api/v1/',
   API_URL2: 'http://sigebimsdev.indep.gob.mx/',
   API_CONTENT: 'http://sigebimsdev.indep.gob.mx/',
@@ -17,3 +38,4 @@ export const environment = {
   api_external_typeUser: 'http://168.61.50.112:3003/api/v1/user/userType/',
   api_external_rolesUser: 'http://168.61.50.112:3003/api/v1/user/roles',
 };
+*/
