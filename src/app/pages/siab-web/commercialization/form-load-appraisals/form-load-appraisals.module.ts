afb5a9b12231c747ed793246e7ff191ee5dcf738
Felix Mamani <felixmamanicala@gmail.com>
2024-02-29 11:08:55 -0400

    fix reports

--- src/app/pages/siab-web/commercialization/form-load-appraisals/form-load-appraisals.module.ts
+++ src/app/pages/siab-web/commercialization/form-load-appraisals/form-load-appraisals.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { SharedModule } from 'src/app/shared/shared.module';
 
+import { FormLoadAppraisalsModalComponent } from './form-load-appraisals-modal/form-load-appraisals-modal/form-load-appraisals-modal.component';
 import { FormLoadAppraisalsRoutingModule } from './form-load-appraisals-routing.module';
 import { FormLoadAppraisalsComponent } from './form-load-appraisals/form-load-appraisals.component';
 
 @NgModule({
-  declarations: [FormLoadAppraisalsComponent],
+  declarations: [FormLoadAppraisalsComponent, FormLoadAppraisalsModalComponent],
   imports: [
     CommonModule,
     FormLoadAppraisalsRoutingModule,
