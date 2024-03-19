d2feb725362ab8348ab44d32f5d0dd7e9e42c200
CxGD3V <dgm3036@gmail.com>
2024-03-18 21:49:35 -0500

    Fixed bugs

--- src/app/pages/commercialization/shared-marketing-components/payment-dispersion-monitor/can-lcs-warranty/can-lcs-warranty.component.ts
+++ src/app/pages/commercialization/shared-marketing-components/payment-dispersion-monitor/can-lcs-warranty/can-lcs-warranty.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalDataSource } from 'ng2-smart-table';
 import { BsModalRef } from 'ngx-bootstrap/modal';
-import { BehaviorSubject, takeUntil } from 'rxjs';
+import {
+  BehaviorSubject,
+  debounceTime,
+  distinctUntilChanged,
+  takeUntil,
+  tap,
+} from 'rxjs';
 import { TABLE_SETTINGS } from 'src/app/common/constants/table-settings';
 import {
   FilterParams,
   ListParams,
+  SearchFilter,
 } from 'src/app/common/repository/interfaces/list-params';
 import { LotService } from 'src/app/core/services/ms-lot/lot.service';
 import { BasePage } from 'src/app/core/shared';
@@ -44,7 +51,10 @@ export class CanLcsWarrantyComponent extends BasePage implements OnInit {
     this.params.value.page = 1;
     this.params.value.limit = 10;
 
+    this.columnsFilter().subscribe();
+
     this.params.pipe(takeUntil(this.$unSubscribe)).subscribe(params => {
+      console.log(params);
       this.getData();
     });
   }
@@ -53,14 +63,60 @@ export class CanLcsWarrantyComponent extends BasePage implements OnInit {
     this.bsModal.hide();
   }
 
-  getData() {
+  columnsFilter() {
+    return this.data.onChanged().pipe(
+      distinctUntilChanged(),
+      debounceTime(500),
+      takeUntil(this.$unSubscribe),
+      tap(dataSource => {
+        console.log(dataSource);
+        this.buildColumnFilter(dataSource);
+      })
+    );
+  }
+
+  buildColumnFilter(dataSource: any) {
+    const params = new FilterParams();
+    params.removeAllFilters();
+
+    if (dataSource.action == 'filter') {
+      const filters = dataSource.filter.filters;
+      filters.forEach((filter: any) => {
+        console.log(filter);
+        const columns = this.settings.columns as any;
+        let operator = columns[filter.field]?.operator;
+
+        if (!filter.search) {
+          return;
+        }
+
+        params.addFilter(
+          filter.field,
+          filter.search,
+          operator || SearchFilter.EQ
+        );
+      });
+      this.getData(params);
+      // this.params.next(params);
+    }
+  }
+
+  getData(paramsP?: FilterParams) {
     this.loading = true;
     const paramsF = new FilterParams();
-    this.idEvent != null ? paramsF.addFilter('Event_ID', this.idEvent) : '';
-    paramsF.page = this.params.value.page;
-    paramsF.limit = this.params.value.limit;
 
-    this.comerLotService.getLotComerRefGuarentee().subscribe(
+    if (paramsP) {
+      console.log(paramsP);
+      paramsF.filters = paramsP.filters;
+      paramsF.page = 1;
+      paramsF.limit = this.params.value.limit;
+    } else {
+      paramsF.page = this.params.value.page;
+      paramsF.limit = this.params.value.limit;
+    }
+    this.idEvent != null ? paramsF.addFilter('Event_ID', this.idEvent) : '';
+
+    this.comerLotService.getLotComerRefGuarentee(paramsF.getParams()).subscribe(
       res => {
         console.log(res);
         this.data.load(res.data);
