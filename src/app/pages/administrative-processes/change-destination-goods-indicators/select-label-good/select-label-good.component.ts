08d2b4be8a39433fdfdc3cc46936525d399ff66f
henrygiovannisanchezsolis <sshenrygiovanni@gmail.com>
2024-03-22 18:05:04 -0600

    arreglar checks

--- src/app/pages/administrative-processes/change-destination-goods-indicators/select-label-good/select-label-good.component.ts
+++ src/app/pages/administrative-processes/change-destination-goods-indicators/select-label-good/select-label-good.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { ModelForm } from 'src/app/core/interfaces/model-form';
+import { LabelOkeyService } from 'src/app/core/services/catalogs/label-okey.service';
+import { GoodService } from 'src/app/core/services/ms-good/good.service';
 import { BasePage } from 'src/app/core/shared';
 
 @Component({
@@ -13,13 +15,29 @@ export class SelectLabelGoodComponent extends BasePage implements OnInit {
   form: ModelForm<any>;
   labelNumber: any;
   path: string = '';
+  title: string = '';
+  goodData: any;
 
-  constructor(private modalRef: BsModalRef, private fb: FormBuilder) {
+  constructor(
+    private modalRef: BsModalRef,
+    private fb: FormBuilder,
+    private labelOkeyService: LabelOkeyService,
+    private goodService: GoodService
+  ) {
     super();
   }
 
   ngOnInit(): void {
     console.log('labelNumber', this.labelNumber);
+
+    this.labelOkeyService.getById(this.labelNumber).subscribe({
+      next: resp => {
+        console.log('Respuesta etiqueta: ', resp);
+        this.title = resp.description;
+      },
+      error: error => {},
+    });
+
     this.prepareForm();
   }
 
@@ -31,6 +49,30 @@ export class SelectLabelGoodComponent extends BasePage implements OnInit {
       this.edit = true;
       this.affairForm.patchValue(this.affair);
     }*/
+  }
+
+  changeIndicataors() {
+    const labelGood = this.form.controls['labelGood'].value;
+
+    console.log('ID de etiqueta seleccionado:_ ', labelGood);
+
+    let objGood = {
+      id: this.goodData.id,
+      goodId: this.goodData.id,
+      labelNumber: labelGood,
+    };
+    console.log('ObjGood', objGood);
+
+    this.goodService.update(objGood).subscribe({
+      next: resp => {
+        console.log('Actualizado: ', resp);
+      },
+      error: error => {
+        console.log('No se actualizó: ', error);
+      },
+    });
+
+    this.modalRef.hide();
   }
 
   close() {
